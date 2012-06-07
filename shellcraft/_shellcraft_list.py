80ef836b4b824a44fb62ead3d66de4a24f65532e
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-06-07 23:02:19 +0200

    dont crash if __init__.py is missing

--- shellcraft/_shellcraft_list.py
+++ shellcraft/_shellcraft_list.py
@@ -10,7 +10,10 @@ CODEZ = join(sys.path[0], CODEZ)
 def shellcraft_list():
     codez = []
     for dir, _, files in os.walk(CODEZ):
-        files.remove('__init__.py')
+        try:
+            files.remove('__init__.py')
+        except ValueError:
+            pass
         files = filter(lambda x: x.endswith('.py'), files)
         codez += (map(lambda x: relpath(join(dir, x), CODEZ)[:-3].replace('/', '.'), files))
     return sorted(codez)
