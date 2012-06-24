9e7e3583653bf2695453c13fe81a06a48724884a
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-06-24 23:04:20 +0200

    removed old shellcode

--- shellcraft/codez/freebsd-sh.py
+++
@@ -1,5 +0,0 @@
-from shellcraft import *
-arch = INTEL
-def main():
-    """Call /bin/sh. Takes no arguments."""
-    return asm('xor eax, eax') + template('freebsd/sh.asm', {})
--- shellcraft/codez/linux-mips-sh.py
+++
@@ -1,5 +0,0 @@
-from core import *
-arch = MIPS
-def main():
-    """Call /bin/sh. Takes no arguments."""
-    return asm('xor ecx, ecx') + template('linux/sh.asm')
