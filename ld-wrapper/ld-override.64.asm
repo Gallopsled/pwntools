482f680b7ebd5786ae638cd46994b21ab439b197
Mathias Svensson <mathias@diku.dk>
2012-06-28 21:09:01 +0200

    64-bit might just be working now

--- ld-wrapper/ld-override.64.asm
+++ ld-wrapper/ld-override.64.asm
@@ -8,6 +8,7 @@ extern thread_stack
 extern rand
 
 struc STATE
+    .RAX: resq 1
     .RBX: resq 1
     .RCX: resq 1
     .RDX: resq 1
@@ -16,6 +17,7 @@ struc STATE
     .RSI: resq 1
     .R8:  resq 1
     .R9:  resq 1
+    .R10: resq 1
     .R12: resq 1
     .R13: resq 1
     .R14: resq 1
@@ -39,6 +41,7 @@ wrapper:
     push r14
     push r13
     push r12
+    push r10
     push r9
     push r8
     push rsi
@@ -47,58 +50,63 @@ wrapper:
     push rdx
     push rcx
     push rbx
+    push rax
 
     ; Call consistency_check with a pointer to the _save_state
     mov rdi, rsp
-    mov qword rax, consistency_check
-    call rax
+    mov qword r11, consistency_check
+    call r11
     
     ; Make sure the function pointer is valid
     mov rdi, [rsp+STATE.FUNCTION]
-    mov qword rax, lookup_function
-    call rax
+    mov qword r11, lookup_function
+    call r11
 
     ; Save state on  thread_stack
     ; Results in everything but the arguments themselves being popped
-    mov qword rax, thread_stack
-    call rax
+    mov qword r11, thread_stack
+    call r11
     add qword [rax], STATE_size
 
-    mov rax, [rax]
+    mov r11, [rax]
     xor rcx, rcx
 
 .loop:
     pop rdx
-    mov [rax-STATE_size+rcx], rdx
+    mov [r11-STATE_size+rcx], rdx
     add rcx, 8
     cmp rcx, STATE_size
     jl .loop
     
 
     ; Call the wrapper function
-    mov rcx, [rax-STATE_size+STATE.RCX]
-    mov rdx, [rax-STATE_size+STATE.RDX]
-    mov rdi, [rax-STATE_size+STATE.RDI]
-    mov rsi, [rax-STATE_size+STATE.RSI]
-    mov r8,  [rax-STATE_size+STATE.R8]
-    mov r9,  [rax-STATE_size+STATE.R9]
-    mov rax, [rax-STATE_size+STATE.FUNCTION]
-    mov rax, [rax+FUNCTION.WRAPPER]
-    call rax
+    mov rax, [r11-STATE_size+STATE.RAX]
+    mov rcx, [r11-STATE_size+STATE.RCX]
+    mov rdx, [r11-STATE_size+STATE.RDX]
+    mov rdi, [r11-STATE_size+STATE.RDI]
+    mov rsi, [r11-STATE_size+STATE.RSI]
+    mov r8,  [r11-STATE_size+STATE.R8]
+    mov r9,  [r11-STATE_size+STATE.R9]
+    mov r10, [r11-STATE_size+STATE.R10]
+    mov r11, [r11-STATE_size+STATE.FUNCTION]
+    mov r11, [r11+FUNCTION.WRAPPER]
+    call r11
 
     ; Call the real function
-    mov qword rax, thread_stack
-    call rax
-    mov rax, [rax]
-    mov rcx, [rax-STATE_size+STATE.RCX]
-    mov rdx, [rax-STATE_size+STATE.RDX]
-    mov rdi, [rax-STATE_size+STATE.RDI]
-    mov rsi, [rax-STATE_size+STATE.RSI]
-    mov r8,  [rax-STATE_size+STATE.R8]
-    mov r9,  [rax-STATE_size+STATE.R9]
-    mov rax, [rax-STATE_size+STATE.FUNCTION]
-    mov rax, [rax+FUNCTION.REAL]
-    call rax
+    mov qword r11, thread_stack
+    call r11
+    mov r11, [rax]
+    mov rax, [r11-STATE_size+STATE.RAX]
+    mov rcx, [r11-STATE_size+STATE.RCX]
+    mov rdx, [r11-STATE_size+STATE.RDX]
+    mov rdi, [r11-STATE_size+STATE.RDI]
+    mov rsi, [r11-STATE_size+STATE.RSI]
+    mov r8,  [r11-STATE_size+STATE.R8]
+    mov r9,  [r11-STATE_size+STATE.R9]
+    mov r10, [r11-STATE_size+STATE.R10]
+    mov r11, [r11-STATE_size+STATE.FUNCTION]
+    mov r11, [r11+FUNCTION.REAL]
+    call r11
 
     ; Make the return value be an argument to skip_real
     mov rdi, rax
@@ -106,23 +114,23 @@ wrapper:
 skip_real:
     push rdi
     ; Find the thread_stack
-    mov qword rax, thread_stack
-    call rax
-    mov rcx, [rax]
+    mov qword r11, thread_stack
+    call r11
+    mov r11, [rax]
 
     ; Get the value to return when done from the stack
     pop rdx
     
     ; Restore callee-saved values
-    mov rbx, [rcx-STATE_size+STATE.RBX]
-    mov rbp, [rcx-STATE_size+STATE.RBP]
-    mov r12, [rcx-STATE_size+STATE.R12]
-    mov r13, [rcx-STATE_size+STATE.R13]
-    mov r14, [rcx-STATE_size+STATE.R14]
-    mov r15, [rcx-STATE_size+STATE.R15]
-    mov rsp, [rcx-STATE_size+STATE.RSP]
-    mov rcx, [rcx-STATE_size+STATE.RIP]
-    mov [rsp], rcx
+    mov rbx, [r11-STATE_size+STATE.RBX]
+    mov rbp, [r11-STATE_size+STATE.RBP]
+    mov r12, [r11-STATE_size+STATE.R12]
+    mov r13, [r11-STATE_size+STATE.R13]
+    mov r14, [r11-STATE_size+STATE.R14]
+    mov r15, [r11-STATE_size+STATE.R15]
+    mov rsp, [r11-STATE_size+STATE.RSP]
+    mov r11, [r11-STATE_size+STATE.RIP]
+    mov [rsp], r11
 
     ; Clean up the thread_stack
     sub qword [rax], STATE_size
@@ -130,13 +138,30 @@ skip_real:
     ; Push the return value to the local stack
     push rdx
 
-    ; Put garbage on rcx and rdx
+    ; Get a lot of garbage and put it on the stack
+    xor rcx, rcx
+    mov ecx, (2*8)
+
+.loop2:
+    push rcx
     mov qword rax, rand
     call rax
-    mov rcx, rax
-    mov qword rax, rand
-    call rax
-    mov rdx, rax
+    pop rcx
+
+    shl rax, 32
+    push rax
+    add rsp, 4
+
+    loop .loop2
+
+    pop rcx
+    pop rdx
+    pop rsi
+    pop rdi
+    pop r8
+    pop r9
+    pop r10
+    pop r11
 
     ; Return
     pop rax
