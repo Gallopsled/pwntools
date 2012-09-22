debf5f1354a3f6b4761f16b4e91e660e89ac2dd3
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-09-22 20:09:12 +0200

    unfoo'ed the spinner

--- pwn/util/log.py
+++ pwn/util/log.py
@@ -3,24 +3,69 @@ from pwn import TRACE, DEBUG
 from text import *
 from threading import Thread
 
-__spinner = None
-
-def __start_spinner():
-    global __spinner
-    __spinner = __Spinner()
-    __spinner.daemon = True
-    __spinner.start()
-
-def __stop_spinner():
-    if __spinner is not None:
-        __spinner.running = False
-        trace('\x1b[?25h\x1b[2K\x1b[0G')
-
-def trace(s):
+def _trace(s):
     if TRACE:
-        __stop_spinner()
         sys.stderr.write(s)
         sys.stderr.flush()
+
+def _debug(s):
+    if DEBUG:
+        sys.stderr.write(s)
+        sys.stderr.flush()
+
+if sys.stderr.isatty():
+    _spinner = None
+
+    class _Spinner(Thread):
+        def __init__(self):
+            Thread.__init__(self)
+            self.running = True
+
+        def run(self):
+            i = 0
+            spinner = '|/-\\'
+            _trace('\x1b[?25l')
+            while self.running:
+                _trace('\x1b[3G' + boldblue(spinner[i]))
+                i = (i + 1) % len(spinner)
+                time.sleep(0.1)
+
+    def _stop_spinner():
+        global _spinner
+        if _spinner is not None:
+            _spinner.running = False
+            _trace('\x1b[?25h\x1b[2K\x1b[0G')
+        _spinner = None
+
+    def _start_spinner():
+        global _spinner
+        if _spinner is not None:
+            _stop_spinner()
+        _spinner = _Spinner()
+        _spinner.daemon = True
+        _spinner.start()
+
+    def trace(s):
+        _stop_spinner()
+        _trace(s)
+
+    def debug(s):
+        _stop_spinner()
+        _debug(s)
+
+    def waitfor(s):
+        trace(''.join([' ', boldblue('[ ]'), ' ', s]))
+        _start_spinner()
+else:
+    def trace(s):
+        _trace(s)
+
+    def debug(s):
+        _debug(s)
+
+    def waitfor(s):
+        info(s + '...\n')
+
 
 def success(s):
     trace(''.join([' ', boldgreen('[+]'), ' ', s]))
@@ -43,31 +88,4 @@ def warning(s):
 def info(s):
     trace(''.join([' ', boldblue('[*]'), ' ', s]))
 
-def debug(s):
-    if DEBUG:
-        __stop_spinner()
-        sys.stderr.write(s)
-        sys.stderr.flush()
 
-
-if sys.stderr.isatty():
-    class __Spinner(Thread):
-        def __init__(self):
-            Thread.__init__(self)
-            self.running = True
-
-        def run(self):
-            i = 0
-            spinner = '|/-\\'
-            trace('\x1b[?25l')
-            while self.running:
-                trace('\x1b[3G' + boldblue(spinner[i]))
-                i = (i + 1) % len(spinner)
-                time.sleep(0.1)
-
-    def waitfor(s):
-        trace(''.join([' ', boldblue('[ ]'), ' ', s]))
-        __start_spinner()
-else:
-    def waitfor(s):
-        info(s + '...\n')
