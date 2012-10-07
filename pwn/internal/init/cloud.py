a9d17c5e38f1cf79bf321134ab18e03442b26749
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-10-07 22:55:38 +0200

    updated cloud info

--- pwn/internal/init/cloud.py
+++ pwn/internal/init/cloud.py
@@ -1,30 +1,75 @@
 import pwn, atexit, os, paramiko
 from paramiko import SSHClient
 
-resolv = {'fantast' : 'fa.ntast.dk'
-          }
+nodes = {'fantast':
+             {'host'  : 'fa.ntast.dk',
+              'cores' : 1,
+              'mem'   : 128
+              },
+         'storm':
+             {'host'  : 'storm.pwnies.dk',
+              'cores' : 40,
+              'mem'   : 128 * 1024
+              },
+         'lightning':
+             {'host'  : 'lightning.pwnies.dk',
+              'cores' : 24,
+              'mem'   : 32 * 1024
+              },
+         'thunder':
+             {'host'  : 'thunder.pwnies.dk',
+              'cores' : 16,
+              'mem'   : 32 * 1024
+              },
+         'rain':
+             {'host'  : 'rain.pwnies.dk',
+              'cores' : 4,
+              'mem'   : 2 * 1024
+              },
+         'stakken':
+             {'host'  : 'stakken.pwnies.dk',
+              'cores' : 4,
+              'mem'   : 12 * 1024
+              },
+         'apollo':
+             {'host'  : 'apollo.pwnies.dk',
+              'cores' : 1,
+              'mem'   : 128
+              },
+         'artemis':
+             {'host'  : 'artemis.pwnies.dk',
+              'cores' : 1,
+              'mem'   : 128
+              }
+         }
 
 class _Node(SSHClient):
-    def __init__(self, *args):
-        SSHClient.__init__(self, *args)
+    def __init__(self):
+        SSHClient.__init__(self)
 
     def execute(self, cmd):
         i, o, e = self.exec_command(cmd)
         i.close()
         return (o.read(), e.read())
 
+    def upload(self, path):
+        pass
+
 class _Cloud(object):
     def __init__(self):
-        object.__setattr__(self, '__dict__', resolv)
-        object.__setattr__(self, '__hosts', dict())
+        dict = {n:None for n in nodes.keys()}
+        dict['nodes'] = nodes
+        object.__setattr__(self, '__dict__', dict)
+        object.__setattr__(self, 'nodes', nodes)
+        object.__setattr__(self, '__hosts', {})
 
     def __getattribute__(self, name):
         hosts = object.__getattribute__(self, '__hosts')
         if name in hosts:
             return hosts[name]
-        elif name in resolv:
-            host = resolv[name]
-            ssh = _Node()
+        elif name in nodes:
+            host = nodes[name]['host']
+            ssh = _Node(name)
             hosts[name] = ssh
             ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
             pwn.log.waitfor('Connecting to %s' % name)
