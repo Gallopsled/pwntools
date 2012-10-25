4a94acdef245138f5613bd920936d57279abb1c7
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-10-25 20:30:18 +0200

    added hash functions and group function, and cleaned up namespace

--- pwn/util.py
+++ pwn/util.py
@@ -1,8 +1,20 @@
 # top-level imports
-import struct, pwn, sys, subprocess, re, time, log, text
+import struct, pwn, sys, subprocess, re, time, log, text, hashlib
 from socket import htons, inet_aton, inet_ntoa, gethostbyname
 from os import system
 from time import sleep
+
+# list utils
+def group(lst, n):
+    """group([0,3,4,10,2,3], 2) => [(0,3), (4,10), (2,3)]
+
+    Group a list into consecutive n-tuples. Incomplete tuples are
+    discarded e.g.
+
+    >>> group(range(10), 3)
+    [(0, 1, 2), (3, 4, 5), (6, 7, 8)]
+    """
+    return zip(*[lst[i::n] for i in range(n)])
 
 # conversion functions
 def p8(x):
@@ -77,6 +89,33 @@ def aligndown(alignment, x):
 def align(alignment, x):
     return alignup(alignment, x)
 
+# hash
+for _algo in hashlib.algorithms:
+    def _closure():
+        hash = hashlib.__dict__[_algo]
+        def file(p):
+            h = hash()
+            fd = open(p)
+            while True:
+                s = fd.read(4096)
+                if not s: break
+                h.update(s)
+            fd.close()
+            return h
+        def sum(s):
+            return hash(s)
+        return (lambda x: file(x).digest(),
+                lambda x: sum(x).digest(),
+                lambda x: file(x).hexdigest(),
+                lambda x: sum(x).hexdigest())
+    file, sum, filehex, sumhex = _closure()
+    globals()[_algo + 'file'] = file
+    globals()[_algo + 'sum'] = sum
+    globals()[_algo + 'filehex'] = filehex
+    globals()[_algo + 'sumhex'] = sumhex
+# ugliest hack
+del _algo, _closure
+
 # network utils
 def ip (host):
     return struct.unpack('I', inet_aton(gethostbyname(host)))[0]
@@ -110,17 +149,23 @@ def die(s = None, e = None, error_code = -1):
         log.trace(str(e) + '\n')
     exit(error_code)
 
-def size(n):
-    '''Convert number of bytes to human readable form'''
-    if n < 1024:
-        return '%dB' % n
+def size(n, abbriv = 'B', si = False):
+    '''Convert number to human readable form'''
+    base = 1000.0 if si else 1024.0
+    if n < base:
+        return '%d%s' % (n, abbriv)
 
-    for postfix in ['KB', 'MB', 'GB', 'TB']:
-        n /= 1024.0
-        if n <= 1024:
-            return '%.2f%s' % (n, postfix)
+    for suffix in ['K', 'M', 'G', 'T']:
+        n /= base
+        if n <= base:
+            num = '%.2f' % n
+            while num[-1] == '0':
+                num = num[:-1]
+            if num[-1] == '.':
+                num = num[:-1]
+            return '%s%s%s' % (num, suffix, abbriv)
 
-    return '%.2fPB' % n
+    return '%.2fP%s' % (n, abbriv)
 
 def prompt(s, default = ''):
     r = raw_input(' ' + text.bold('[?]') + ' ' + s)
