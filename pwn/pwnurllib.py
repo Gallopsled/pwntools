561843f62f7d88994f8dde3649bf533d23b30b58
bsm <bmathiasen@gmail.com>
2012-11-12 16:31:41 +0100

    changed handling of result headers and html contents, renamed class to something more better and less crappy.

--- pwn/pwnurllib.py
+++ pwn/pwnurllib.py
@@ -3,32 +3,35 @@ import cookielib
 import urllib
 import useragents as ua
 
-class pwnurllib(object):
+class HTTPwn(object):
+
+    headers = None
+    html = None
+
     def __init__(self, ua=True):
         self.jar           = cookielib.CookieJar()
         self.cookieHandler = urllib2.HTTPCookieProcessor(self.jar)
         self.opener   = urllib2.build_opener(self.cookieHandler)
-        self.url_fd   = None
-        self.headers  = None
+        if ua:
+            self.add_uaheaders()
+
 
     def login(self, url, username, password):
         passman = urllib2.HTTPPasswordMgrWithDefaulRealm()
         passman.add_password(None, url, username, password)
         authhandler  = urllib2.HTTPBasicAuthHandler(passman)
         self.opener.add_handler(authhandler)
-        return self.open(url)
+        self.open(url)
 
 
     def open(self, url):
-        self.url_fd = self.opener.open(url)
-        self.headers = self.url_fd.headers
-        return self.url_fd
-
-
-    def get_header(self):
-        if self.headers == None:
-            return False
-        return self.headers
+        try:
+            url_fd  = self.opener.open(url)
+        except:
+            print "Something went wrong, site not fetched correctly"
+            return
+        self.headers = url_fd.headers.dict
+        self.html    = url_fd.read()            
 
 
     def add_uaheaders(self):
