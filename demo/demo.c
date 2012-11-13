87d3423113a91f29c5f18e843066aade93a943b2
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-11-13 20:13:39 +0100

    demo now also reads shellcode from stdin

--- demo/demo.c
+++ demo/demo.c
@@ -5,48 +5,69 @@
 #include <fcntl.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <string.h>
 
 int main(int argc, char **argv) {
   char *stack_space[0x1000];
-	int fd;
-	struct stat sbuf;
-	void (*shellcode)(void);
-	int i, z;
-	unsigned char *p;
-	if (argc != 2) {
-		fprintf(stderr, "Usage: %s [shellcode]\n", argv[0]);
-		return EXIT_FAILURE;
-	}
-	if ((fd = open(argv[1], O_RDONLY)) < 0) {
-		perror("open() failed");
-		return EXIT_FAILURE;
-	}
-	if (fstat(fd, &sbuf)) {
-		perror("fstat() failed");
-		return EXIT_FAILURE;
-	}
-	if ((shellcode = mmap(NULL, sbuf.st_size, PROT_READ | PROT_EXEC | PROT_WRITE,
-                        MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
-		perror("mmap() failed");
-		return EXIT_FAILURE;
-	}
+  int fd;
+  struct stat sbuf;
+  void (*shellcode)(void);
+  int i, z, c, bufsize, numb;
+  unsigned char *p;
+  if (argc != 2) {
+    fprintf(stderr, "Usage: %s (<filename>|-)\n", argv[0]);
+    return EXIT_FAILURE;
+  }
 
-	close(fd);
+  if (strcmp(argv[1], "-") == 0) {
+    bufsize = 4096;
+    p = malloc(bufsize);
+    z = 0;
+    numb = 0;
+    while ((c = getchar()) != EOF) {
+      if (numb == bufsize) {
+        bufsize += 4096;
+        p = realloc(p, bufsize);
+      }
+      p[numb++] = c;
+      if (c == '\0') z++;
+    }
 
-  printf("Read %d bytes of shell code. Here goes.\n", (int)sbuf.st_size);
+    if ((shellcode = mmap(NULL, numb, PROT_READ | PROT_EXEC | PROT_WRITE,
+                          MAP_ANONYMOUS | MAP_PRIVATE, -1, 0)) == MAP_FAILED) {
+      perror("mmap() failed");
+      return EXIT_FAILURE;
+    }
+    memcpy(shellcode, p, numb);
+    free(p);
+  } else {
+    if ((fd = open(argv[1], O_RDONLY)) < 0) {
+      perror("open() failed");
+      return EXIT_FAILURE;
+    }
+    if (fstat(fd, &sbuf)) {
+      perror("fstat() failed");
+      return EXIT_FAILURE;
+    }
+    if ((shellcode = mmap(NULL, sbuf.st_size, PROT_READ | PROT_EXEC | PROT_WRITE,
+                          MAP_PRIVATE, fd, 0)) == MAP_FAILED) {
+      perror("mmap() failed");
+      return EXIT_FAILURE;
+    }
 
-	for (i = 0, z = 0, p = (void *)shellcode; i < sbuf.st_size; i++) {
-		if (p[i] == '\0')
-      z++;
-	}
+    close(fd);
+    for (i = 0, z = 0, p = (void *)shellcode; i < sbuf.st_size; i++) {
+      if (p[i] == '\0') z++;
+    }
+    numb = sbuf.st_size;
+  }
 
-  if (z > 0)
-    printf("Shellcode contains %d zero bytes.\n", z);
-
+  printf("Read %d bytes of shell code. Here goes.\n", numb);
+  if (z > 0) printf("Shellcode contains %d zero bytes.\n", z);
   printf("~~~ Running shellcode ~~~\n");
 
-	shellcode();
+  shellcode();
 
-	/* should not be reached */
-	return EXIT_FAILURE;
+  /* should not be reached */
+  return EXIT_FAILURE;
 }
