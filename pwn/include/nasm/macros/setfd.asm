534ca36fa2f2b7ee8eda0a2484e74b0b237963e2
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-11-15 21:41:56 +0100

    special case on fd=0

--- pwn/include/nasm/macros/setfd.asm
+++ pwn/include/nasm/macros/setfd.asm
@@ -1,7 +1,11 @@
 %macro setfd 2
         %ifnum %2
-          push byte %2
-          pop %1
+          %if %2 == 0
+            xor %1, %1
+          %else
+            push byte %2
+            pop %1
+          %endif
         %else
         %if %1 <> %2
           mov %1, %2
