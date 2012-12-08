0be9b8ec7f0f3b43a118f167fde6300db17cf586
Mathias Svensson <mathias@diku.dk>
2012-12-08 02:36:04 +0100

    Fixed hashes

--- pwn/hashes.py
+++ pwn/hashes.py
@@ -14,17 +14,16 @@ for _algo in hashlib.algorithms:
             return h
         def sum(s):
             return hash(s)
-        file = lambda x: file(x).digest()
-        file.__doc__ = 'Calculates the %s sum of a file' % _algo
-        sum = lambda x: sum(x).digest()
-        sum.__doc__ = 'Calculates the %s sum of a string' % _algo
+        filef = lambda x: file(x).digest()
+        filef.__doc__ = 'Calculates the %s sum of a file' % _algo
+        sumf = lambda x: sum(x).digest()
+        sumf.__doc__ = 'Calculates the %s sum of a string' % _algo
         fileh = lambda x: file(x).hexdigest()
         fileh.__doc__ = 'Calculates the %s sum of a file; returns hex-encoded' % _algo
         sumh = lambda x: sum(x).hexdigest()
         sumh.__doc__ = 'Calculates the %s sum of a string; returns hex-encoded' % _algo
-        return file, sum, fileh, sumh
-    file, sum, filehex, sumhex = _closure()
-    globals()[_algo + 'file'] = file
-    globals()[_algo + 'sum'] = sum
-    globals()[_algo + 'filehex'] = filehex
-    globals()[_algo + 'sumhex'] = sumhex
+        return filef, sumf, fileh, sumh
+    (globals()[_algo + 'file'],
+     globals()[_algo + 'sum'],
+     globals()[_algo + 'filehex'],
+     globals()[_algo + 'sumhex']) = _closure()
