2f31fa5baa4197a46b723f81a2c989e652fdfc90
Morten Brøns-Pedersen <mortenbp@gmail.com>
2012-12-22 18:10:27 +0100

    knuth-morris-pratt -- not sure if right file, move if you want to

--- pwn/listutil.py
+++ pwn/listutil.py
@@ -35,3 +35,52 @@ def concat_all(*args):
         res.extend(concat_all(k))
 
     return res
+
+def __kmp_table(W):
+    pos = 1
+    cnd = 0
+    T = []
+    T.append(-1)
+    T.append(0)
+    while pos < len(W):
+        if W[pos] == W[cnd]:
+            cnd += 1
+            pos += 1
+            T.append(cnd)
+        elif cnd > 0:
+            cnd = T[cnd]
+        else:
+            pos += 1
+            T.append(0)
+    return T
+
+def __kmp_search(S, W):
+    m = 0
+    i = 0
+    T = __kmp_table(W)
+    while m + i < len(S):
+        if S[m + i] == W[i]:
+            i += 1
+            if i == len(W):
+                yield m
+                m += i - T[i]
+                i = max(T[i], 0)
+        else:
+            m += i - T[i]
+            i = max(T[i], 0)
+
+def __single_search(S, w):
+    for i in xrange(len(S)):
+        if S[i] == w:
+            yield i
+
+def findall(haystack, needle):
+    '''Find all occurences of needle in haystack
+
+    Uses the Knuth-Morris-Pratt algorithm'''
+    if type(haystack) <> type(needle):
+        needle = [needle]
+    if len(needle) == 1:
+        return __single_search(haystack, needle[0])
+    else:
+        return __kmp_search(haystack, needle)
