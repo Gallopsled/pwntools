f47d96a0745811bb91dcddb6e0a655757e8998d7
bsm <bmathiasen@gmail.com>
2013-01-07 07:29:58 +0100

    more basic stuff to sploitmeister

--- sploitmeister/sploitmeister.py
+++ sploitmeister/sploitmeister.py
@@ -9,6 +9,39 @@ Supported operations:
 - halt exploit for all or specific targets.
 - insert new target for existing exploit.
 - more stuff?
+
+
+Idea:
+an exploit is a json like this:
+exploit1.json:
+exploit1 = {
+  name = 'Leet hacksploit',
+  author = 'Mr. McLeetson',
+  exploits = ['/path/to/exploit1.py'],
+  targets = ['123.123.123.0', '123.123.123.1'],
+  delay = 60,
+}
+
+editing an exploit: include ID.
+new options for editing:
+addTargets
+addExploit
+removeTarget
+removeExploit
+
+e.g.:
+edit_exploit2.json = {
+  id = 1,
+  addExploit = ['/path/to/exploit2.py'],
+  removeTarget = ['123.123.123.1']
+}
+
+
+
+maybe...?
+
+
+
 '''
 
 from pwn import log
@@ -16,31 +49,48 @@ import sys
 import pymongo
 
 class Sploitmeister(object):
-    def __init__(self):
-        pass
+    def __init__(self, db):
+        self.db = db
 
     def listExploits(self):
-        pass
+        log.info("listing my current exploits")
 
     def insertExploit(self, config):
-        pass
+        ''' @return exploit ID
+'''
+        log.info("inserting exploit with config:\n %s" % str(config))
 
-    def removeExploit(self, config):
-        pass
+    def removeExploit(self, ID):
+        log.info("removing exploit with id: %s" % str(ID))
 
     def editExploit(self, config):
-        pass
+        log.info("editing exploit with config:\n %s" % str(config))
 
 
 if __name__ == "__main__":
     import argparse
     parser = argparse.ArgumentParser(description="Schrödingers Sploitmeister module :D")
+    parser.add_argument('-d', '--db',     action="store", help="The active database name", required=True)
     parser.add_argument('-i', '--insert', action="store", help="Insert new exploit")
     parser.add_argument('-r', '--remove', action="store", help="Remove existing exploit (ID)")
-    parser.add_argument('-l', '--list',   action="store", help="List existing exploits")
+    parser.add_argument('-l', '--list',   action="store_true", help="List existing exploits")
     parser.add_argument('-e', '--edit',   action="store", help="Edit existing exploit")
 
     result = parser.parse_args()
+
+
+    sploiter = Sploitmeister(result.db)
+    if result.insert:
+        sploiter.insertExploit(result.insert)
+
+    if result.remove:
+        sploiter.removeExploit(result.remove)
+
+    if result.list:
+        sploiter.listExploits()
+
+    if result.edit:
+        sploiter.editExploit(result.edit)
 
     log.warning("Avast ye, here be pirates... or nothing...")
     sys.exit(0)
