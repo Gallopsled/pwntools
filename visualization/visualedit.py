d7bf7d0d4b319839a8c4fed35a05a1ef7c343f6f
bsm <bmathiasen@gmail.com>
2013-03-14 15:23:15 +0100

    some shit

--- visualization/visualedit.py
+++ visualization/visualedit.py
@@ -62,72 +62,61 @@ def move(pos,scale,px,screen):
 def setup(path):
     px = pygame.image.load(path)
     screen = pygame.display.set_mode( px.get_rect()[2:] )
-    screen = pygame.display.set_mode( px.get_rect()[2:] )
     screen.blit(px, px.get_rect())
     pygame.display.flip()
     return screen, px
 
 def mainLoop(screen, px):
-    topleft = bottomright = prior = None
+    SNAP_TO_GRID_TOPLEFT = SNAP_TO_GRID_BOTTOMRIGHT = SELECTION = None
     n=0
     scale = 1
     pos = [0,0]
     while 1:
-        stopit = False
-        event = pygame.event.wait() # just get a single event, idle until it comes... this lowered %CPU from around 80-100% to around 1-10%
+        SNAP_TO_GRID_STOPIT = False
+        event = pygame.event.wait() # just get a single event, idle until it comes... this lowered %CPU from around 80-100 to around 1-10
+
         if event.type == pygame.MOUSEBUTTONUP:
-            if not topleft:
-                topleft = snap_it(event.pos)
+            mods = pygame.key.get_mods()
+            if mods & pygame.KMOD_CTRL: # CTRL-LeftClick -> snap to grid selection
+                if not SNAP_TO_GRID_TOPLEFT:
+                    SNAP_TO_GRID_TOPLEFT = snap_it(event.pos)
+                else:
+                    SNAP_TO_GRID_BOTTOMRIGHT = event.pos
+                    SNAP_TO_GRID_STOPIT = True
             else:
-                bottomright = event.pos
-                stopit = True
+                pass # these key-combinations should select based on the underlying graph (hilbert/entropy/etc)
 
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_s:
-                if prior:
-                    print "--saving coordinates-- topleft: (%s,%s), width: %s, height: %s" % prior
-                    print "topleft: %s, %s" % (prior[0], prior[1])
-                    print "bottomright: %s, %s" % (prior[2] + prior[0], prior[3] + prior[1])
+                if SELECTION:
+                    print "--saving coordinates-- topleft: (%s,%s), width: %s, height: %s" % SELECTION
+                    print "topleft: %s, %s" % (SELECTION[0], SELECTION[1])
+                    print "bottomright: %s, %s" % (SELECTION[2] + SELECTION[0], SELECTION[3] + SELECTION[1])
             if event.key == pygame.K_d:
                 mods = pygame.key.get_mods()
                 if mods & pygame.KMOD_CTRL:
                     print "ctrl-d :D"
 
-        # for event in pygame.event.get():
-        #     if event.type == pygame.MOUSEBUTTONUP:
-        #         if not topleft:
-        #             topleft = snap_it(event.pos)
-        #         else:
-        #             bottomright = event.pos
-        #             stopit = True
-
-        #     if event.type == pygame.KEYDOWN:
-        #         if event.key == pygame.K_s:
-        #             if prior:
-        #                 print "--saving coordinates-- topleft: (%s,%s), width: %s, height: %s" % prior
-        #                 print "topleft: %s, %s" % (prior[0], prior[1])
-        #                 print "bottomright: %s, %s" % (prior[2] + prior[0], prior[3] + prior[1])
-        #         if event.key == pygame.K_d:
-        #             mods = pygame.key.get_mods()
-        #             if mods & pygame.KMOD_CTRL:
-        #                 print "ctrl-d :D"
-
-
-        if topleft:
-            prior = displayImage(screen, px, topleft, prior)
-        if stopit:
-            topleft = bottomright = None
+        if SNAP_TO_GRID_TOPLEFT:
+            SELECTION = displayImage(screen, px, SNAP_TO_GRID_TOPLEFT, SELECTION)
+        if SNAP_TO_GRID_STOPIT:
+            SNAP_TO_GRID_TOPLEFT = SNAP_TO_GRID_BOTTOMRIGHT = None
     return
 
 if __name__ == "__main__":
-    input_loc = 'output.png'
-    output_loc = 'outtest.png'
-    screen, px = setup(input_loc)
-    try:
-        mainLoop(screen, px)
-    except KeyboardInterrupt:
-        print "got a keyboard interrupt... dying"
+    if len(sys.argv) <> 2:
+        print "usage: <program> <intput_filename>"
         exit(0)
+    else:
+        input_loc = sys.argv[1]
+    # input_loc = 'output.png'
+        output_loc = 'outtest.png'
+        screen, px = setup(input_loc)
+        try:
+            mainLoop(screen, px)
+        except KeyboardInterrupt:
+            print "got a keyboard interrupt... dying"
+            exit(0)
 
 
     # left the following code in case we want to save it out somehow... nice to have
