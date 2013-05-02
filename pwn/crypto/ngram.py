b848ccf80647da78a7f19edfeca34196c37770e2
Kristoffer Søholm <k.soeholm@gmail.com>
2013-05-02 19:26:46 +0200

    Added english bi/tri-gram frequencies

--- pwn/crypto/ngram.py
+++ pwn/crypto/ngram.py
@@ -1,6 +1,18 @@
 from math import sqrt
+from pwn import read
+
+
+english_freq = {}
+for i in [2,3]:
+    data = read('count_%dl.txt' % (i)).split()
+    total = sum(map(int, data[1::2])) * 1.
+    english_freq[i] = dict(zip(data[0::2], [int(x) / total for x in data[1::2]]))
+
 
 def generate_ngram(text, n=3):
+    """
+    Generate n-gram frequency table for given text.
+    """
     occurences = ngram = dict()
     for i in range(len(text)):
         try:
@@ -18,6 +30,9 @@ def generate_ngram(text, n=3):
     return ngram
 
 def dot(a,b):
+    """
+    Dot product between two dictionaries.
+    """
     keys = set(a.keys()).union(set(b.keys()))
     sum = 0
     for i in keys:
@@ -28,10 +43,18 @@ def dot(a,b):
     return sum
 
 def norm(a):
+    """ Euclidean vector norm """
     sum = 0
     for value in a.values():
       sum += value ** 2
     return sqrt(sum)
 
 def cosine_similarity(a,b):
+    """
+    Measure of similarity between two vectors in an inner product space. Used for
+    comparing n-gram frequency of e.g. english with the n-gram frequency of a given text,
+    and thus getting a measure of how close the text is to english.
+    """
     return dot(a,b) / (norm(a) * norm(b))
+
+
