a284fb010f98d057c19404bc7b70fda118c84a67
Mathias Svensson <idolf@pwnies.dk>
2013-06-02 20:36:16 +0200

    How did that get in there?

--- pwn/test/blocktest.py
+++ pwn/test/blocktest.py
@@ -2,7 +2,7 @@ from pwn import *
 from pwn.blocks import *
 
 b = Block([1,2,3])
-foo = (42, b << name('foo'), sizeof('foo'), addr) << name('bar')
+foo = (42, b << name('foo'), sizeof('foo')) << name('bar')
 print repr(flat(foo))
 print len(flat(foo))
 print len(foo)
