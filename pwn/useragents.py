7183edda5d30c615fcf85d25f7a04a47e3427b4f
Mathias Svensson <idolf@pwnies.dk>
2013-06-24 20:53:45 +0200

    Improved runejuhl's improvement to useragents with even more betterness

--- pwn/useragents.py
+++ pwn/useragents.py
@@ -1,15 +1,10 @@
-from pwn import memoize
-from os import path
-
 def randomua():
-    import random
+    import random, pwn, os
 
-    @memoize(use_file = False)
+    @pwn.memoize(use_file = False)
     def get_agents():
-        return open(
-            path.join(
-                path.dirname(path.realpath(__file__)),
-                'useragents.txt')
-            ).read().strip().split('\n')
+        return pwn.read(
+                os.path.join(pwn.installpath, 'pwn', 'useragents.txt')
+            ).strip().split('\n')
 
     return random.sample(get_agents(), 1)[0]
