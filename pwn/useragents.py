f141d9bf94bc8168f336a48deab0f2aba59f93c5
Rune Juhl Jacobsen <rune@juhljacobsen.dk>
2013-06-24 08:32:39 +0200

    Fixed bugs in useragents.py

--- pwn/useragents.py
+++ pwn/useragents.py
@@ -1,9 +1,15 @@
+from pwn import memoize
+from os import path
 
 def randomua():
     import random
 
     @memoize(use_file = False)
     def get_agents():
-        return read('useragents.txt').strip().split('\n')
+        return open(
+            path.join(
+                path.dirname(path.realpath(__file__)),
+                'useragents.txt')
+            ).read().strip().split('\n')
 
-    return random.sample(get_argents(), 1)[0]
+    return random.sample(get_agents(), 1)[0]
