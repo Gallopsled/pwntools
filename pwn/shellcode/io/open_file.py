648971937a6f34918aca5260607e097299bbd5d3
Mathias Svensson <idolf@pwnies.dk>
2013-06-26 12:38:17 +0200

    Added open_file shellcode for arm-linux

--- pwn/shellcode/io/open_file.py
+++ pwn/shellcode/io/open_file.py
@@ -1,7 +1,7 @@
 from pwn.internal.shellcode_helper import *
 from ..misc.pushstr import pushstr
 
-@shellcode_reqs(arch=['i386', 'amd64'], os=['linux', 'freebsd'])
+@shellcode_reqs(arch=['i386', 'amd64', 'arm'], os=['linux', 'freebsd'])
 def open_file(filepath, flags = 0, mode = 0, arch = None, os = None):
     """Args: filepath [flags = O_RDONLY] [mode = 0]
     Opens a file or directory with the specified flags and mode.
@@ -19,6 +19,9 @@ def open_file(filepath, flags = 0, mode = 0, arch = None, os = None):
     elif arch == 'amd64':
         if os in ['linux', 'freebsd']:
             return pushstr(filepath), _open_file_amd64(flags, mode)
+    elif arch == 'arm' and os == 'linux':
+        return _open_file_linux_arm(filepath, flags, mode)
+
     bug("OS/arch combination (%s, %s) is not supported for open_file" % (os, arch))
 
 def _open_file_linux_i386(flags, mode):
@@ -70,3 +73,20 @@ def _open_file_amd64(flags, mode):
             syscall"""
 
     return out
+
+def _open_file_linux_arm(filepath, flags, mode):
+    out = []
+
+    if (flags & 0o100) != 0:
+        out += ['mov r2, #%d' % mode]
+
+
+    out += ['mov r1, #%d' % flags,
+            'adr r0, filepath',
+            'svc SYS_open',
+            'b after_open',
+            'filepath: .byte %s // %s' % (', '.join(hex(ord(c)) for c in filepath), filepath),
+            '.align 2',
+            'after_open:']
+
+    return '\n'.join(out)
