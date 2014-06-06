10fb4b3780dfc9c4f739cc1cc45b3b67245ee8e0
Mathias Svensson <idolf@pwnies.dk>
2014-06-06 17:13:59 +0200

    New pushstr

--- pwnlib/shellcraft/templates/i386/pushstr.asm
+++ pwnlib/shellcraft/templates/i386/pushstr.asm
@@ -19,23 +19,22 @@ Args:
     else:
         extend = '\x00'
 
-    string = string.ljust(pwn.align(4, len(string)), extend)
+    string = string.ljust(util.align(4, len(string)), extend)
 %>
 
-% for s in pwn.group(4, string)[::-1]:
+% for s in util.group(4, string)[::-1]:
 <%
-    n = pwn.u32(s)
-    sign = n - (2 * (n & 2**31))
+    sign = util.u32ls(s)
 %>\
-    % if n == 0:
+    % if sign == 0:
         push 1
         dec byte [esp] ; ${repr(s)}
     % elif -128 <= sign < 128:
-        push `${repr(s)[1:-1]}`
+        push ${hex(sign)} ; ${repr(s)}
     % elif '\x00' not in s and '\n' not in s:
         push `${repr(s)[1:-1]}`
     % else:
-<% a,b = pwn.xor_pair(s, avoid = '\x00\n') %>\
+<% a,b = util.xor_pair(s, avoid = '\x00\n') %>\
         push `${repr(a)[1:-1]}`
         xor dword [esp], `${repr(b)[1:-1]}` ; ${repr(s)}
     % endif
