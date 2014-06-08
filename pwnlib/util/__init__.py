86f2f7745203e1de6ea803e2f9d6ed3369009507
Mathias Svensson <idolf@pwnies.dk>
2014-06-08 11:27:52 +0200

    Closer to having a functional replacement for the old pwntools. We now have the flat and p[0-8]+ functions.

--- pwnlib/util/__init__.py
+++ pwnlib/util/__init__.py
@@ -1,6 +1,7 @@
-from binary import *
-from decorator import *
-from iterator import *
-from lists import *
-from process import *
-from misc import *
+import binary
+import decorator
+import iterator
+import lists
+import process
+import misc
+import packing
--- pwnlib/util/packing.py
+++ pwnlib/util/packing.py
@@ -1,9 +1,125 @@
 from pwnlib import context
 import struct
 
+def pack(number, word_size=None, endianness=None, sign=None):
+    """Packs arbitrary-sized integer.
+
+    Word-size, endianness and signedness is done according to context.
+
+    word_size can be any positive number. The output will contain word_size/8
+    rounded up number of bytes. If word_size is not a multiple of 8, it will be
+    padded with zeroes up to a byte boundary.
+
+    Args:
+        number (int): Number to convert
+        word_size (int): Word size of the converted integer
+        endianness (str): Endianness of the converted integer ("little"/"big")
+        sign (str): Signedness of the converted integer ("unsigned"/"signed")
+
+    Returns:
+        The packed number as a string"""
+
+    # Lookup in context if not found
+    if word_size == None:
+        word_size = context.word_size
+    if endianness == None:
+        endianness = context.endianness
+    if sign == None:
+        sign = context.sign
+
+    # Verify that word_size make sense
+    if not isinstance(word_size, (int, long)) or word_size <= 0:
+        raise ValueError("word_size must be a positive integer")
+
+    if sign == 'signed':
+        limit = 1 << (word_size-1)
+        if not (-limit <= number < limit):
+            raise ValueError("number does not fit within word_size")
+    elif sign == 'unsigned':
+        limit = 1 << word_size
+        if not (0 <= number < limit):
+            raise ValueError("number does not fit within word_size")
+    else:
+        raise ValueError("sign must be either 'signed' or 'unsigned'")
+
+    # Normalize number and size now that we have verified them
+    # From now on we can treat positive and negative numbers the same
+    number = number & ((1 << word_size) - 1)
+    byte_size = (word_size + 7) / 8
+
+    out = []
+
+    for _ in range(byte_size):
+        out.append(chr(number & 0xff))
+        number = number >> 8
+
+    if endianness == 'little':
+        return ''.join(out)
+    elif endianness == 'big':
+        return ''.join(reversed(out))
+    else:
+        raise ValueError("endianness must be either 'little' or 'big'")
+
+
+def unpack(data, word_size=None, endianness=None, sign=None):
+    """Packs arbitrary-sized integer.
+
+    Word-size, endianness and signedness is done according to context.
+
+    word_size can be any positive number. If word_size is not a multiple of 8,
+    it will be, then the bits used for padding are discarded.
+
+    Args:
+        number (int): Number to convert
+        word_size (int): Word size of the converted integer
+        endianness (str): Endianness of the converted integer ("little"/"big")
+        sign (str): Signedness of the converted integer ("unsigned"/"signed")
+
+    Returns:
+        The unpacked number"""
+
+    # Lookup in context if not found
+    if word_size == None:
+        word_size = context.word_size
+    if endianness == None:
+        endianness = context.endianness
+    if sign == None:
+        sign = context.sign
+
+    # Verify that word_size make sense
+    if not isinstance(word_size, (int, long)) or word_size <= 0:
+        raise ValueError("word_size must be a positive integer")
+
+    byte_size = (word_size + 7) / 8
+
+    if byte_size != len(data):
+        raise ValueError("data must have length %d, since word_size was %d" % (byte_size, word_size))
+
+    number = 0
+
+    if endianness == "little":
+        for c in reversed(data):
+            number = (number << 8) + ord(c)
+    elif endianness == "big":
+        for c in data:
+            number = (number << 8) + ord(c)
+    else:
+        raise ValueError("endianness must be either 'little' or 'big'")
+
+    number = number & ((1 << word_size) - 1)
+
+    if sign == "unsigned":
+        return number
+    elif sign == "signed":
+        signbit = number & (1 << (word_size-1))
+        return number - 2*signbit
+    else:
+        raise ValueError("sign must be either 'signed' or 'unsigned'")
+
+
 def p8(number, endianness=None, sign=None):
     """Packs 8-bit integer.
-    Endianness and signedness is done according to context
+    Endianness and signedness is done according to context.
 
     Args:
         number (int): Number to convert
@@ -11,7 +127,7 @@ def p8(number, endianness=None, sign=None):
         sign (str): Signedness of the converted integer ("unsigned"/"signed")
 
     Returns:
-        A string of the number converted"""
+        The packed number as a string"""
     if endianness == None:
         endianness = context.endianness
     if sign == None:
@@ -21,9 +137,10 @@ def p8(number, endianness=None, sign=None):
             ("big",    "signed"  ): p8bs,
             ("big",    "unsigned"): p8bu}[endianness, sign](number)
 
+
 def p16(number, endianness=None, sign=None):
     """Packs 16-bit integer.
-    Endianness and signedness is done according to context
+    Endianness and signedness is done according to context.
 
     Args:
         number (int): Number to convert
@@ -31,7 +148,7 @@ def p16(number, endianness=None, sign=None):
         sign (str): Signedness of the converted integer ("unsigned"/"signed")
 
     Returns:
-        A string of the number converted"""
+        The packed number as a string"""
     if endianness == None:
         endianness = context.endianness
     if sign == None:
@@ -41,9 +158,10 @@ def p16(number, endianness=None, sign=None):
             ("big",    "signed"  ): p16bs,
             ("big",    "unsigned"): p16bu}[endianness, sign](number)
 
+
 def p32(number, endianness=None, sign=None):
     """Packs 32-bit integer.
-    Endianness and signedness is done according to context
+    Endianness and signedness is done according to context.
 
     Args:
         number (int): Number to convert
@@ -51,7 +169,7 @@ def p32(number, endianness=None, sign=None):
         sign (str): Signedness of the converted integer ("unsigned"/"signed")
 
     Returns:
-        A string of the number converted"""
+        The packed number as a string"""
     if endianness == None:
         endianness = context.endianness
     if sign == None:
@@ -61,9 +179,10 @@ def p32(number, endianness=None, sign=None):
             ("big",    "signed"  ): p32bs,
             ("big",    "unsigned"): p32bu}[endianness, sign](number)
 
+
 def p64(number, endianness=None, sign=None):
     """Packs 64-bit integer.
-    Endianness and signedness is done according to context
+    Endianness and signedness is done according to context.
 
     Args:
         number (int): Number to convert
@@ -71,7 +190,7 @@ def p64(number, endianness=None, sign=None):
         sign (str): Signedness of the converted integer ("unsigned"/"signed")
 
     Returns:
-        A string of the number converted"""
+        The packed number as a string"""
     if endianness == None:
         endianness = context.endianness
     if sign == None:
@@ -81,9 +200,10 @@ def p64(number, endianness=None, sign=None):
             ("big",    "signed"  ): p64bs,
             ("big",    "unsigned"): p64bu}[endianness, sign](number)
 
+
 def u8(data, endianness=None, sign=None):
     """Unpacks 8-bit integer.
-    Endianness and signedness is done according to context
+    Endianness and signedness is done according to context.
 
     Args:
         data (str): stuff to convert
@@ -91,7 +211,7 @@ def u8(data, endianness=None, sign=None):
         sign (str): Signedness of the converted integer ("unsigned"/"signed")
 
     Returns:
-        A string of the number converted"""
+        The unpacked number"""
     if endianness == None:
         endianness = context.endianness
     if sign == None:
@@ -101,9 +221,10 @@ def u8(data, endianness=None, sign=None):
             ("big",    "signed"  ): u8bs,
             ("big",    "unsigned"): u8bu}[endianness, sign](data)
 
+
 def u16(data, endianness=None, sign=None):
     """Unpacks 16-bit integer.
-    Endianness and signedness is done according to context
+    Endianness and signedness is done according to context.
 
     Args:
         data (str): stuff to convert
@@ -111,7 +232,7 @@ def u16(data, endianness=None, sign=None):
         sign (str): Signedness of the converted integer ("unsigned"/"signed")
 
     Returns:
-        A string of the number converted"""
+        The unpacked number"""
     if endianness == None:
         endianness = context.endianness
     if sign == None:
@@ -121,9 +242,10 @@ def u16(data, endianness=None, sign=None):
             ("big",    "signed"  ): u16bs,
             ("big",    "unsigned"): u16bu}[endianness, sign](data)
 
+
 def u32(data, endianness=None, sign=None):
     """Unpacks 32-bit integer.
-    Endianness and signedness is done according to context
+    Endianness and signedness is done according to context.
 
     Args:
         data (str): stuff to convert
@@ -131,7 +253,7 @@ def u32(data, endianness=None, sign=None):
         sign (str): Signedness of the converted integer ("unsigned"/"signed")
 
     Returns:
-        A string of the number converted"""
+        The unpacked number"""
     if endianness == None:
         endianness = context.endianness
     if sign == None:
@@ -141,9 +263,10 @@ def u32(data, endianness=None, sign=None):
             ("big",    "signed"  ): u32bs,
             ("big",    "unsigned"): u32bu}[endianness, sign](data)
 
+
 def u64(data, endianness=None, sign=None):
     """Unpacks 64-bit integer.
-    Endianness and signedness is done according to context
+    Endianness and signedness is done according to context.
 
     Args:
         data (str): stuff to convert
@@ -151,7 +274,7 @@ def u64(data, endianness=None, sign=None):
         sign (str): Signedness of the converted integer ("unsigned"/"signed")
 
     Returns:
-        A string of the number converted"""
+        The unpacked number"""
     if endianness == None:
         endianness = context.endianness
     if sign == None:
@@ -161,50 +284,66 @@ def u64(data, endianness=None, sign=None):
             ("big",    "signed"  ): u64bs,
             ("big",    "unsigned"): u64bu}[endianness, sign](data)
 
+
 def u8lu(data):
     return struct.unpack("<B", data)[0]
+
 
 def u8ls(data):
     return struct.unpack("<s", data)[0]
 
+
 def u8bu(data):
     return struct.unpack(">B", data)[0]
+
 
 def u8bs(data):
     return struct.unpack(">s", data)[0]
 
+
 def u16lu(data):
     return struct.unpack("<H", data)[0]
+
 
 def u16ls(data):
     return struct.unpack("<h", data)[0]
 
+
 def u16bu(data):
     return struct.unpack(">H", data)[0]
+
 
 def u16bs(data):
     return struct.unpack(">h", data)[0]
 
+
 def u32lu(data):
     return struct.unpack("<I", data)[0]
+
 
 def u32ls(data):
     return struct.unpack("<i", data)[0]
 
+
 def u32bu(data):
     return struct.unpack(">I", data)[0]
+
 
 def u32bs(data):
     return struct.unpack(">i", data)[0]
 
+
 def u64lu(data):
     return struct.unpack("<Q", data)[0]
+
 
 def u64ls(data):
     return struct.unpack("<q", data)[0]
 
+
 def u64bu(data):
     return struct.unpack(">Q", data)[0]
+
 
 def u64bs(data):
     return struct.unpack(">q", data)[0]
@@ -213,47 +352,88 @@ def u64bs(data):
 def p8lu(number):
     return struct.pack("<B", number)
 
+
 def p8ls(number):
     return struct.pack("<b", number)
+
 
 def p8bu(number):
     return struct.pack(">B", number)
 
+
 def p8bs(number):
     return struct.pack(">b", number)
+
 
 def p16lu(number):
     return struct.pack("<H", number)
 
+
 def p16ls(number):
     return struct.pack("<h", number)
+
 
 def p16bu(number):
     return struct.pack(">H", number)
 
+
 def p16bs(number):
     return struct.pack(">h", number)
+
 
 def p32lu(number):
     return struct.pack("<I", number)
 
+
 def p32ls(number):
     return struct.pack("<i", number)
+
 
 def p32bu(number):
     return struct.pack(">I", number)
 
+
 def p32bs(number):
     return struct.pack(">i", number)
+
 
 def p64lu(number):
     return struct.pack("<Q", number)
 
+
 def p64ls(number):
     return struct.pack("<q", number)
+
 
 def p64bu(number):
     return struct.pack(">Q", number)
 
+
 def p64bs(number):
     return struct.pack(">q", number)
+
+def flat(*args, **kwargs):
+    """Flattens the arguments into a string.
+
+    This function takes an arbitrary number of arbitrarily nested lists and
+    tuples. It will then find every string and number inside those and flatten
+    them out. Strings are inserted directly while numbers are packed using the
+    :func:`pack` function.
+
+    Args:
+      *args: Values to flatten
+      word_size (int): Word size of the converted integer
+      endianness (str): Endianness of the converted integer ("little"/"big")
+      sign (str): Signedness of the converted integer ("unsigned"/"signed")"""
+
+    out = []
+    for arg in args:
+        if isinstance(arg, (list, tuple)):
+            out.append(flat(*arg, **kwargs))
+        elif isinstance(arg, str):
+            out.append(arg)
+        elif isinstance(arg, (int, long)):
+            out.append(pack(arg, **kwargs))
+        else:
+            raise ValueError("Flat does not support values of type %s" % type(arg))
+    return ''.join(out)
