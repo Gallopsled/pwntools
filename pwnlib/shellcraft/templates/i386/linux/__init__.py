c5607924f34d15a52d19a6ff8549ab460b598889
DrDimon <drdimon@DDDatamaten.DDDatamaten>
2014-06-12 21:13:28 +0200

    Added getdents.asm, it needs mov.asm to work

--- pwnlib/shellcraft/templates/i386/linux/__init__.py
+++ pwnlib/shellcraft/templates/i386/linux/__init__.py
@@ -3,5 +3,6 @@
 shellcodes = {
     'i386_to_amd64' : 'i386_to_amd64.asm',
     'mprotect_all'  : 'mprotect_all.asm',
-    'sh'            : 'sh.asm'
+    'sh'            : 'sh.asm',
+    'getdents'      : 'getdents.asm'
 }
---
+++ pwnlib/shellcraft/templates/i386/linux/getdents.asm
@@ -0,0 +1,24 @@
+<%page args="in_fd=0, size=255, allocate_stack=True"/>
+<%docstring> Reads to the stack from a directory.
+
+Args:
+    in_fd (int/str): Directory to be read from. Defaults to `STDIN_FILENO`.
+    size (int): Buffer size.
+    allocate_stack (bool): allocate 'size' bytes on the stack.
+
+    You can optioanlly shave a few bytes not allocating the stack space.
+
+    Leaves the size read in eax.
+</%docstring>
+
+${common.label("getdents")}: 
+  ${i386.mov('ebx', in_fd)}
+  xor eax, eax
+  mov al, SYS_getdents
+  cdq
+  ${i386.mov('dl', size)}
+%if allocate_stack:
+  sub esp, edx
+%endif
+  mov ecx, esp
+  int 0x80
