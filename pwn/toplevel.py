4377c8a0c18ca8228680dc9b3b4383b686a6ed9e
Mathias Svensson <idolf@pwnies.dk>
2014-07-14 23:09:34 +0200

    Also add it to toplevel

--- pwn/toplevel.py
+++ pwn/toplevel.py
@@ -4,11 +4,12 @@ from pwnlib                 import *
 # Promote functions from these modules to toplevel
 from pwnlib.asm             import *
 from pwnlib.ui              import *
+from pwnlib.util.fiddling   import *
 from pwnlib.util.lists      import *
 from pwnlib.util.misc       import *
 from pwnlib.util.packing    import *
 from pwnlib.util.proc       import *
-from pwnlib.util.strings    import *
+from pwnlib.util.splash     import *
 
 # Promote these modules, so that "from pwn import *" will still let you access them directly
 import pwn, re, time, sys, os
