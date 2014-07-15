5bc41feaa853385f6daeb6df0fbfb851585af974
Mathias Svensson <idolf@pwnies.dk>
2014-07-15 19:51:32 +0200

    Made the crc lib nicer

--- pwnlib/util/crc.py
+++ pwnlib/util/crc.py
@@ -1,6 +1,6 @@
 """Module for calculating CRC-sums.
 
-Contains all commonly used crc algorithms. For most implementations it
+Contains all crc implementations know on the interwebz. For most implementations it
 contains only the core crc algorithm and not e.g. padding schemes.
 
 It is horribly slow, as implements a naive algorithm working direclty on
@@ -13,8 +13,9 @@ An obvious optimization would be to actually generate some lookup-tables.
 """
 
 from . import fiddling, packing, misc, lists
+import sys, types
 
-class _BitPolynom:
+class BitPolynom:
     def __init__(self, n):
         if not isinstance(n, (int, long)):
             raise TypeError("Polynomial must be called with an integer or a list")
@@ -26,34 +27,34 @@ class _BitPolynom:
         return self.n
 
     def __add__(self, other):
-        return _BitPolynom(int(self) ^ int(other))
+        return BitPolynom(int(self) ^ int(other))
 
     def __radd__(self, other):
-        return _BitPolynom(int(self) ^ int(other))
+        return BitPolynom(int(self) ^ int(other))
 
     def __sub__(self, other):
-        return _BitPolynom(int(self) ^ int(other))
+        return BitPolynom(int(self) ^ int(other))
 
     def __rsub__(self, other):
-        return _BitPolynom(int(self) ^ int(other))
+        return BitPolynom(int(self) ^ int(other))
 
     def __xor__(self, other):
-        return _BitPolynom(int(self) ^ int(other))
+        return BitPolynom(int(self) ^ int(other))
 
     def __rxor__(self, other):
-        return _BitPolynom(int(self) ^ int(other))
+        return BitPolynom(int(self) ^ int(other))
 
     def __or__(self, other):
-        return _BitPolynom(int(self) | int(other))
+        return BitPolynom(int(self) | int(other))
 
     def __ror__(self, other):
-        return _BitPolynom(int(self) | int(other))
+        return BitPolynom(int(self) | int(other))
 
     def __and__(self, other):
-        return _BitPolynom(int(self) & int(other))
+        return BitPolynom(int(self) & int(other))
 
     def __rand__(self, other):
-        return _BitPolynom(int(self) & int(other))
+        return BitPolynom(int(self) & int(other))
 
     def __mul__(self, other):
         a, b = int(self), int(other)
@@ -64,13 +65,13 @@ class _BitPolynom:
         for n in range(a.bit_length()):
             if a & (1 << n):
                 res ^= b << n
-        return _BitPolynom(res)
+        return BitPolynom(res)
 
     def __rmul__(self, other):
         return self * other
 
     def __divmod__(self, other):
-        other = _BitPolynom(int(other))
+        other = BitPolynom(int(other))
 
         if other == 0:
             raise ZeroDivisionError
@@ -82,10 +83,10 @@ class _BitPolynom:
             if resm & (1 << (n + other.degree())):
                 resm ^= int(other) << n
                 resd ^= 1 << n
-        return (_BitPolynom(resd), _BitPolynom(resm))
+        return (BitPolynom(resd), BitPolynom(resm))
 
     def __rdivmod__(self, other):
-        return divmod(_BitPolynom(int(other)), self)
+        return divmod(BitPolynom(int(other)), self)
 
     def __div__(self, other):
         return divmod(self, other)[0]
@@ -109,16 +110,16 @@ class _BitPolynom:
         return int(self).__cmp__(int(other))
 
     def __lshift__(self, other):
-        return _BitPolynom(int(self) << int(other))
+        return BitPolynom(int(self) << int(other))
 
     def __rlshift__(self, other):
-        return _BitPolynom(int(other) << int(self))
+        return BitPolynom(int(other) << int(self))
 
     def __rshift__(self, other):
-        return _BitPolynom(int(self) >> int(other))
+        return BitPolynom(int(self) >> int(other))
 
     def __rlshift__(self, other):
-        return _BitPolynom(int(other) >> int(self))
+        return BitPolynom(int(other) >> int(self))
 
     def degree(self):
         return max(0, int(self).bit_length()-1)
@@ -135,124 +136,190 @@ class _BitPolynom:
             out.append("1")
         return ' + '.join(out)
 
-def generic_crc(data, polynom, width, init, refin, refout, xorout):
-    """A generic CRC-sum function.
 
-    This is suitable to use with:
-    http://reveng.sourceforge.net/crc-catalogue/all.htm
+class Module(types.ModuleType):
+    def __init__(self):
+        super(Module, self).__init__(__name__)
+        self._cached_crcs = None
+        self.__dict__.update({
+            '__file__'    : __file__,
+            '__package__' : __package__,
+        })
 
-    Args:
-      data(str):    The data to calculate the CRC-sum of. This should either be a string or a list of bits.
-      polynom(int): The polynomial to use.
-      init(int):    If the CRC-sum was calculated in hardware, then this would b
-                    the initial value of the checksum register.
-      refin(bool):  Should the input bytes be reflected?
-      refout(bool): Should the checksum be reflected?
-      xorout(int):  The value to xor the checksum with before outputting
+    def __getattr__(self, attr):
+        crcs = crc._all_crcs()
 
-    The "check" value in the document is the CRC-sum of the string "123456789".
+        if attr == '__all__':
+            return ['generic_crc', 'cksum', 'find_crc_function'] + sorted(crcs.keys())
+
+        info = crcs.get(attr, None)
+        if not info:
+            raise AttributeError("'module' object has no attribute %s" % repr(attr))
+
+        func = self._make_crc(info['name'], info['poly'], info['width'], info['init'], info['refin'], info['refout'], info['xorout'], info['check'], 'See also: ' + info['link'])
+
+        setattr(self, attr, func)
+
+        return func
+
+    def __dir__(self):
+        return self.__all__
+
+    @staticmethod
+    def generic_crc(data, polynom, width, init, refin, refout, xorout):
+        """A generic CRC-sum function.
+
+        This is suitable to use with:
+        http://reveng.sourceforge.net/crc-catalogue/all.htm
+
+        The "check" value in the document is the CRC-sum of the string "123456789".
+
+        Args:
+          data(str):    The data to calculate the CRC-sum of. This should either be a string or a list of bits.
+          polynom(int): The polynomial to use.
+          init(int):    If the CRC-sum was calculated in hardware, then this would b
+                        the initial value of the checksum register.
+          refin(bool):  Should the input bytes be reflected?
+          refout(bool): Should the checksum be reflected?
+          xorout(int):  The value to xor the checksum with before outputting
 """
 
-    polynom = _BitPolynom(int(polynom)) | (1 << width)
-    if polynom.degree() != width:
-        raise ValueError("Polynomial is too large for that width")
+        polynom = BitPolynom(int(polynom)) | (1 << width)
+        if polynom.degree() != width:
+            raise ValueError("Polynomial is too large for that width")
 
-    init   &= (1 << width)-1
-    xorout &= (1 << width)-1
+        init   &= (1 << width)-1
+        xorout &= (1 << width)-1
 
-    if isinstance(data, list):
-        # refin is not meaningful in this case
-        inlen = len(data)
-        p = _BitPolynom(int(''.join('1' if v else '0' for v in data), 2))
-    elif isinstance(data, str):
-        inlen = len(data)*8
-        if refin:
-            data = fiddling.bitswap(data)
-        p = _BitPolynom(packing.unpack(data, 'all', 'big', 'unsigned'))
-    p = p << width
-    p ^= init << inlen
-    p  = p % polynom
-    res = p.n
-    if refout:
-        res = fiddling.bitswap_int(res, width)
-    res ^= xorout
+        if isinstance(data, list):
+            # refin is not meaningful in this case
+            inlen = len(data)
+            p = BitPolynom(int(''.join('1' if v else '0' for v in data), 2))
+        elif isinstance(data, str):
+            inlen = len(data)*8
+            if refin:
+                data = fiddling.bitswap(data)
+            p = BitPolynom(packing.unpack(data, 'all', 'big', 'unsigned'))
+        p = p << width
+        p ^= init << inlen
+        p  = p % polynom
+        res = p.n
+        if refout:
+            res = fiddling.bitswap_int(res, width)
+        res ^= xorout
 
-    return res
+        return res
 
-def make_crc(name, polynom, width, init, refin, refout, xorout, extra_doc = ''):
-    def inner(data):
-        return generic_crc(data, polynom, width, init, refin, refout, xorout)
-    inner.func_name = 'crc_' + name
-    inner.__name__  = 'crc_' + name
+    def _make_crc(self, name, polynom, width, init, refin, refout, xorout, check, extra_doc = ''):
+        def inner(data):
+            return crc.generic_crc(data, polynom, width, init, refin, refout, xorout)
+        inner.func_name = 'crc_' + name
+        inner.__name__  = 'crc_' + name
 
-    inner.__doc__   = """Calculates the %s checksum.
+        inner.__doc__   = """%s(data) -> int
 
-    This is simply the generic_crc function with the arguments:
-    polynom = 0x%x
-    width   = %d
-    init    = 0x%x
-    refin   = %s
-    refout  = %s
-    xorout  = 0x%x
+        Calculates the %s checksum.
 
-    %s""" % (name, polynom, width, init, refin, refout, xorout, extra_doc)
+        This is simply the :func:`generic_crc` with these frozen arguments:
 
-    return inner
+        * polynom = 0x%x
+        * width   = %d
+        * init    = 0x%x
+        * refin   = %s
+        * refout  = %s
+        * xorout  = 0x%x
 
-crc32 = make_crc('crc32', 0x04c11db7, 32, 0xffffffff, True, True, 0xffffffff, 'This is the most commonly used CRC sum.')
-crc_a = make_crc('crc_a', 0x04c11db7, 32, 0xffffffff, True, True, 0xffffffff, 'This is the CRC sum used for e.g. MIFARE cards.')
+        %s
 
-def cksum(data):
-    l = len(data)
-    while l:
-        data += p8(l & 0xff)
-        l >>= 8
-    return crc(data, 0x04c11db7, 32, 0, False, False, 0xffffffff)
+        Args:
+          data(str): The data to checksum.
 
-def all_crcs():
-    """Generates a dictionary of all the known CRC formats from:
-    http://reveng.sourceforge.net/crc-catalogue/all.htm"""
-    import os, re
-    curdir, _ = os.path.split(__file__)
-    data = misc.read(os.path.join(curdir, '..', '..', 'data', 'crcsums.txt'))
-    out = {}
-    def fixup(s):
-        if s == 'true':
-            return True
-        elif s == 'false':
-            return False
-        elif s.startswith('"'):
-            assert re.match('"[^"]+"', s)
-            return s[1:-1]
-        elif s.startswith('0x'):
-            assert re.match('0x[0-9a-fA-F]+', s)
-            return int(s[2:], 16)
-        else:
-            assert re.match('[0-9]+', s)
-            return int(s, 10)
+        Example:
+          >>> print %s('123456789')
+          %d
+    """ % (name, name, polynom, width, init, refin, refout, xorout, extra_doc, name, check)
 
-    data = [l for l in data.strip().split('\n') if l and l[0] != '#']
-    assert len(data) % 2 == 0
-    for ref, l in lists.group(2, data):
-        cur = {}
-        cur['link'] = 'http://reveng.sourceforge.net/crc-catalogue/all.htm#' + ref
-        for key in ['width', 'poly', 'init', 'refin', 'refout', 'xorout', 'check', 'name']:
-            cur[key] = fixup(re.findall('%s=(\S+)' % key, l)[0])
-        cur['impl'] = make_crc(cur['name'], cur['poly'], cur['width'], cur['init'], cur['refin'], cur['refout'], cur['xorout'], 'See also: ' + cur['link'])
-        assert cur['impl']('123456789') == cur['check']
-        assert cur['name'] not in out
-        out[cur['name']] = cur
-    return out
+        return inner
 
-def find_crc_function(data, checksum):
-    """Finds a specific CRC function from a set of data and its checksum."""
-    candidates = []
-    for v in all_crcs().values():
-        if v['impl'](data) == checksum:
-            candidates.append(v)
-    if len(candidates) > 1:
-        print ("Not enough data to decide which CRC-sum it was. It could be any of:" + ''.join('\n    ' + c['name'] for c in candidates))
-    elif len(candidates) == 0:
-        print "None of my CRC-sum implementations match this data."
-    else:
-        return candidates[0]
+    @staticmethod
+    def cksum(data):
+        """cksum(data) -> int
+
+        Calculates the same checksum as returned by the UNIX-tool ``cksum``.
+
+        Args:
+            data(str): The data to checksum.
+
+        Example:
+          >>> print cksum('123456789')
+          930766865
+"""
+
+        l = len(data)
+        data += packing.pack(l, 'all', 'little', 'unsigned')
+        return crc.crc_32_posix(data)
+
+    def _all_crcs(self):
+        """Generates a dictionary of all the known CRC formats from:
+        http://reveng.sourceforge.net/crc-catalogue/all.htm"""
+
+        if self._cached_crcs:
+            return self._cached_crcs
+
+        import os, re
+        curdir, _ = os.path.split(__file__)
+        data = misc.read(os.path.join(curdir, '..', '..', 'data', 'crcsums.txt'))
+        out = {}
+        def fixup(s):
+            if s == 'true':
+                return True
+            elif s == 'false':
+                return False
+            elif s.startswith('"'):
+                assert re.match('"[^"]+"', s)
+                return s[1:-1]
+            elif s.startswith('0x'):
+                assert re.match('0x[0-9a-fA-F]+', s)
+                return int(s[2:], 16)
+            else:
+                assert re.match('[0-9]+', s)
+                return int(s, 10)
+
+        data = [l for l in data.strip().split('\n') if l and l[0] != '#']
+        assert len(data) % 2 == 0
+        for ref, l in lists.group(2, data):
+            cur = {}
+            cur['link'] = 'http://reveng.sourceforge.net/crc-catalogue/all.htm#' + ref
+            for key in ['width', 'poly', 'init', 'refin', 'refout', 'xorout', 'check', 'name']:
+                cur[key] = fixup(re.findall('%s=(\S+)' % key, l)[0])
+
+            cur['name'] = cur['name'].lower().replace('/', '_').replace('-', '_')
+            assert cur['name'] not in out
+            out[cur['name']] = cur
+
+        self._cached_crcs = out
+        return out
+
+    @staticmethod
+    def find_crc_function(data, checksum):
+        """Finds all known CRCs function that hashes a piece of data into a specific
+        checksum. It does this by trying all known CRC functions one after the other.
+
+        Args:
+          data(str): Data for which the checksum is known.
+
+        Example:
+          >>> find_crc_function('test', 46197)
+          [<function crc_crc_16_dnp at ...>]
+        """
+        candidates = []
+        for v in crc._all_crcs().keys():
+            func = getattr(crc, v)
+            if func(data) == checksum:
+                candidates.append(func)
+        return candidates
+
+
+tether = sys.modules[__name__]
+crc = sys.modules[__name__] = Module()
+crc.__doc__ = tether.__doc__
