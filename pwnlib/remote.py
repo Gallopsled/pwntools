a427f1cf7fc1c685fc55657a9cc37877ae5644f7
Mathias Svensson <idolf@pwnies.dk>
2014-07-18 15:07:36 +0200

    Removed unused file

--- pwnlib/remote.py
+++
@@ -1,7 +0,0 @@
-import basechatter
-import socket
-
-class Remote(basechatter.basechatter, socket.socket):
-    def __init__(self, addr, port):
-        socket.socket.__init__(self)
-        self.connect((addr, port))
