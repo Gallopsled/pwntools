45b3b491af75b3d728ccf629e55a71763135be4c
Morten Brøns-Pedersen <f@ntast.dk>
2014-07-29 16:23:00 +0200

    version bump to 2.1.0

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ for dirpath, dirnames, filenames in os.walk(os.path.join('pwnlib', 'shellcraft',
 setup(
     name                 = 'pwntools',
     packages             = find_packages(),
-    version              = '2.0',
+    version              = '2.1.0',
     package_data         = {
         'pwnlib': [
             'data/crcsums.txt',
