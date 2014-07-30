a5bc72f179a7581deba01c6a03994f516274ed01
Mathias Svensson <idolf@pwnies.dk>
2014-07-30 11:02:05 +0200

    Version bump and fixed some LICENSE.txt and setup.py issues

--- pwnlib/__init__.py
+++ pwnlib/__init__.py
@@ -2,9 +2,11 @@ __all__ = [
     'asm', 'constants', 'context', 'dynelf',
     'elf', 'exception', 'log_levels', 'log',
     'memleak', 'shellcraft', 'term', 'tubes',
-    'ui','util'
+    'ui', 'util'
 ]
 from . import asm, constants, context, dynelf
 from . import elf, exception, log_levels, log
 from . import memleak, shellcraft, term, tubes
 from . import ui, util
+
+from version import __version__
--- setup.py
+++ setup.py
@@ -1,26 +1,37 @@
 #!/usr/bin/env python
 from setuptools import setup, find_packages
+from distutils.util import convert_path
+from distutils.command.install import INSTALL_SCHEMES
 import os, sys, glob
 
+# Get all template files
 templates = []
-for dirpath, dirnames, filenames in os.walk(os.path.join('pwnlib', 'shellcraft', 'templates')):
+for dirpath, dirnames, filenames in os.walk(convert_path('pwnlib/shellcraft/templates')):
     for f in filenames:
         templates.append(os.path.relpath(os.path.join(dirpath, f), 'pwnlib'))
 
-with open('VERSION') as fd:
-    version = fd.read().strip()
+# Get the version
+ns = {}
+with open(convert_path('pwnlib/version.py')) as fd:
+    exec fd.read() in ns
+version = ns['__version__']
+
+# This makes pwntools-LICENSE.txt appear with the package folders
+for scheme in INSTALL_SCHEMES.values():
+    scheme['data'] = scheme['purelib']
 
 setup(
     name                 = 'pwntools',
     packages             = find_packages(),
     version              = version,
+    data_files           = [('', ['LICENSE-pwntools.txt'])],
     package_data         = {
         'pwnlib': [
             'data/crcsums.txt',
             'data/binutils/*',
             'data/includes/*.h',
             'data/includes/*/*.h',
-        ] + templates
+        ] + templates,
     },
     scripts              = glob.glob("bin/*"),
     description          = "This is the CTF framework used by Gallopsled in every CTF.",
@@ -29,7 +40,7 @@ setup(
     url                  = 'https://github.com/Gallopsled/pwntools/',
     download_url         = "https://github.com/Gallopsled/pwntools/tarball/%s" % version,
     install_requires     = ['paramiko','argparse', 'mako'],
-    license              = "MIT",
+    license              = "Mostly MIT, some GPL/BSD, see LICENSE-pwntools.txt",
     classifiers          = [
         'Topic :: Security',
         'Environment :: Console',
