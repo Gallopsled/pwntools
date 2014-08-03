6f10e75c9bb875b37f10b7b44731d00927f07173
Mathias Svensson <idolf@pwnies.dk>
2014-08-03 12:13:20 -0700

    Updated remote_gdb_debugging.py example

--- examples/remote_gdb_debugging.py
+++ examples/remote_gdb_debugging.py
@@ -1,7 +1,14 @@
+"""
+Simple example showing how to use the remote
+gdb debugging features available in pwntools.
+"""
+
 from pwn import *
 
-s = ssh('freaken', '82.180.25.186', port = 2223, keyfile = "/home/freaken/.ssh/id_rsa")
-c = gdb.ssh_gdb(s, '/bin/sh', 'c')
+s = ssh('user', '127.0.0.1', port = 1234, keyfile = "/home/user/.ssh/id_rsa")
+c = gdb.ssh_gdb(s, '/bin/sh', execute = '''
+p/x $pc
+c''')
 
 c.sendline('ls -la')
 c.sendline('exit')
