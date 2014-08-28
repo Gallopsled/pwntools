d0907f867730dfccc7702ad5e8597a46f2ff668f
Morten Brøns-Pedersen <f@ntast.dk>
2014-08-28 20:20:30 +0200

    added `replacements` to __init__.py

--- pwnlib/__init__.py
+++ pwnlib/__init__.py
@@ -2,8 +2,8 @@ __all__ = [
     'atexception' , 'atexit'      , 'asm'         , 'constants'   ,
     'context'     , 'dynelf'      , 'elf'         , 'exception'   ,
     'gdb'         , 'log_levels'  , 'log'         , 'memleak'     ,
-    'rop'         , 'shellcraft'  , 'term'        , 'tubes'       ,
-    'ui'          , 'useragents'  , 'util'
+    'replacements', 'rop'         , 'shellcraft'  , 'term'        ,
+    'tubes'       , 'ui'          , 'useragents'  , 'util'
 ]
 
 
@@ -11,7 +11,7 @@ from . import \
     atexception   , atexit        , asm           , constants     , \
     context       , dynelf        , elf           , exception     , \
     gdb           , log_levels    , log           , memleak       , \
-    rop           , shellcraft    , term          , tubes         , \
-    ui            , useragents    , util
+    replacements  , rop           , shellcraft    , term          , \
+    tubes         , ui            , useragents    , util
 
 from .version import __version__
