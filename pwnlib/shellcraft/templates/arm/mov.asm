ba053eed81cd07b9dabec27a826094a0bcce98a6
Robert Larsen <robert@the-playground.dk>
2014-09-09 12:53:57 +0200

    Improved mov() helper.

--- pwnlib/shellcraft/templates/arm/mov.asm
+++ pwnlib/shellcraft/templates/arm/mov.asm
@@ -6,17 +6,16 @@
     Returns THUMB code for moving the specified source value
     into the specified destination register.
 </%docstring>
-/* Set ${dst} = ${src} */
+/* Set ${dst} = ${src} = 0x${'%x' % src} */
 %if not isinstance(src, (int, long)):
     mov ${dst}, ${src}
 %else:
-  %if (src & 0xffff0000) == 0 or (src & 0xffff) == 0:
+  %if src == 0:
     eor ${dst}, ${dst}
-  %endif
-  %if src & 0xffff0000:
-    movt ${dst}, #${src >> 16}
-  %endif
-  %if src & 0xffff:
+  %elif src & 0xffff0000 == 0:
+    mov ${dst}, #${src}
+  %else:
     movw ${dst}, #${src & 0xffff}
+    movt ${dst}, #${src >> 16}
   %endif
 %endif
