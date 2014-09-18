873afe189b2e83ec56d3d8154f0ada4f35815859
Zach Riggle <zach@riggle.me>
2014-09-17 20:09:17 -0400

    Fixed regression in local copy

--- pwnlib/thread.py
+++ pwnlib/thread.py
@@ -4,12 +4,10 @@ import threading
 class Thread(threading.Thread):
     def __init__(self, *args, **kwargs):
         super(Thread, self).__init__(*args, **kwargs)
-        self.parent_ctx    = context._thread_ctx()
+        self.parent_ctx    = context._thread_ctx().__dict__
         self.__old_bootstrap = self._Thread__bootstrap
         self._Thread__bootstrap = self.__new_bootstrap
 
     def __new_bootstrap(self):
-        context._ctxs.setdefault(
-            threading.current_thread().ident, self.parent_ctx
-        )
+        context._thread_ctx().__dict__.update(self.parent_ctx)
         return self.__old_bootstrap()
