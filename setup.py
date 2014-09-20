6dfda50522ccf381647c7b8189e10c77676a42ff
Pilen <fiskomaten@gmail.com>
2014-09-20 03:06:37 +0200

    Explicitly use python2 and pip2

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python2
 from setuptools import setup, find_packages
 from distutils.util import convert_path
 from distutils.command.install import INSTALL_SCHEMES
