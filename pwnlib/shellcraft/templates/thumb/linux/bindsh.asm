6c57393c57f21ff896067085ebe4bf94335e31e7
Robert Larsen <robert@the-playground.dk>
2014-10-14 20:31:12 +0200

    Describe arguments.

--- pwnlib/shellcraft/templates/thumb/linux/bindsh.asm
+++ pwnlib/shellcraft/templates/thumb/linux/bindsh.asm
@@ -6,6 +6,7 @@
     bindsh(port,network)
 
     Listens on a TCP port and spawns a shell for the first to connect.
+    Port is the TCP port to listen on, network is either 'ipv4' or 'ipv6'.
 </%docstring>
 ${listen(port, network)}
 ${dupsh()}
