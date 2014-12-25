bb3d98f95b85c5d4ad24ec1ef4baf8d242d25ee9
Zach Riggle <zach@riggle.me>
2014-12-25 00:10:01 -0500

    Enable testing SSH on Travis CI

--- .travis_install.sh
+++ .travis_install.sh
@@ -27,3 +27,22 @@ esac
 pip install -r requirements.txt
 pip install -r docs/requirements.txt
 pip install .
+
+
+#
+# Install a demo user for SSH purposes
+#
+U=demouser
+H=/home/$U
+
+sudo useradd -m $U
+ssh-keygen -t rsa -f /tmp/$U -N ''
+
+eval $(ssh-agent -s)
+ssh-add    /tmp/$U
+
+sudo -u $U mkdir $H/.ssh
+sudo -u $U cp    /tmp/$U.pub $H/.ssh/authorized_keys
+sudo chmod -R 700 $H
+
+(echo -n "$U:"; pwgen 100 1) | sudo chpasswd
