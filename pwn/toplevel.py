7fddd2fc78fd5d3ea5551dc64bcf268148aed644
Mathias Svensson <idolf@pwnies.dk>
2014-12-27 16:47:20 +0100

    Added the right Thread top toplevel

--- pwn/toplevel.py
+++ pwn/toplevel.py
@@ -3,7 +3,7 @@ from pwnlib                  import *
 
 # Promote functions from these modules to toplevel
 from pwnlib.asm              import asm, disasm, cpp
-from pwnlib.context          import context
+from pwnlib.context          import context, Thread
 from pwnlib.dynelf           import DynELF
 from pwnlib.elf              import ELF, load
 from pwnlib.exception        import PwnlibException
