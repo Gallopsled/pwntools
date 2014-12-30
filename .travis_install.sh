214862e4f74aacf8a3f6137eb52d6a30fd99cc50
Zach Riggle <riggle@google.com>
2014-12-30 15:32:16 -0500

    Split Travis installation steps into its own script so we can check the OS

---
+++ .travis_install.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+
+setup_linux()
+{
+    sudo apt-get update
+    sudo apt-get install software-properties-common pwgen
+    sudo apt-add-repository --yes ppa:pwntools/binutils
+    sudo apt-get update
+    sudo apt-get install binutils-arm-linux-gnu binutils-mips-linux-gnu binutils-powerpc-linux-gnu
+    wget -nc http://www.capstone-engine.org/download/2.1.2/capstone-2.1.2_amd64.deb
+    (echo "a30bcb58fd82d32b135eec978eaa22230c44e722 *capstone-2.1.2_amd64.deb" | sha1sum -c) || exit
+    sudo dpkg -i *.deb
+}
+
+setup_osx()
+{
+    brew update
+    brew install binutils
+    brew install capstone
+}
+
+case $(uname) in
+Darwin) setup_osx   ;;
+Linux)  setup_linux ;;
+esac
+
+pip install -r requirements.txt
+pip install -r docs/requirements.txt
+pip install .
