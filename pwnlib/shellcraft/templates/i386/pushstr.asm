f54621db55d98cf3f986a5634adcbab9c0ec9006
Mathias Svensson <idolf@pwnies.dk>
2015-01-02 12:36:20 +0100

    Updated the pushstr a bit more

--- pwnlib/shellcraft/templates/i386/pushstr.asm
+++ pwnlib/shellcraft/templates/i386/pushstr.asm
@@ -23,13 +23,11 @@ Args:
 
 % for word in lists.group(4, string, 'fill', extend)[::-1]:
 <%
-    sign = packing.u32(word, 'little', False)
+    sign = packing.u32(word, 'little', 'unsigned')
 %>
 % if sign == 0:
     push 1
     dec byte ptr [esp] /*  ${repr(word)} */
-% elif -128 <= sign < 128:
-    push ${hex(sign)} /*  ${repr(word)} */
 % elif '\x00' not in word and '\n' not in word:
     push ${hex(sign)} /*  ${repr(word)} */
 % else:
