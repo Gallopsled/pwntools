33b70138db69b133e92220d25f4a1241daf135bb
Zach Riggle <zachriggle@gmail.com>
2015-01-01 22:07:25 -0500

    Fix i386_to_amd64 and pushstr against GNU GAS syntax

--- pwnlib/shellcraft/templates/i386/freebsd/i386_to_amd64.asm
+++ pwnlib/shellcraft/templates/i386/freebsd/i386_to_amd64.asm
@@ -1,12 +1,12 @@
 <% from pwnlib.shellcraft import common %>
 <%docstring>Returns code to switch from i386 to amd64 mode.</%docstring>
 <% helper, end = common.label("helper"), common.label("end") %>
-[bits 32]
+.code32
     push 0x43 /*  This is the segment we want to go to */
     call $+4
 ${helper}:
-    db 0xc0
-    add dword [esp], ${end} - ${helper}
+    .byte 0xc0
+    add dword ptr [esp], ${end} - ${helper}
     jmp far [esp]
 ${end}:
-[bits 64]
+.code64
