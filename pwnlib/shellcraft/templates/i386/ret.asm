29590a38dbb9c6d59312dbe45a680f510bd67394
Zach Riggle <zachriggle@gmail.com>
2015-01-02 14:41:16 -0500

    Add return values for all RET shellcrafts

--- pwnlib/shellcraft/templates/i386/ret.asm
+++ pwnlib/shellcraft/templates/i386/ret.asm
@@ -1,2 +1,13 @@
-<%docstring>A single-byte RET instruction.</%docstring>
+<% from pwnlib.shellcraft import i386 %>
+<%docstring>A single-byte RET instruction.
+
+Args:
+    return_value: Value to return
+</%docstring>
+<%page args="return_value = None"/>
+
+% if return_value != None:
+    ${i386.mov('eax', return_value)}
+% endif
+
     ret
