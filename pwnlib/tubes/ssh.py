7d08ba6893695dd54a30d995c252d2af9f520d60
Zach Riggle <riggle@google.com>
2015-01-23 04:31:38 -0800

    Rename ssh.run to ssh.system since that's closer to what it really is

--- pwnlib/tubes/ssh.py
+++ pwnlib/tubes/ssh.py
@@ -377,8 +377,8 @@ class ssh(Timeout):
         """
         return self.run(shell, tty, timeout = timeout)
 
-    def run(self, process, tty = False, wd = None, env = None, timeout = Timeout.default):
-        r"""run(process, tty = False, wd = None, env = None, timeout = Timeout.default) -> ssh_channel
+    def system(self, process, tty = False, wd = None, env = None, timeout = Timeout.default):
+        r"""system(command, tty = False, wd = None, env = None, timeout = Timeout.default) -> ssh_channel
 
         Open a new channel with a specific process inside. If `tty` is True,
         then a TTY is requested on the remote server.
@@ -401,6 +401,9 @@ class ssh(Timeout):
             wd = self._wd
 
         return ssh_channel(self, process, tty, wd, env, timeout)
+
+    # Backward compatibility
+    run = system
 
     def run_to_end(self, process, tty = False, wd = None, env = None):
         r"""run_to_end(process, tty = False, timeout = Timeout.default, env = None) -> str
