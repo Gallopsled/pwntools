73a494cfd0430a675983dcce2fe3624801d6c98f
Zach Riggle <riggle@google.com>
2015-01-25 22:02:33 -0800

    Clean up import name duplication, add pwnlib.version

--- pwnlib/__init__.py
+++ pwnlib/__init__.py
@@ -1,18 +1,29 @@
+import importlib
+from .version import __version__
+version = __version__
+
 __all__ = [
-    'atexception' , 'atexit'      , 'asm'         , 'constants'   ,
-    'context'     , 'dynelf'      , 'elf'         , 'exception'   ,
-    'gdb'                         , 'log'         , 'memleak'     ,
-    'replacements', 'rop'         , 'shellcraft'  , 'term'        ,
-    'tubes'       , 'ui'          , 'useragents'  , 'util'
+    'asm',
+    'atexception',
+    'atexit',
+    'commandline',
+    'constants',
+    'context',
+    'dynelf',
+    'elf',
+    'exception',
+    'gdb',
+    'log',
+    'memleak',
+    'replacements',
+    'rop',
+    'shellcraft',
+    'term',
+    'tubes',
+    'ui',
+    'useragents',
+    'util'
 ]
 
-from . import \
-    atexception   , atexit        , asm           , constants     , \
-                    dynelf        , elf           , exception     , \
-    gdb                           , log           , memleak       , \
-    replacements  , rop           , shellcraft    , term          , \
-    tubes         , ui            , useragents    , util,           \
-    pep237
-
-# from .context import context
-from .version import __version__
+for module in __all__:
+    importlib.import_module('.%s' % module, 'pwnlib')
