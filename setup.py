cc6ca79a455863fafa67b53e97a7d30302bc8ccd
Zach Riggle <riggle@google.com>
2015-02-21 11:24:31 -0800

    Add tox

--- setup.py
+++ setup.py
@@ -38,7 +38,8 @@ install_requires     = ['paramiko>=1.15.2',
                         'ropgadget>=5.3',
                         'pyserial>=2.7',
                         'requests>=2.5.1',
-                        'pip>=6.0.8']
+                        'pip>=6.0.8',
+                        'tox>=1.8.1']
 
 # This is a hack until somebody ports psutil to OpenBSD
 if platform.system() != 'OpenBSD':
