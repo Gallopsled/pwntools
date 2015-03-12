fced987465a7ec38cffb30b317b2aa2321879817
Zach Riggle <riggle@google.com>
2015-03-12 00:12:42 -0700

    Add checksec command-line script

---
+++ pwnlib/commandline/checksec.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python2
+import argparse
+import sys
+
+from pwn import *
+
+from . import common
+
+parser = argparse.ArgumentParser(
+    description = 'Check binary security settings'
+)
+
+parser.add_argument(
+    'elf',
+    nargs='+',
+    type=file,
+    help='Files to check'
+)
+
+def main():
+    args   = parser.parse_args()
+    for f in args.elf:
+        with context.local(log_level='error'):
+            e = ELF(f.name)
+        log.info("%s (%s-bit %s)\n%s" %
+            (e.path, e.elfclass, e.arch, e.checksec()))
+
+if __name__ == '__main__': main()
