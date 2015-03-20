061ca4ab12a41f683549f370268ba69446454b5e
Zach Riggle <riggle@google.com>
2015-03-20 05:42:07 -0400

    Use container-based Travis CI so that it builds faster

--- .travis_install.sh
+++ .travis_install.sh
@@ -1,4 +1,30 @@
 #!/usr/bin/env bash
+local_deb_extract()
+{
+    wget $1
+    ar vx *.deb
+    tar xvf data.tar.gz
+    rm -f *.tar.gz *deb*
+}
+
+setup_travis()
+{
+    local_deb_extract https://launchpad.net/~pwntools/+archive/ubuntu/binutils/+files/binutils-arm-linux-gnu_2.22-6ubuntu1.1cross0.11pwntools12~precise_amd64.deb
+    local_deb_extract https://launchpad.net/~pwntools/+archive/ubuntu/binutils/+files/binutils-mips-linux-gnu_2.22-6ubuntu1.1cross0.11pwntools12~precise_amd64.deb
+    local_deb_extract https://launchpad.net/~pwntools/+archive/ubuntu/binutils/+files/binutils-powerpc-linux-gnu_2.22-6ubuntu1.1cross0.11pwntools12~precise_amd64.deb
+    local_deb_extract http://launchpadlibrarian.net/96008040/binutils-multiarch_2.22-6ubuntu1_amd64.deb
+    export PATH=$PWD/usr/bin:$PATH
+    export LD_LIBRARY_PATH=$PWD/usr/lib
+
+    pushd usr/lib
+    ln -s libbfd-2.22-multiarch.so libbfd-2.22.so
+    ln -s libopcodes-2.22-multiarch.so libopcodes-2.22.so
+    popd
+
+    which arm-linux-gnu-as
+    which mips-linux-gnu-as
+    which powerpc-linux-gnu-as
+}
 
 setup_linux()
 {
@@ -6,9 +32,6 @@ setup_linux()
     sudo apt-add-repository --yes ppa:pwntools/binutils
     sudo apt-get update
     sudo apt-get install binutils-arm-linux-gnu binutils-mips-linux-gnu binutils-powerpc-linux-gnu
-    wget -nc https://github.com/Gallopsled/pwntools-dependencies/raw/master/capstone-2.1.2_amd64.deb
-    (echo "a30bcb58fd82d32b135eec978eaa22230c44e722 *capstone-2.1.2_amd64.deb" | sha1sum -c) || exit
-    sudo dpkg -i *.deb
 }
 
 setup_osx()
@@ -18,18 +41,13 @@ setup_osx()
     brew install capstone
 }
 
-case $(uname) in
-Darwin) setup_osx   ;;
-Linux)  setup_linux ;;
-esac
+if [[ "$TRAVIS" ]]; then
+    setup_travis
+elif [[ "$(uname)" == "Darwin" ]]; then
+    setup_osx
+elif [[ "$(uname)" == "Linux" ]]; then
+    setup_linux
+fi
 
-# We want to avoid isntalling Capstone 3.0.1 as it
-# incurs a big penalty to Travis CI build speeds by
-# building libcapstone.so on-the-fly.
-#
-# To avoid this, we should pre-install capstone==2.1
-pip install capstone==2.1
-
-pip install -r requirements.txt
+pip install --upgrade -e .
 pip install -r docs/requirements.txt
-pip install -e .
