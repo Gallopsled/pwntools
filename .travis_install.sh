51d8a89d4b8a9e8e2cfd70724bc323d83243bfcb
Zach Riggle <riggle@google.com>
2015-06-22 11:58:28 -0400

    Shippable needs apt-get update to find gcc-multilib

--- .travis_install.sh
+++ .travis_install.sh
@@ -56,6 +56,7 @@ setup_osx()
 if [[ "$USER" == "travis" ]]; then
     setup_travis
 elif [[ "$USER" == "shippable" ]]; then
+    sudo apt-get update
     sudo apt-get install openssh-server gcc-multilib
     sudo /usr/sbin/sshd -f /etc/ssh/sshd_config &
     setup_travis
