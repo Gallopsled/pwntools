24dfbc850f4785612cecfef6bf49fb37b61639a4
Robert Larsen <robert@the-playground.dk>
2015-06-29 16:16:47 +0200

    Trying to build a mov() shellcode generator for MIPS.

---
+++ pwnlib/shellcraft/templates/mips/mov.asm
@@ -0,0 +1,63 @@
+<%
+  from pwnlib.util import packing, fiddling
+  from pwnlib import constants
+  from pwnlib.log import getLogger
+  from pwnlib.shellcraft.registers import arm as regs
+  from pwnlib.shellcraft import mips
+  log = getLogger('pwnlib.shellcraft.arm.mov')
+%>
+<%page args="dst, src"/>
+<%docstring>
+Move src into dest.
+
+Args:
+  dest (str): The destination register.
+  src (str): Either the input register, or an immediate value.
+</%docstring>
+<%
+#if not dst in regs:
+#    log.error('%r is not a register' % str(dst))
+#    
+#if not src in regs:
+#    src = constants.eval(src)
+
+%>
+.set noat
+%if not isinstance(src, (int, long)):
+    %if dst == src:
+        /* move ${dst}, ${src} is a no-op */
+    %else:
+        sw ${src}, -4($sp)
+        lw ${dst}, -4($sp)
+    %endif
+%else:
+    %if src == 0:
+        slti ${dst}, $zero, -1
+    %elif src == -1:
+        addi ${dst}, $zero, -1
+    %elif src > 0xffff:
+        %if (src & 0xff000000 == 0) or (src & 0xff0000 == 0):
+            ;Find out what to do with ${"0x%x" % src}
+        %else:
+            lui ${dst}, ${src >> 16}
+        %endif
+
+        <%
+            src &= 0xffff
+        %>\
+    %endif
+
+    %if src > 0:
+        %if (src & 0xff00 == 0) or (src & 0xff == 0):
+            <%
+                a, b = fiddling.xor_pair(packing.pack(src, 16), avoid = '\x00\n')
+                a = hex(packing.unpack(a, 16))
+                b = hex(packing.unpack(b, 16))
+            %>\
+            ori ${dst}, $zero, ${a}
+            xori ${dst}, ${dst}, ${b}
+        %else:
+            ori ${dst}, $zero, ${src}
+        %endif
+    %endif
+%endif
