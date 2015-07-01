5ad8ec2c3f5ed719265973e9a359fe3168b65cb5
Robert Larsen <robert@the-playground.dk>
2015-07-01 23:38:12 +0200

    Removed comments I no longer need.

--- pwnlib/shellcraft/templates/mips/mov.asm
+++ pwnlib/shellcraft/templates/mips/mov.asm
@@ -29,23 +29,18 @@ if not src in regs:
     %if dst == src:
         /* move ${dst}, ${src} is a no-op */
     %else:
-        /* Verified to not generate nul bytes */
         sw ${src}, -4($sp)
         lw ${dst}, -4($sp)
     %endif
 %else:
     %if src == 0:
-        /* Verified to not generate nul bytes */
         slti ${dst}, $zero, -1
     %elif src == 1:
-        /* Verified to not generate nul bytes */
         slti ${dst}, $zero, 0x0101
     %elif src == -1:
-        /* Verified to not generate nul bytes */
         addi ${dst}, $zero, -1
     %elif src < 0x10000:
         %if '\x00' in packing.pack(src, 16) or '\n' in packing.pack(src, 16):
-            /* Verified to not generate nul bytes */
             <%
                 a, b = fiddling.xor_pair(packing.pack(src, 16), avoid = '\x00\n')
                 a = hex(packing.unpack(a, 16))
@@ -54,15 +49,12 @@ if not src in regs:
             ori ${dst}, $zero, ${a}
             xori ${dst}, ${dst}, ${b}
         %else:
-            /* Verified to not generate nul bytes */
             ori ${dst}, $zero, ${src}
         %endif
     %elif not '\x00' in packing.pack(src, 32) or '\n' in packing.pack(src, 32):
-        /* Verified to not generate nul bytes */
         lui ${dst}, ${src >> 16}
         ori ${dst}, ${dst}, ${src & 0xffff}
     %else:
-        /* Verified to not generate nul bytes */
         ${mips.mov(dst, src >> 16)}
         sh ${dst}, -4($sp)
         ${mips.mov(dst, src & 0xffff)}
