1f289e3dbdff96e9af4bda18eee9578eea7b1cab
eQuiNoX__ <equinox.71717171@gmail.com>
2015-07-02 19:01:57 +0200

    The following changes were made :- - test for amd64 added in - sigreturn template for amd64 added in. sigreturn template for i386 modified - read.asm added for amd64

--- pwnlib/shellcraft/templates/amd64/linux/sigreturn.asm
+++ pwnlib/shellcraft/templates/amd64/linux/sigreturn.asm
@@ -1,10 +1,10 @@
 
 <%
-    from pwnlib.shellcraft.i386.linux import syscall
+    from pwnlib.shellcraft.amd64.linux import syscall
 %>
 <%docstring>
 Invokes the syscall sigreturn.  See 'man 2 sigreturn' for more information.
 
 </%docstring>
 
-    ${syscall('SYS_sigreturn', )}
+    ${syscall('SYS_rt_sigreturn', )}
