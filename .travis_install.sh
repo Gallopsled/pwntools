76d78a0e7e36b0bbd391f826cec6987a952cc376
Zach Riggle <riggle@google.com>
2015-07-29 08:03:01 -0400

    Install QEMU 2.3 instead of 2.1

--- .travis_install.sh
+++ .travis_install.sh
@@ -17,7 +17,7 @@ get_binutils()
 get_qemu()
 {
     echo "Installing qemu"
-    QEMU_URL='https://launchpadlibrarian.net/187478490/qemu-user-static_2.1%2Bdfsg-4ubuntu6_amd64.deb'
+    QEMU_URL='http://mirrors.kernel.org/ubuntu/pool/universe/q/qemu/qemu-user-static_2.3+dfsg-5ubuntu3_amd64.deb'
     local_deb_extract "$QEMU_URL"
 }
 
