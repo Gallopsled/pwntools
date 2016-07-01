dcb0c11eb57b7a99621a7737d2537327e2ec0158
Zach Riggle <riggle@google.com>
2016-07-01 13:56:41 +0200

    Enhance i386.linux.socket() shellcode to add docs and allow UDP

--- pwnlib/shellcraft/templates/i386/linux/socket.asm
+++ pwnlib/shellcraft/templates/i386/linux/socket.asm
@@ -4,9 +4,15 @@
 <%page args="network = 'ipv4', proto = 'tcp'"/>
 <%docstring>
 Creates a new socket
+
+Arguments:
+    network(str): ipv4 or ipv6
+    proto(str): tcp or udp
 </%docstring>
 <%
     address_family = {'ipv4':AF_INET,'ipv6':AF_INET6}.get(network, network)
+    proto          = {'tcp':SOCK_STREAM, SOCK_STREAM: SOCK_STREAM,
+                      'udp':SOCK_DGRAM,  SOCK_DGRAM:  SOCK_DGRAM}[proto]
 %>\
     /* open new socket */
-    ${socketcall(SYS_socketcall_socket, address_family, SOCK_STREAM, 0)}
+    ${socketcall(SYS_socketcall_socket, address_family, proto, 0)}
