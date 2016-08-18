9e57328d1ba7c0bd0b2883fb3010f47b61e48809
Zach Riggle <zachriggle@users.noreply.github.com>
2016-08-17 19:57:24 -0700

    Add warning described in Gallopsled/pwntools#518 (#652)

--- pwn/__init__.py
+++ pwn/__init__.py
@@ -6,3 +6,9 @@ pwnlib.log.install_default_handler()
 
 log = pwnlib.log.getLogger('pwnlib.exploit')
 args = pwnlib.args.args
+
+if not platform.architecture()[0].startswith('64'):
+    """Determines if the current Python interpreter is supported by Pwntools.
+
+    See Gallopsled/pwntools#518 for more information."""
+    log.warn_once('Pwntools does not support 32-bit Python.  Use a 64-bit release.')
