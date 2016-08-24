f4a72da40e3e17090b793c866ac1c70b0bc4d173
Zach Riggle <riggle@google.com>
2016-08-24 14:59:37 -0700

    Update MIPS execve to use pushstr_array

--- pwnlib/shellcraft/templates/mips/linux/execve.asm
+++ pwnlib/shellcraft/templates/mips/linux/execve.asm
@@ -1,15 +1,47 @@
-
 <%
-    from pwnlib.shellcraft.mips.linux import syscall
+from pwnlib.shellcraft import mips, registers
+from pwnlib.abi import linux_mips_syscall as abi
 %>
-<%page args="path, argv, envp"/>
 <%docstring>
-Invokes the syscall execve.  See 'man 2 execve' for more information.
+Execute a different process.
 
-Arguments:
-    path(char): path
-    argv(char): argv
-    envp(char): envp
+Attempts to perform some automatic detection of types.
+Otherwise, the arguments behave as normal.
+
+- If ``path`` is a string that is not a known register,
+  it is pushed onto the stack.
+- If ``argv`` is an array of strings, it is pushed onto
+  the stack, and NULL-terminated.
+- If ``envp`` is an dictionary of {string:string},
+  it is pushed onto the stack, and NULL-terminated.
+
+Example:
+
+    >>> path = '/bin/sh'
+    >>> argv = ['sh', '-c', 'echo Hello, $NAME; exit $STATUS']
+    >>> envp = {'NAME': 'zerocool', 'STATUS': 3}
+    >>> sc = shellcraft.mips.linux.execve(path, argv, envp)
+    >>> io = run_assembly(sc)
+    >>> io.recvall()
+    'Hello, zerocool\n'
+    >>> io.poll(True)
+    3
 </%docstring>
-
-    ${syscall('SYS_execve', path, argv, envp)}
+<%page args="path = '/system/bin//sh', argv=0, envp=0"/>
+<%
+if isinstance(envp, dict):
+    envp = ['%s=%s' % (k,v) for (k,v) in envp.items()]
+%>
+% if isinstance(argv, (list, tuple)):
+    ${mips.pushstr_array(abi.register_arguments[3], argv)}
+    <% argv = abi.register_arguments[3] %>
+% endif
+% if isinstance(envp, (list, tuple)):
+    ${mips.pushstr_array(abi.register_arguments[2], envp)}
+    <% envp = abi.register_arguments[2] %>
+% endif
+% if isinstance(path, str) and not registers.is_register(path):
+    ${mips.pushstr(path)}
+    <% path = '$sp' %>
+%endif
+    ${mips.syscall('SYS_execve', path, argv, envp)}
