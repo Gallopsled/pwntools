4dcc080e0ee8bdaf0fc3f16a6d67e79192066020
Zach Riggle <riggle@google.com>
2016-09-07 13:25:51 -0700

    Add .os to Device and set it automatically
    The use case here is setting context.os via:
    
    >>> context.device = adb.wait_for_device()

--- pwnlib/device.py
+++ pwnlib/device.py
@@ -4,12 +4,13 @@ class Device(object):
     bits = None
     endian = None
     serial = None
+    os = None
 
     def __init__(self, serial=None):
         self.serial = serial
 
     def __str__(self):
         return self.serial
- 
+
     def __eq__(self, other):
         return self.serial == other or self.serial == str(other)
