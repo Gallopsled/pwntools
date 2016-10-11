e5b6aedd3d66e5afc12770b71bd8a6e717d4d828
Mathias Svensson <idolf@pwnies.dk>
2016-10-11 03:00:18 +0200

    Release 3.2.0beta3

--- pwnlib/version.py
+++ pwnlib/version.py
@@ -1,1 +1,1 @@
-__version__ = '3.2.0beta2'
+__version__ = '3.2.0beta3'
--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ except ImportError:
 setup(
     name                 = 'pwntools',
     packages             = find_packages(),
-    version              = '3.2.0beta2',
+    version              = '3.2.0beta3',
     data_files           = [('',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
