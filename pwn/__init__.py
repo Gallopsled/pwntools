420f99c5d001918c6e39d41a7640f9937528623b
Zach Riggle <zachriggle@users.noreply.github.com>
2016-10-20 15:41:00 -0700

    Automatic update checking should not log to stdout. (#774)
    This breaks piping into files for all of the command-line utilities.

--- pwn/__init__.py
+++ pwn/__init__.py
@@ -13,4 +13,5 @@ if not platform.architecture()[0].startswith('64'):
     See Gallopsled/pwntools#518 for more information."""
     log.warn_once('Pwntools does not support 32-bit Python.  Use a 64-bit release.')
 
-pwnlib.update.check_automatically()
+with context.local(log_console=sys.stderr):
+    pwnlib.update.check_automatically()
