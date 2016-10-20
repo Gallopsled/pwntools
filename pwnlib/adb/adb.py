16ddd7d772d48ea5affb3afcb7dbb9de4d439994
mspectorgoogle <mspector@google.com>
2016-10-20 02:32:45 -0700

    Fix uninstall bug

--- pwnlib/adb/adb.py
+++ pwnlib/adb/adb.py
@@ -1036,9 +1036,9 @@ def uninstall(package, *arguments):
         package(str): Name of the package to uninstall (e.g. ``'com.foo.MyPackage'``)
         arguments: Supplementary arguments to ``'pm install'``, e.g. ``'-k'``.
     """
-    with log.progress("Installing package {}".format(package)):
+    with log.progress("Uninstalling package {}".format(package)):
         with context.quiet:
-            return process(['pm','uninstall',apk] + list(arguments)).recvall()
+            return process(['pm','uninstall',package] + list(arguments)).recvall()
 
 @context.quiet
 def packages():
