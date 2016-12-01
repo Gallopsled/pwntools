3146977ba50558b4d64bea73a771d0e9f5d18e24
Owlz <Owlz@users.noreply.github.com>
2016-11-30 23:42:20 -0500

    Adding bufSize to sock

--- pwnlib/tubes/sock.py
+++ pwnlib/tubes/sock.py
@@ -10,8 +10,8 @@ log = getLogger(__name__)
 class sock(tube):
     """Methods available exclusively to sockets."""
 
-    def __init__(self, timeout):
-        super(sock, self).__init__(timeout)
+    def __init__(self, timeout, bufSize = 4096):
+        super(sock, self).__init__(timeout, bufSize = bufSize)
         self.closed = {"recv": False, "send": False}
 
     # Overwritten for better usability
