8b30700f92b01c2cb4aecc52deded6a0244f134c
Mathias Svensson <idolf@pwnies.dk>
2016-12-24 12:14:05 +0100

    Bump to 3.5.0dev

--- pwnlib/version.py
+++ pwnlib/version.py
@@ -1,1 +1,1 @@
-__version__ = '3.4.0beta0'
+__version__ = '3.5.0dev'
--- setup.py
+++ setup.py
@@ -80,7 +80,7 @@ except ImportError:
 setup(
     name                 = 'pwntools',
     packages             = find_packages(),
-    version              = '3.4.0beta0',
+    version              = '3.5.0dev',
     data_files           = [('',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
