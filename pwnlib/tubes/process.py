Gallopsled/pwntools

commit 0cead77039ecdf5f9b0325b0b0eea1f352393abd

Move STDOUT/PIPE to process class, so they can be referenced directly

pwnlib/tubes/process.py
@@ -23,9 +23,6 @@ from pwnlib.util.misc import which
 
 log = getLogger(__name__)
 
-PIPE = subprocess.PIPE
-STDOUT = subprocess.STDOUT
-
 class PTY(object): pass
 PTY=PTY()
 
@@ -196,6 +193,8 @@ class process(tube):
         >>> p = process(binary.path)
     """
 
+    STDOUT = subprocess.STDOUT
+    PIPE = subprocess.PIPE
     PTY = PTY
 
     #: Have we seen the process stop?

commit 3dd60454885030d9594c511c653c8c5cd654a8e7

Fix regression caused by 0cead77

pwnlib/tubes/process.py
@@ -25,6 +25,8 @@ log = getLogger(__name__)
 
 class PTY(object): pass
 PTY=PTY()
+STDOUT = subprocess.STDOUT
+PIPE = subprocess.PIPE
 
 class process(tube):
     r"""
@@ -193,8 +195,8 @@ class process(tube):
         >>> p = process(binary.path)
     """
 
-    STDOUT = subprocess.STDOUT
-    PIPE = subprocess.PIPE
+    STDOUT = STDOUT
+    PIPE = PIPE
     PTY = PTY
 
     #: Have we seen the process stop?