f46112e873f5833c7d702f0d91491f6a17e213d1
Zach Riggle <riggle@google.com>
2017-01-11 00:45:25 -0500

    Fix hexdump_iter for actual file objects, like those used by phd

--- pwnlib/util/fiddling.py
+++ pwnlib/util/fiddling.py
@@ -3,6 +3,7 @@ from __future__ import absolute_import
 
 import base64
 import random
+import os
 import re
 import string
 import StringIO
@@ -617,14 +618,15 @@ def hexdump_iter(fd, width=16, skip=True, hexii=False, begin=0, style=None,
         total = fd.len
     else:
         # Save the current file offset
-        cur = fd.seek(0, os.SEEK_CUR)
+        cur = fd.tell()
 
         # Determine the total size of the file
         fd.seek(0, os.SEEK_END)
-        total = fd.tell()
+        total = fd.tell() - cur
 
-        # Restore the file offset
-        fd.seek(cur, os.SEEK_SET)
+        # Restore the file offset, and
+        fd.seek(cur or 0, os.SEEK_SET)
+
 
     if hexii:
         column_sep = ''
