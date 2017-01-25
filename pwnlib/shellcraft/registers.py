e7562fbfba9a19efaf2ecb911711bbe577f6ed97
Zach Riggle <riggle@google.com>
2017-01-25 08:16:01 -0500

    Add pwnlib.registers.current()

--- pwnlib/shellcraft/registers.py
+++ pwnlib/shellcraft/registers.py
@@ -204,6 +204,14 @@ def bits_required(value):
         bits += 8
     return bits
 
+def current():
+    return {
+        'i386': i386,
+        'amd64': amd64,
+        'arm': arm,
+        'aarch64': aarch64,
+        'mips': mips,
+    }[context.arch]
 
 # def is_register(sz):
 #     try:
