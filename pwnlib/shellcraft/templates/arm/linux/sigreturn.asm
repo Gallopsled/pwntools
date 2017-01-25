4e4df14164f3649f256cd2061c31e77fa1fa5b72
Zach Riggle <riggle@google.com>
2017-01-25 08:38:26 -0500

    Remove old templates

--- pwnlib/shellcraft/templates/arm/linux/sigreturn.asm
+++
@@ -1,8 +0,0 @@
-<%
-    from pwnlib.shellcraft.arm.linux import syscall
-%>
-<%docstring>
-Invokes the syscall sigreturn.  See 'man 2 sigreturn' for more information.
-</%docstring>
-
-    ${syscall('SYS_sigreturn')}
