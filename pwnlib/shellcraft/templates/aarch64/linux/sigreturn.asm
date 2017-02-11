646723fa8ea65354077454ac3efe65c4d2f6f493
Zach Riggle <riggle@google.com>
2017-02-10 22:19:04 -0500

    Remove unneded arg from aarch64 sigreturn

--- pwnlib/shellcraft/templates/aarch64/linux/sigreturn.asm
+++ pwnlib/shellcraft/templates/aarch64/linux/sigreturn.asm
@@ -2,7 +2,6 @@
 <%
     from pwnlib.shellcraft.aarch64.linux import syscall
 %>
-<%page args="scp"/>
 <%docstring>
 Invokes the syscall sigreturn.  See 'man 2 sigreturn' for more information.
 </%docstring>
