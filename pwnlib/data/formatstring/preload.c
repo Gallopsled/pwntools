0e6e4aeaa0f72b3d11f297d20da51576d18307e8
Zach Riggle <riggle@google.com>
2017-03-05 00:45:48 +0000

    Add preload binary

---
+++ pwnlib/data/formatstring/preload.c
@@ -0,0 +1,25 @@
+#include <signal.h>
+
+void printf() {
+    raise(SIGTRAP);
+}
+
+void sprintf() {
+    raise(SIGTRAP);
+}
+
+void snprintf() {
+    raise(SIGTRAP);
+}
+
+void asprintf() {
+    raise(SIGTRAP);
+}
+
+void dprintf() {
+    raise(SIGTRAP);
+}
+
+void fprintf() {
+    raise(SIGTRAP);
+}
