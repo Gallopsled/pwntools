3236b67abc02603c22a8de746784a9e489abda08
Securifera, Inc <securifera@gmail.com>
2017-05-15 15:39:49 -0400

    Updates

--- preinstall_win.ps1
+++ preinstall_win.ps1
@@ -1,18 +1,14 @@
 If( !(Test-Path "C:\Python27\Scripts\pip.exe") ){
-   & deps\python-2.7.12.amd64.msi /qb
+  $process="deps\python-2.7.12.amd64.msi"
+  $args="/qb"
+  Start-Process $process -ArgumentList $args -Wait
 }
 
 $cur_path = [System.Environment]::GetEnvironmentVariable("Path","User")
-If( !($cur_path -like "C:\Python27\" )){
-   $env:Path = [System.Environment]::GetEnvironmentVariable("Path","User") + ";C:\Python27"
+If( !($cur_path -like "C:\Python27" )){
+   $env:Path = [System.Environment]::GetEnvironmentVariable("Path","User") + ";C:\Python27;C:\Python27\Scripts"
 }
 
-$cur_path = [System.Environment]::GetEnvironmentVariable("Path","User")
-If( !($cur_path -like "C:\Python27\Scripts" )){
-   $env:Path = [System.Environment]::GetEnvironmentVariable("Path","User") + ";C:\Python27\Scripts"
-}
-
-pip install --upgrade pip
 pip install wheel
 pip install unicorn
 wheel install deps\capstone-3.0.5rc2-py2-none-win_amd64.whl
