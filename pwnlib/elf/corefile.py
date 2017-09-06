c438893d7eee53a82ab8f9bcf0fde5bf7186d520
Christophe GRENIER <grenier@cgsecurity.org>
2017-09-06 19:55:37 +0200

    Fix for overwritten env when parsing core file (#1014)
    * Fix for overwritten env when parsing core file
    
    * warn_once message when dealing with overwritten pointer to env variable in core file

--- pwnlib/elf/corefile.py
+++ pwnlib/elf/corefile.py
@@ -545,7 +545,7 @@ class Corefile(ELF):
                         log.warn('Could not find the stack!')
                         self.stack = None
 
-            with context.local(bytes=self.bytes, log_level='error'):
+            with context.local(bytes=self.bytes, log_level='warn'):
                 try:
                     self._parse_stack()
                 except ValueError:
@@ -826,6 +826,10 @@ class Corefile(ELF):
         # find a pointer to this address, followed by a NULL.
         last_env_addr = address + 1
         p_last_env_addr = stack.find(pack(last_env_addr), None, last_env_addr)
+        if p_last_env_addr < 0:
+            # Something weird is happening.  Just don't touch it.
+            log.warn_once("Found bad environment at %#x", last_env_addr)
+            return
 
         # Sanity check that we did correctly find the envp NULL terminator.
         envp_nullterm = p_last_env_addr+context.bytes
