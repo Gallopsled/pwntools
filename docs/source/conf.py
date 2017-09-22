601b1f22626f0b9a53bd7535d22c3b04216f8592
Zach Riggle <riggle@google.com>
2017-09-21 17:34:57 -0700

    Prevent Sphinx from dumping all level=INFO logging output to stdout.
    See Gallopsled/pwntools#1036 for more information.

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,23 @@ extensions = [
     'sphinxcontrib.napoleon'
 ]
 
+# Disable "info" logging directly to stdout by Sphinx
+import logging
+import sphinx.util.logging
+
+class SphinxPwnlibFilter(logging.Filter):
+    def filter(self, record):
+        if record.name.startswith('pwn'):
+            return False
+        return True
+
+log_filter = SphinxPwnlibFilter()
+
+for i, handler in enumerate(logging.root.handlers):
+    if isinstance(handler, sphinx.util.logging.NewLineStreamHandler):
+        print("Filtering Sphinx handler", handler)
+        handler.addFilter(log_filter)
+
 # Napoleon settings
 napoleon_use_ivar = True
 napoleon_use_rtype = False
