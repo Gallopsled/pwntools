6ea5722f5b1f62f148d5adba3867e6c9c29090a9
Zach Riggle <riggle@google.com>
2017-11-10 17:00:25 -0600

    Add a comment to dup shellcodes

--- pwnlib/shellcraft/templates/thumb/linux/dup.asm
+++ pwnlib/shellcraft/templates/thumb/linux/dup.asm
@@ -9,6 +9,7 @@ Args: [sock (imm/reg) = r6]
   dup       = common.label("dup")
   looplabel = common.label("loop")
 %>
+    /* dup() file descriptor ${sock} into stdin/stdout/stderr */
 ${dup}:
         ${mov('r1', 2)}
         ${mov('r7', 'SYS_dup2')}
