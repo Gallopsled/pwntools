f4159bdad4f3739e08a9d466e03a1c05c87da274
pwn <excalibur44@users.noreply.github.com>
2018-06-05 00:13:03 +0800

    check if ":" exists before index(':') (#1159)
    * check if ":" exists before index(':')
    
    * Update proc.py

--- pwnlib/util/proc.py
+++ pwnlib/util/proc.py
@@ -245,6 +245,8 @@ def status(pid):
     try:
         with open('/proc/%d/status' % pid) as fd:
             for line in fd:
+                if ':' not in line:
+                    continue
                 i = line.index(':')
                 key = line[:i]
                 val = line[i + 2:-1] # initial :\t and trailing \n
