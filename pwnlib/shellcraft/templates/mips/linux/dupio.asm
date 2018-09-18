58967ddfe0eaaf722a6843fbf0c8d9598c08fadf
leommxj <leommxj@gmail.com>
2018-09-18 22:31:28 +0800

    Rename dup to dupio and Modify dupsh/dupio

--- pwnlib/shellcraft/templates/mips/linux/dupio.asm
+++ pwnlib/shellcraft/templates/mips/linux/dupio.asm
@@ -1,5 +1,6 @@
 <% from pwnlib.shellcraft import common %>
 <% from pwnlib.shellcraft.mips.linux import dup2 %>
+<% from pwnlib.shellcraft.mips import mov %>
 <%page args="sock = '$s0'"/>
 <%docstring>
 Args: [sock (imm/reg) = s0]
@@ -7,11 +8,13 @@ Args: [sock (imm/reg) = s0]
 </%docstring>
 <%
    dup       = common.label("dup")
+   looplabel = common.label("loop")
 %>
 
     /* dup() file descriptor ${sock} into stdin/stdout/stderr */
 ${dup}:
-    ${dup2(sock, 2)}
-    ${dup2(sock, 1)}
-    ${dup2(sock, 0)}
-
+    ${mov('$t0',2)}
+${looplabel}:
+    ${dup2(sock,'$t0')}
+    bgtz $t0,${looplabel}
+    addi $t0,-1
