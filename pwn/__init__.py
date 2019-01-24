ef8d51f5187ca5ae85338cebd58f3c4adce76434
Arusekk <arek_koz@o2.pl>
2019-01-24 10:44:21 +0100

    Limit the 32-bit complaint to python 2

--- pwn/__init__.py
+++ pwn/__init__.py
@@ -10,11 +10,11 @@ pwnlib.config.initialize()
 log = pwnlib.log.getLogger('pwnlib.exploit')
 args = pwnlib.args.args
 
-if not platform.architecture()[0].startswith('64'):
+if not platform.architecture()[0].startswith('64') and sys.version_info() < (3,):
     """Determines if the current Python interpreter is supported by Pwntools.
 
     See Gallopsled/pwntools#518 for more information."""
-    log.warn_once('Pwntools does not support 32-bit Python.  Use a 64-bit release.')
+    log.warn_once('Pwntools does not support 32-bit Python 2.  Use a 64-bit release or Python 3.')
 
 with context.local(log_console=sys.stderr):
     pwnlib.update.check_automatically()
