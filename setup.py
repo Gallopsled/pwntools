55dfe9b5e4df51987d173f85164bfbdc327abaad
Arusekk <arek_koz@o2.pl>
2019-11-05 19:53:20 +0100

    Begin working on 4.1.0

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     name                 = 'pwntools',
     python_requires      = '>=2.7',
     packages             = find_packages(),
-    version              = '4.0.0beta0',
+    version              = '4.1.0dev',
     data_files           = [('',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
