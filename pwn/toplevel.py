43ed66cf960b1d50764dbd0b10818e0415af68ef
Arusekk <arek_koz@o2.pl>
2019-12-08 16:02:34 +0100

    Actually run the tests + basic py3 fixes
    also rename the module to lower case to keep consistency

--- pwn/toplevel.py
+++ pwn/toplevel.py
@@ -29,7 +29,7 @@ from pwnlib.elf.elf import ELF, load
 from pwnlib.encoders import *
 from pwnlib.exception import PwnlibException
 from pwnlib.gdb import attach, debug, debug_assembly, debug_shellcode
-from pwnlib.FilePointer import *
+from pwnlib.filepointer import *
 from pwnlib.flag import *
 from pwnlib.fmtstr import FmtStr, fmtstr_payload
 from pwnlib.log import getLogger
