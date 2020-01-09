a50265eb303aadfe57c3bba432b19ea76580e40b
Arusekk <arek_koz@o2.pl>
2020-01-09 18:54:49 +0100

    Release 4.0.0

--- setup.py
+++ setup.py
@@ -82,7 +82,7 @@ setup(
     name                 = 'pwntools',
     python_requires      = '>=2.7',
     packages             = find_packages(),
-    version              = '4.0.0beta0',
+    version              = '4.0.0',
     data_files           = [('',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
