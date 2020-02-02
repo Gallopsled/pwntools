3d1f5f2ef45192d4fd21b60c9bc1d6f7eb69e8e7
Arusekk <arek_koz@o2.pl>
2020-02-02 23:22:31 +0100

    Actually enable caching for all of it

--- travis/install.sh
+++ travis/install.sh
@@ -172,7 +172,7 @@ setup_android_emulator()
 
     # In the future, it would be nice to be able to use snapshots.
     # However, I haven't gotten them to work nicely.
-    emulator -avd android-$ABI -no-window -no-boot-anim -no-skin -no-audio -no-window -no-snapshot &
+    emulator64 -avd android-$ABI -no-window -no-boot-anim -no-skin -no-audio -no-window -no-snapshot || kill -INT $$ &
     adb wait-for-device
     adb shell id
     adb shell getprop
