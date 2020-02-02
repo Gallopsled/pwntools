19b1fe78b9cff4b234b194ca6616f859fa344858
Zer1t0 <1933675-Zer1t0@users.noreply.gitlab.com>
2020-02-02 17:42:20 +0100

    added examples to all public methods of HeapExplorer

--- pwnlib/heap/heap_explorer.py
+++ pwnlib/heap/heap_explorer.py
@@ -96,14 +96,15 @@ class HeapExplorer:
     def arenas_count(self):
         """Returns the number of arenas
 
+        Returns:
+            int
+
         Examples:
             >>> p = process('sh')
             >>> hp = p.heap_explorer
-            >>> hp.arenas_count() # doctest: +SKIP
+            >>> number_of_arenas = hp.arenas_count()
+            >>> number_of_arenas # doctest: +SKIP
             2
-
-        Returns:
-            int
         """
         return len(self.all_arenas_malloc_states())
 
@@ -114,15 +115,21 @@ class HeapExplorer:
             arena_index (int, optional): The index of the desired arena. If none
                 is specified, then the index of the main arena will be selected
 
+        Returns:
+            :class:`MallocState`
+
         Examples:
             >>> p = process('sh')
             >>> hp = p.heap_explorer
             >>> ms = hp.malloc_state()
-            >>> hex(ms.top) # doctest: +SKIP
+            >>> ms_top = ms.top
+            >>> hex(ms_top) # doctest: +SKIP
             '0x55c4669ff6e0'
-            >>> hex(ms.address) # doctest: +SKIP
+            >>> ms_address = ms.address
+            >>> hex(ms_address) # doctest: +SKIP
             '0x7f97053fbc40'
-            >>> print(ms) # doctest: +SKIP
+            >>> ms_str = str(ms)
+            >>> print(ms_str) # doctest: +SKIP
             ======================== Malloc State (0x7f97053fbc40) ========================
             mutex = 0x0
             flags = 0x1
@@ -162,9 +169,6 @@ class HeapExplorer:
             system_mem = 0x21000
             max_system_mem = 0x21000
             ================================================================================
-
-        Returns:
-            :class:`MallocState`
         """
         return self.all_arenas_malloc_states()[arena_index]
 
@@ -173,6 +177,13 @@ class HeapExplorer:
 
         Returns:
             list of :class:`MallocState`
+
+        Examples:
+            >>> p = process('sh')
+            >>> hp = p.heap_explorer
+            >>> mmss = hp.all_arenas_malloc_states()
+            >>> mmss_str = "\\n".join([str(ms) for ms in mmss])
+            >>> print(mmss_str) # doctest: +SKIP
         """
         return self._malloc_state_parser.parse_all_from_main_malloc_state_address(
             self._main_arena_address
@@ -185,15 +196,19 @@ class HeapExplorer:
             arena_index (int, optional): The index of the desired arena. If none
                 is specified, then the index of the main arena will be selected
 
+        Returns:
+            :class:`Heap`
 
         Examples:
             >>> p = process('sh')
             >>> hp = p.heap_explorer
             >>> heap = hp.heap()
-            >>> len(heap.chunks) # doctest: +SKIP
+            >>> number_chunks = len(heap.chunks)
+            >>> len(number_chunks) # doctest: +SKIP
             574
             >>> top_chunk = heap.top
-            >>> hex(top_chunk.address) # doctest: +SKIP
+            >>> top_chunk_address = top_chunk.address
+            >>> hex(top_chunk_address) # doctest: +SKIP
             '0x56080f5dbb00'
             >>> heap_str = str(heap)
             >>> print(heap_str) # doctest: +SKIP
@@ -241,9 +256,6 @@ class HeapExplorer:
             0x5556351026e0 0x1e920 PREV_IN_USE
               00 00 00 00 00 00 00 00 00 00 00 00 00 00 00 00   ................
             ================================================================================
-
-        Returns:
-            :class:`Heap`
         """
         malloc_state = self.malloc_state(arena_index)
         return self._heap_parser.parse_from_malloc_state(malloc_state)
@@ -252,7 +264,14 @@ class HeapExplorer:
         """Returns the heaps of all arenas
 
         Returns:
-            list of :class:`Heap`
+            :obj:`list` of :class:`Heap`
+
+        Example:
+            >>> p = process('sh')
+            >>> hp = p.heap_explorer
+            >>> hhpp = hp.all_arenas_heaps()
+            >>> hhpp_str = "\\n".join([str(h) for h in hhpp])
+            >>> print(hhpp_str) # doctest: +SKIP
         """
         malloc_states = self.all_arenas_malloc_states()
         heaps = []
@@ -269,19 +288,20 @@ class HeapExplorer:
             arena_index (int, optional): The index of the desired arena. If none
                 is specified, then the index of the main arena will be selected
 
+        Returns:
+            :class:`UnsortedBins`
+
         Examples:
 
             >>> p = process('sh')
             >>> hp = p.heap_explorer
             >>> unsorted_bins = hp.unsorted_bin()
-            >>> print(unsorted_bins) # doctest: +SKIP
+            >>> unsorted_bins_str = str(unsorted_bins)
+            >>> print(unsorted_bins_str) # doctest: +SKIP
             ================================ Unsorted Bins ================================
             [0] Unsorted Bin (2) => Chunk(0x555635101d40 0x910 PREV_IN_USE) => Chunk(0x55563
             5100ca0 0x1010 PREV_IN_USE) => 0x7f8bd66e9ca0
             ================================================================================
-
-        Returns:
-            :class:`UnsortedBins`
         """
         malloc_state = self.malloc_state(arena_index)
         return self._bin_parser.parse_unsorted_bin_from_malloc_state(
@@ -293,6 +313,13 @@ class HeapExplorer:
 
         Returns:
             list of :class:`UnsortedBins`
+
+        Example:
+            >>> p = process('sh')
+            >>> hp = p.heap_explorer
+            >>> uubb = hp.all_arenas_unsorted_bins()
+            >>> uubb_str = "\\n".join([str(ub) for ub in uubb])
+            >>> print(uubb_str) # doctest: +SKIP
         """
         unsorted_bins = []
         for malloc_state in self.all_arenas_malloc_states():
@@ -310,6 +337,9 @@ class HeapExplorer:
             arena_index (int, optional): The index of the desired arena. If none
                 is specified, then the index of the main arena will be selected
 
+        Returns:
+            :class:`SmallBins`
+
         Examples:
             >>> p = process('sh')
             >>> hp = p.heap_explorer
@@ -318,10 +348,6 @@ class HeapExplorer:
             ================================== Small Bins ==================================
                 [-] No chunks found
             ================================================================================
-
-
-        Returns:
-            :class:`SmallBins`
         """
         malloc_state = self.malloc_state(arena_index)
         return self._bin_parser.parse_small_bins_from_malloc_state(
@@ -333,6 +359,13 @@ class HeapExplorer:
 
         Returns:
             list of :class:`SmallBins`
+
+        Example:
+            >>> p = process('sh')
+            >>> hp = p.heap_explorer
+            >>> bb = hp.all_arenas_small_bins()
+            >>> bb_str = "\\n".join([str(b) for b in bb])
+            >>> print(bb_str) # doctest: +SKIP
         """
         all_small_bins = []
         for malloc_state in self.all_arenas_malloc_states():
@@ -350,6 +383,9 @@ class HeapExplorer:
             arena_index (int, optional): The index of the desired arena. If none
                 is specified, then the index of the main arena will be selected
 
+        Returns:
+            :class:`LargeBins`
+
         Examples:
             >>> p = process('sh')
             >>> hp = p.heap_explorer
@@ -358,10 +394,6 @@ class HeapExplorer:
             ================================== Large Bins ==================================
                 [-] No chunks found
             ================================================================================
-
-
-        Returns:
-            :class:`LargeBins`
         """
         malloc_state = self.malloc_state(arena_index)
         return self._bin_parser.parse_large_bins_from_malloc_state(
@@ -372,7 +404,15 @@ class HeapExplorer:
         """Returns the large bins of all arenas
 
         Returns:
-            list of :class:`LargeBins`
+            :obj:`list` of :class:`LargeBins`
+
+        Example:
+            >>> p = process('sh')
+            >>> hp = p.heap_explorer
+            >>> bb = hp.all_arenas_large_bins()
+            >>> bb_str = "\\n".join([str(b) for b in bb])
+            >>> print(bb_str) # doctest: +SKIP
+
         """
         all_large_bins = []
         for malloc_state in self.all_arenas_malloc_states():
@@ -390,6 +430,9 @@ class HeapExplorer:
             arena_index (int, optional): The index of the desired arena. If none
                 is specified, then the index of the main arena will be selected
 
+        Returns:
+            :class:`FastBins`
+
         Examples:
             >>> p = process('sh')
             >>> hp = p.heap_explorer
@@ -399,9 +442,6 @@ class HeapExplorer:
             [4] Fast Bin 0x60 (2) => Chunk(0x555635100c20 0x60 PREV_IN_USE) => Chunk(0x55563
             5100ba0 0x60 PREV_IN_USE) => 0x0
             ================================================================================
-
-        Returns:
-            :class:`FastBins`
         """
         malloc_state = self.malloc_state(arena_index)
         return self._fast_bin_parser.parse_all_from_malloc_state(malloc_state)
@@ -410,7 +450,14 @@ class HeapExplorer:
         """Returns the small bins of all arenas
 
         Returns:
-            list of :class:`FastBin`
+            :obj:`list` of :class:`FastBins`
+
+        Example:
+            >>> p = process('sh')
+            >>> hp = p.heap_explorer
+            >>> bb = hp.all_arenas_fast_bins()
+            >>> bb_str = "\\n".join([str(b) for b in bb])
+            >>> print(bb_str) # doctest: +SKIP
         """
         malloc_states = self.all_arenas_malloc_states()
         all_fast_bins = []
@@ -427,21 +474,22 @@ class HeapExplorer:
             arena_index (int, optional): The index of the desired arena. If none
                 is specified, then the index of the main arena will be selected
 
+        Returns:
+            :class:`Tcaches`
+
         >>> p = process('sh')
         >>> hp = p.heap_explorer
-        >>> try: # doctest: +SKIP
-        ...     print(hp.tcaches())
+        >>> try:
+        ...     tcaches_str = str(hp.tcaches())
         ... except NoTcacheError:
-        ...     pass
+        ...     tcaches_str = ""
         ...
+        >>> print(tcaches_str) # doctest: +SKIP
         =================================== Tcaches ===================================
         [23] Tcache 0x188 (1) => Chunk(0x56383e3c0250 0x190 PREV_IN_USE) => 0x0
         [41] Tcache 0x2a8 (1) => Chunk(0x56383e3bffa0 0x2b0 PREV_IN_USE) => 0x0
         ================================================================================
 
-
-        Returns:
-            :class:`Tcache`
         """
         if not self.tcaches_enabled:
             raise NoTcacheError()
@@ -453,7 +501,18 @@ class HeapExplorer:
         """Returns the tcaches of all arenas
 
         Returns:
-            list of :class:`Tcache`
+            :obj:`list` of :class:`Tcaches`
+
+        Example:
+            >>> p = process('sh')
+            >>> hp = p.heap_explorer
+            >>> try:
+            ...     ttcc = hp.all_arenas_tcaches()
+            ...     tcaches_str = "\\n".join([str(tc) for tc in ttcc])
+            ... except NoTcacheError:
+            ...     tcaches_str = ""
+            ...
+            >>> print(tcaches_str) # doctest: +SKIP
         """
         if not self.tcaches_enabled:
             raise NoTcacheError()
@@ -472,6 +531,9 @@ class HeapExplorer:
         Args:
             arena_index (int, optional): The index of the desired arena. If none
                 is specified, then the index of the main arena will be selected
+
+        Returns:
+            :class:`Arena`
 
         Examples:
             >>> p = process('sh')
@@ -585,10 +647,6 @@ class HeapExplorer:
             ================================================================================
             ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-
-
-        Returns:
-            :class:`Arena`
         """
         malloc_state = self.malloc_state(arena_index)
         return self._arena_parser.parse_from_malloc_state(malloc_state)
@@ -597,7 +655,14 @@ class HeapExplorer:
         """Returns all arenas
 
         Returns:
-            list of :class:`Arena`
+            :obj:`list` of :class:`Arena`
+
+        Example:
+            >>> p = process('sh')
+            >>> hp = p.heap_explorer
+            >>> arenas = hp.all_arenas()
+            >>> arenas_str = "\\n".join([str(a) for a in arenas])
+            >>> print(arenas_str) # doctest: +SKIP
         """
         return self._arena_parser.parse_all_from_main_malloc_state_address(
             self._main_arena_address
