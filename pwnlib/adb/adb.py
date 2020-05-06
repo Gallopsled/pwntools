5978c8c04763efbe9be0a61f6fcd1813527bb584
Arusekk <arek_koz@o2.pl>
2020-05-07 00:24:43 +0200

    More ADB

--- pwnlib/adb/adb.py
+++ pwnlib/adb/adb.py
@@ -117,7 +117,7 @@ def current_device(any=False):
 
         >>> device = adb.current_device(any=True)
         >>> device
-        AdbDevice(serial='emulator-5554', type='device', port='emulator', product='sdk_google_phone_armv7', model='sdk phone armv7', device='generic')
+        AdbDevice(serial='emulator-5554', type='device', port='emulator', product='sdk_google_phone_armv7', model='sdk google phone armv7', device='generic')
         >>> device.port
         'emulator'
     """
