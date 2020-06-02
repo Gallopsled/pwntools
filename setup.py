163c19dd49cc75859398f8a2685e118587ca07be
Arusekk <arek_koz@o2.pl>
2020-06-03 00:57:28 +0200

    Freeze unicorn (see #1538)

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ install_requires     = ['paramiko>=1.15.2',
                         'psutil>=3.3.0',
                         'intervaltree>=3.0',
                         'sortedcontainers',
-                        'unicorn>=1.0.2rc1', # see unicorn-engine/unicorn#1100, unicorn-engine/unicorn#1170
+                        'unicorn>=1.0.2rc1,<1.0.2rc4', # see unicorn-engine/unicorn#1100, unicorn-engine/unicorn#1170, Gallopsled/pwntools#1538
 ]
 
 # Check that the user has installed the Python development headers
