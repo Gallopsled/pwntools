da439f329305ddb481a4131c4595e705933ab4c5
heapcrash <heapcrash@gmail.com>
2020-06-05 14:36:11 -0500

    Add six >= 1.12.0 to requirements

--- setup.py
+++ setup.py
@@ -61,6 +61,7 @@ install_requires     = ['paramiko>=1.15.2',
                         'intervaltree>=3.0',
                         'sortedcontainers',
                         'unicorn>=1.0.2rc1,<1.0.2rc4', # see unicorn-engine/unicorn#1100, unicorn-engine/unicorn#1170, Gallopsled/pwntools#1538
+                        'six>=1.12.0',
 ]
 
 # Check that the user has installed the Python development headers
