aafbdd3284846d78e6e12e7493dbf4eaa5e1595b
Saullo Carvalho <saullocarvalho@gmail.com>
2020-06-14 10:55:25 -0700

    Add doctest to ROP.jmp_rsp

--- pwnlib/rop/rop.py
+++ pwnlib/rop/rop.py
@@ -88,6 +88,14 @@ You can also append complex arguments onto stack when the stack pointer is known
     0x7fffe038:       b'-c\x00$'
     0x7fffe03c:       b'ls\x00$'
 
+ROP also detects 'jmp $sp' gadget to help exploit binaries with NX disabled.
+
+    >>> context.clear(arch='amd64')
+    >>> elf = ELF.from_assembly('nop; mov rax, 0x10; jmp rsp; ret')
+    >>> rop = ROP(elf)
+    >>> jmp_gadget = rop.jmp_rsp
+    >>> elf.read(jmp_gadget.address,2) == asm('jmp rsp')
+    True
 
 ROP Example
 -------------------
