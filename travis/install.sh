2c3bd4b9a6c77852b90ed1f65ca637daab624370
heapcrash <heapcrash@gmail.com>
2020-06-13 19:44:15 -0500

    Remove unused CI setup code

--- travis/install.sh
+++ travis/install.sh
@@ -19,51 +19,6 @@ install_deb()
     local_deb_extract "$URL"
 }
 
-setup_travis()
-{
-    export PATH=$PWD/usr/bin:$PATH
-    export LD_LIBRARY_PATH=$PWD/usr/lib:$LD_LIBRARY_PATH
-    export LD_LIBRARY_PATH=$PWD/usr/lib/x86_64-linux-gnu:$LD_LIBRARY_PATH
-
-    # Install a more modern binutils, which is required for some of the tests
-    [[ -f usr/bin/objcopy ]] || install_deb binutils
-
-    # Install/upgrade qemu
-    [[ -f usr/bin/qemu-arm-static ]] || install_deb qemu-user-static xenial
-
-    # Install cross-binutils
-    [[ -f usr/bin/x86_64-linux-gnu-ar ]]    || install_deb binutils-multiarch
-    [[ -f usr/bin/aarch64-linux-gnu-as ]]   || install_deb binutils-aarch64-linux-gnu
-    [[ -f usr/bin/arm-linux-gnueabihf-as ]] || install_deb binutils-arm-linux-gnueabihf
-    [[ -f usr/bin/mips-linux-gnu-as ]]      || install_deb binutils-mips-linux-gnu
-    [[ -f usr/bin/powerpc-linux-gnu-as ]]   || install_deb binutils-powerpc-linux-gnu
-
-    # Test that the installs worked
-    as                      --version
-    x86_64-linux-gnu-ar     --version
-    aarch64-linux-gnu-as    --version
-    arm-linux-gnueabihf-as  --version
-    mips-linux-gnu-as       --version
-    powerpc-linux-gnu-as    --version
-    qemu-arm-static         --version
-
-    mips-linux-gnu-ld       --version
-
-    # Force-install capstone because it's broken somehow
-    [[ -f usr/lib/libcapstone.so.3 ]] || install_deb libcapstone3
-
-    # Install a newer copy of GDB
-    if [[ ! -f usr/bin/gdb ]]; then
-        git clone --depth=1 https://github.com/zachriggle/pwntools-gdb-travis-ci.git
-        tar xf pwntools-gdb-travis-ci/gdb.tar.xz
-        which gdb
-        usr/bin/gdb --version
-    fi
-
-    # Get rid of files we don't want cached
-    rm -rf usr/share
-}
-
 setup_ipv6()
 {
     echo 0 | sudo tee /proc/sys/net/ipv6/conf/all/disable_ipv6
@@ -190,15 +145,9 @@ setup_osx()
 }
 
 if [[ "$USER" == "travis" ]]; then
-#   setup_travis
     setup_ipv6
     setup_gdbserver
     setup_android_emulator
-elif [[ "$USER" == "shippable" ]]; then
-    sudo apt-get update
-    sudo apt-get install openssh-server gcc-multilib
-    sudo /usr/sbin/sshd -f /etc/ssh/sshd_config &
-    setup_travis
 elif [[ "$(uname)" == "Darwin" ]]; then
     setup_osx
 elif [[ "$(uname)" == "Linux" ]]; then
