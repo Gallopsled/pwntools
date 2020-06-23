de7e5aea09d9d39a7163ea1bdd0ad2497fd3b117
heapcrash <heapcrash@gmail.com>
2020-06-22 22:36:17 -0500

    [docker] Get Travis CI dockerfile working more

--- travis/docker/run.sh
+++ travis/docker/run.sh
@@ -1,5 +1,10 @@
 #!/usr/bin/env bash
+
+# We have to explicitly start the ssh service
 sudo service ssh start
+
+# Enable the IPv6 interface
+echo 0 | sudo tee /proc/sys/net/ipv6/conf/all/disable_ipv6
 
 case "$ANDROID" in
     [Yy]* )
@@ -17,4 +22,4 @@ case "$ANDROID" in
         ;;
 esac
 
-PWNLIB_NOTERM=1 coverage run -m sphinx -b doctest docs/source docs/build/doctest $TARGET
+PWNLIB_NOTERM=1 coverage3 run -m sphinx -b doctest docs/source docs/build/doctest $TARGET
