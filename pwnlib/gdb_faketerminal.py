4c385f341e2a531d85c7e33379ef9a78d0643f13
heapcrash <heapcrash@gmail.com>
2020-06-22 23:20:59 -0500

    Second try

--- pwnlib/gdb_faketerminal.py
+++ pwnlib/gdb_faketerminal.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python
-import os
-arg = 'exec ' + argv[1] + ' -ex detach -batch'
-args = ['bash', '-c', arg]
-sys.stderr.write(repr(args))
-os.execve('/bin/bash', args, os.environ)
+from pwnlib.tubes.process import process
+from time import sleep
+from sys import argv
+sleep(1)
+
+# Launch GDB under bash
+sh = process(argv[1], shell=True)
+
+# Sleep for long enough for pwntools to find out that the debugger has attached
+sleep(5)
+
+# Detach from the debuggee after the script runs
+sh.sendline('detach')
+sh.sendline('quit')
+sh.recvall()
