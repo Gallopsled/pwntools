c609e85e6a1e2c8357e4d514e6ac0267e7816356
heapcrash <heapcrash@gmail.com>
2020-06-22 23:12:22 -0500

    [gdb] Optimize faketerm

--- pwnlib/gdb_faketerminal.py
+++ pwnlib/gdb_faketerminal.py
@@ -1,14 +1,6 @@
 #!/usr/bin/env python
-from pwnlib.tubes.process import process
-from time import sleep
-from sys import argv
-sleep(1)
-if len(argv) == 2:
-	sh = process(argv[1], shell=True)
-else:
-	sh = process(argv[1:])
-sh.sendline('set prompt (gdb)')
-res = sh.sendlineafter('(gdb)', 'c')
-while b'The program is not being run.' not in res:
-    res = sh.sendlineafter('(gdb)', 'c')
-sh.close()
+import os
+arg = 'exec ' + argv[1] + ' -ex detach -batch'
+args = ['bash', '-c', arg]
+sys.stderr.write(repr(args))
+os.execve('/bin/bash', args, os.environ)
