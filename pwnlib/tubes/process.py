Gallopsled/pwntools

commit 230a42ffc0b63148b82b5c6c270d211f8ad83507

OSError is not subscriptable (#1608)

pwnlib/tubes/process.py
@@ -752,7 +752,7 @@ class process(tube):
             # ValueError: I/O operation on closed file
             raise EOFError
         except select.error as v:
-            if v[0] == errno.EINTR:
+            if v.errno == errno.EINTR:
                 return False
 
     def connected_raw(self, direction):

commit 7b7dd4d8e02912521395b6532154c91ec82b27cb

Fix python2 regression from #1608
Closes #1627

pwnlib/tubes/process.py
@@ -752,7 +752,7 @@ class process(tube):
             # ValueError: I/O operation on closed file
             raise EOFError
         except select.error as v:
-            if v.errno == errno.EINTR:
+            if v.args[0] == errno.EINTR:
                 return False
 
     def connected_raw(self, direction):