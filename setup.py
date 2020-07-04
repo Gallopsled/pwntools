2e61a6be7f7aaba5d240f545864bc912449adff8
heapcrash <heapcrash@gmail.com>
2020-07-03 19:10:40 -0500

    [release] 4.2.0 is now "stable"

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     name                 = 'pwntools',
     python_requires      = '>=2.7',
     packages             = find_packages(),
-    version              = '4.2.0beta0',
+    version              = '4.2.0',
     data_files           = [('',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
