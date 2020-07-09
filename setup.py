d3614f14a932e8b8241343c934b646b68c95b4dc
Arusekk <arek_koz@o2.pl>
2020-07-09 12:20:42 +0200

    Sync CHANGELOG from dev + version bump

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     name                 = 'pwntools',
     python_requires      = '>=2.7',
     packages             = find_packages(),
-    version              = '4.2.0',
+    version              = '4.2.1',
     data_files           = [('',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
