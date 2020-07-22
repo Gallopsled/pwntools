556685e150f95afb1d829f8d4e6a6ad96152cadb
Heap Crash <66139157+heapcrash@users.noreply.github.com>
2020-07-22 01:39:04 -0500

    [ci] Disable pwnlib.ui tests for branch dev (#1647)
    These still run for all other branches and tags

--- pwnlib/ui.py
+++ pwnlib/ui.py
@@ -58,6 +58,10 @@ def yesno(prompt, default=None):
       `True` if the answer was "yes", `False` if "no"
 
     Examples:
+
+    .. doctest::
+       :skipif: branch_dev
+
         >>> yesno("A number:", 20)
         Traceback (most recent call last):
         ...
@@ -76,6 +80,10 @@ def yesno(prompt, default=None):
          [?] is it good 3 [yes/No] False
 
     Tests:
+
+    .. doctest::
+       :skipif: branch_dev
+
         >>> p = testpwnproc("print(yesno('is it ok??'))")
         >>> b"is it ok" in p.recvuntil("??")
         True
@@ -137,12 +145,20 @@ def options(prompt, opts, default = None):
       The users choice in the form of an integer.
 
     Examples:
+
+    .. doctest::
+       :skipif: branch_dev
+
         >>> options("Select a color", ("red", "green", "blue"), "green")
         Traceback (most recent call last):
         ...
         ValueError: options(): default must be a number or None
 
     Tests:
+
+    .. doctest::
+       :skipif: branch_dev
+
         >>> p = testpwnproc("print(options('select a color', ('red', 'green', 'blue')))")
         >>> p.sendline(b"\33[C\33[A\33[A\33[B\33[1;5A\33[1;5B 0310")
         >>> _ = p.recvall()
@@ -253,6 +269,10 @@ def pause(n=None):
     r"""Waits for either user input or a specific number of seconds.
 
     Examples:
+
+    .. doctest::
+       :skipif: branch_dev
+
         >>> with context.local(log_level="INFO"):
         ...     pause(1)
         [x] Waiting
@@ -264,6 +284,10 @@ def pause(n=None):
         ValueError: pause(): n must be a number or None
 
     Tests:
+
+    .. doctest::
+       :skipif: branch_dev
+
         >>> saved_stdin = sys.stdin
         >>> try:
         ...     sys.stdin = io.TextIOWrapper(io.BytesIO(b"\n"))
@@ -310,6 +334,9 @@ def more(text):
 
     Tests:
 
+    .. doctest::
+       :skipif: branch_dev
+       
         >>> more("text")
         text
         >>> p = testpwnproc("more('text\\n' * (term.height + 2))")
