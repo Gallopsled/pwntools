62d2540d3183da319c8a0edabbcce64c40775ca1
TwoUnderscorez <ron.yutkin@gmail.com>
2020-08-15 22:11:27 +0300

    Add doctests

--- docs/source/ascii_shellcode.rst
+++ docs/source/ascii_shellcode.rst
@@ -7,9 +7,10 @@
    from typing import Sequence
    from typing import Tuple
    from typing import Union
-   from more_itertools import grouper
+   from pwnlib.util.iters import group
    from pwnlib.context import LocalContext
    from pwnlib.context import context
+   from pwnlib.ascii_shellcode import asciify_shellcode, _get_allocator, _find_negatives, _get_subtractions, _calc_subtractions
 
 :mod:`pwnlib.ascii_shellcode` --- ASCII Shell Code
 ==================================================
--- pwnlib/ascii_shellcode.py
+++ pwnlib/ascii_shellcode.py
@@ -13,8 +13,6 @@ from pwnlib.util.iters import group
 from pwnlib.context import LocalContext
 from pwnlib.context import context
 
-__all__ = ['asciify_shellcode']
-
 
 @LocalContext
 def asciify_shellcode(shellcode: bytes, slop: int, vocab: bytes = None) -> bytes:
@@ -31,6 +29,12 @@ def asciify_shellcode(shellcode: bytes, slop: int, vocab: bytes = None) -> bytes
 
     Returns:
         bytes: The packed shellcode
+
+    Examples:
+
+        >>> sc = bytes.fromhex('83c41831c031dbb006cd8053682f747479682f64657689e331c966b91227b005cd806a175831dbcd806a2e5853cd8031c050682f2f7368682f62696e89e3505389e199b00bcd80')
+        >>> print(asciify_shellcode(sc, 300, arch='i386').hex())
+        54582d212121212d355f60602d7e7e7e7e505c252121212125404040402d213621212d567e21212d7e7e3c2d502d216d68612d617e7e7e502d21214c602d615e7e7e2d7e7e7e7e502d212169662d39607e7e502d212121212d614f61662d7e7e7e7e502d2126213c2d217e607e2d2d7e7e7e502d212121212d2121485e2d2b417e7e502d5521215b2d7e41317e502d2c3c56212d7e7e7e212d7e7e7e47502d213221382d6a7e4f7e502d215d21212d217e21722d797e777e502d632121212d7e3c282b502d4e215f572d7e317e7e502d21215d212d4d6e7e212d7e7e7e3c502d213c21212d727e21502d7e7e787e502d666521242d7e7e537e2d7e7e7e7e502d212127242d257a7e7e502d412121212d7e2123212d7e2a7e3d502d213721212d547e21212d7e7e455e505050
     """
     if not vocab:
         vocab = b"!\"#$%&'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~"
@@ -38,12 +42,12 @@ def asciify_shellcode(shellcode: bytes, slop: int, vocab: bytes = None) -> bytes
         required_chars = b'\\-%TXP'
         if not all((c in required_chars) for c in vocab):
             raise RuntimeError(
-                f"These characters ({str(required_chars, 'ascii')}) are required because they assemble into instructions used to unpack the shellcode")
+                "These characters ({}) are required because they assemble into instructions used to unpack the shellcode".format(str(required_chars, 'ascii')))
 
     if context.arch != 'i386' and context.bits != 32:
         raise RuntimeError('Only 32-bit i386 is currently supported')
 
-    int_size = context.bits / 8
+    int_size = context.bits // 8
 
     # Prepend with NOPs for the NOP sled
     shellcode = b'\x90'*int_size + shellcode
@@ -68,9 +72,11 @@ def _get_allocator(slop: int, vocab: bytes) -> bytes:
 
     Examples:
 
-        >>> assert False
+        >>> vocab = bytes.fromhex('2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e')
+        >>> print(_get_allocator(300, vocab, arch='i386').hex())
+        54582d212121212d355f60602d7e7e7e7e505c25212121212540404040
     """
-    int_size = context.bits / 8
+    int_size = context.bits // 8
     # Use eax for subtractions because sub esp, X doesn't assemble to ascii
     result = b'TX'  # push esp; pop eax
     # Set target to the `slop` arg
@@ -80,7 +86,7 @@ def _get_allocator(slop: int, vocab: bytes) -> bytes:
     # That's why the `last` parameter for `calc_subtractions` can just be zero
     for subtraction in _calc_subtractions(b'\x00'*int_size, target, vocab):
         # sub eax, subtraction
-        result += struct.pack(f'=c{int_size}s', b'-', subtraction)
+        result += struct.pack('=c{}s'.format(int_size), b'-', subtraction)
     result += b'P\\'  # push eax, pop esp
     # Zero out eax for the unpacking part
     pos, neg = _find_negatives(vocab)
@@ -103,9 +109,12 @@ def _find_negatives(vocab: bytes) -> Tuple[int, int]:
 
     Examples:
 
-        >>> assert False
+        >>> vocab = bytes.fromhex('2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e')
+        >>> a, b = _find_negatives(vocab, arch='i386')
+        >>> a & b
+        0
     """
-    int_size = context.bits / 8
+    int_size = context.bits // 8
     for products in product(*[vocab for _ in range(2)]):
         if products[0] & products[1] == 0:
             return tuple(int.from_bytes(x.to_bytes(1, 'little')*int_size, 'little') for x in products)
@@ -131,21 +140,24 @@ def _get_subtractions(
 
     Examples:
 
-        >>> assert False
+        >>> sc = bytes.fromhex('414243444546474849474b4c4d4e4f5051525354555658595a')
+        >>> vocab = bytes.fromhex('2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e')
+        >>> print(_get_subtractions(sc, vocab, arch='i386').hex())
+        2d282121212d7e4e4e4e502d213d3b3a2d667e7e7e2d7e7e7e7e502d212121212d656465652d7e7e7e7e502d212121212d656464642d7e7e7e7e502d212121212d656764642d7e7e7e7e502d212121212d656164642d7e7e7e7e502d212121212d656464642d7e7e7e7e50
     """
-    int_size = context.bits / 8
+    int_size = context.bits // 8
     result = bytes()
     last = b'\x00'*int_size
     # Group the shellcode into bytes of stack cell size, pad with NOPs
     # if the shellcode does not divide into stack cell size and reverse.
     # The shellcode will be reversed again back to it's original order once
     # it's pushed onto the stack
-    sc = tuple(group(shellcode, int_size, 0x90))[::-1]
+    sc = tuple(group(int_size, shellcode, 0x90))[::-1]
     # Pack the shellcode to a sub/push sequence
     for x in [bytes(y) for y in sc]:
         for subtraction in _calc_subtractions(last, x, vocab):
             # sub eax, `subtraction`
-            result += struct.pack(f'=c{int_size}s', b'-', subtraction)
+            result += struct.pack('=c{}s'.format(int_size), b'-', subtraction)
         last = x
         result += b'P'  # push eax
     return result
@@ -172,9 +184,13 @@ def _calc_subtractions(
 
     Examples:
 
-        >>> assert False
+        >>> vocab = bytes.fromhex('2122232425262728292a2b2c2d2e2f303132333435363738393a3b3c3d3e3f404142434445464748494a4b4c4d4e4f505152535455565758595a5b5c5d5e5f606162636465666768696a6b6c6d6e6f707172737475767778797a7b7c7d7e')
+        >>> print(_calc_subtractions(b'\x10'*4, b'\x11'*4, vocab))
+        [bytearray(b'!!!!'), bytearray(b'`___'), bytearray(b'~~~~')]
+        >>> print(_calc_subtractions(b'\x11\x12\x13\x14', b'\x15\x16\x17\x18', vocab))
+        [bytearray(b'~}}}'), bytearray(b'~~~~')]
     """
-    int_size = context.bits / 8
+    int_size = context.bits // 8
     subtractions = [bytearray(b'\x00'*int_size)]
     for subtraction in range(max_subs):
         carry = success_count = 0
@@ -204,4 +220,4 @@ def _calc_subtractions(
             subtractions.append(bytearray(b'\x00'*int_size))
     else:
         raise ArithmeticError(
-            f'Could not find the correct subtraction sequence to get the the desired target ({target[byte]}) from ({last[byte]})')
+            str.format('Could not find the correct subtraction sequence to get the the desired target ({}) from ({})', target[byte], last[byte]))
