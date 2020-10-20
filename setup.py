447aa551f6f7e1263a77245754717c91ef27b18d
Arusekk <arek_koz@o2.pl>
2020-10-20 17:57:56 +0200

    Begin working on 4.5.0

--- setup.py
+++ setup.py
@@ -83,7 +83,7 @@ setup(
     name                 = 'pwntools',
     python_requires      = '>=2.7',
     packages             = find_packages(),
-    version              = '4.4.0beta0',
+    version              = '4.5.0dev',
     data_files           = [('',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
