47b20a3c9d6c4fe94adfdca5feacb7c1119053cd
Arusekk <arek_koz@o2.pl>
2020-12-02 21:56:10 +0100

    Minimal fix in encoders
    This must have been somehow overlooked before.

--- pwnlib/encoders/i386/xor.py
+++ pwnlib/encoders/i386/xor.py
@@ -65,7 +65,7 @@ end:
 
     def __call__(self, raw_bytes, avoid, pcreg=''):
         while len(raw_bytes) % context.bytes:
-            raw_bytes += '\x00'
+            raw_bytes += b'\x00'
 
         a, b = xor_pair(raw_bytes, avoid)
 
