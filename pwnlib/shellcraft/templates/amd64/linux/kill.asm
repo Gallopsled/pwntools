21e51949fd57caa743f3dd2ff189cc62b9cfd6cd
r0bre <robert.reith@t-online.de>
2020-12-07 18:39:13 +0100

    fixed wrong description for shellcraft.amd64.linux.kill(). new desciption copied from pwntools/pwnlib/shellcraft/templates/common/linux/syscalls/kill.asm

--- pwnlib/shellcraft/templates/amd64/linux/kill.asm
+++ pwnlib/shellcraft/templates/amd64/linux/kill.asm
@@ -1,5 +1,16 @@
 <% from pwnlib.shellcraft import amd64 %>
 <%page args="pid, signal = 'SIGKILL'"/>
-<%docstring>Writes a string to a file descriptor</%docstring>
+<%docstring>kill(pid, sig) -> str
+
+Invokes the syscall kill.
+
+See 'man 2 kill' for more information.
+
+Arguments:
+    pid(pid_t): pid
+    sig(int): sig
+Returns:
+    int
+</%docstring>
 
     ${amd64.linux.syscall('SYS_kill', pid, signal)}
