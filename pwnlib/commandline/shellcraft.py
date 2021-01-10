a061fbc40d20163e58d058b867e4679eb944707c
152334H <54623771+152334H@users.noreply.github.com>
2021-01-10 22:35:48 +0800

    shut up pylint

--- pwnlib/commandline/shellcraft.py
+++ pwnlib/commandline/shellcraft.py
@@ -295,7 +295,7 @@ def main(args):
 
     vma = args.address
     if vma:
-        vma = util.safeeval.expr(vma)
+        vma = pwnlib.util.safeeval.expr(vma)
 
     if args.format in ['e','elf']:
         args.format = 'default'
