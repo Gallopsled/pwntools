b7b9256338628aeb5a0204c7711e14b59cb97926
heapcrash <heapcrash@gmail.com>
2021-02-17 16:33:35 -0600

    [mips] Do not replace "asm" in the global scope with doctest

--- pwnlib/shellcraft/templates/mips/linux/cat.asm
+++ pwnlib/shellcraft/templates/mips/linux/cat.asm
@@ -11,8 +11,8 @@ Example:
 
     >>> f = tempfile.mktemp()
     >>> write(f, 'FLAG')
-    >>> asm  = shellcraft.mips.linux.cat(f)
-    >>> asm += shellcraft.mips.linux.exit(0)
+    >>> sc = shellcraft.mips.linux.cat(f)
+    >>> sc += shellcraft.mips.linux.exit(0)
     >>> run_assembly(asm).recvall()
     b'FLAG'
 
