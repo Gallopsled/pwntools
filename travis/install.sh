294e8f2ed833caf2ea6cacb2791a6495abf5d165
heapcrash <heapcrash@gmail.com>
2021-02-25 17:01:38 -0500

    [travis] Switch to a different server that still has the magic gdbserver

--- travis/install.sh
+++ travis/install.sh
@@ -72,7 +72,8 @@ setup_ipv6()
 setup_gdbserver()
 {
     # https://docs.improbable.io/reference/14.3/shared/debug-cloud-workers#common-issues
-    wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdb/gdbserver_8.3-0ubuntu1_amd64.deb
+    # wget http://archive.ubuntu.com/ubuntu/pool/main/g/gdb/gdbserver_8.3-0ubuntu1_amd64.deb
+    wget https://launchpad.net/ubuntu/+source/gdb/8.3-0ubuntu1/+build/16807407/+files/gdbserver_8.3-0ubuntu1_amd64.deb
     sudo apt-get install ./gdbserver_8.3-0ubuntu1_amd64.deb
 }
 
