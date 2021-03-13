10c362436d65ce3f9212231da545210014ff74bd
Eloy Pérez <zer1t0ps@protonmail.com>
2021-03-13 13:37:31 +0100

    use #: to document malloc state bins attributes

--- pwnlib/heap/malloc_state/bins/bins.py
+++ pwnlib/heap/malloc_state/bins/bins.py
@@ -4,39 +4,30 @@ from pwnlib.heap.malloc_state.bins.bins_indexes import \
 
 class Bins:
     """Class to represent the `bins` attribute of malloc_state struct.
-
-    Attributes:
-        entries (list of :class:`BinEntry`): entries with pointers to the first
-            and last chunks of the each double linked bin.
     """
 
     def __init__(self, entries):
+        #: :class:`list` of :class:`BinEntry`: Entries with pointers to the first
+        #: and last chunks of the each double linked bin.
         self.entries = entries
 
     @property
     def unsorted_bin_entry(self):
-        """Returns the entry of the unsorted bin.
-
-        Returns:
-            BinEntry
+        """:class:`BinEntry`: Returns the entry of the unsorted bin.
         """
         return self.entries[UNSORTED_BIN_INDEX]
 
     @property
     def small_bins_entries(self):
-        """Returns the entries of the small bins.
-
-        Returns:
-            :obj:`list` of  :class:`BinEntry`
+        """:obj:`list` of  :class:`BinEntry`: Returns the entries of
+        the small bins.
         """
         return self.entries[SMALL_BINS_START_INDEX:LARGE_BINS_START_INDEX]
 
     @property
     def large_bins_entries(self):
-        """Returns the entries of the large bins.
-
-        Returns:
-            :obj:`list` of  :class:`BinEntry`
+        """:obj:`list` of :class:`BinEntry`: Returns the entries of
+        the large bins.
         """
         return self.entries[LARGE_BINS_START_INDEX:]
 
