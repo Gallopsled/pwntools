20f708794ce433303036d9169d6b5209b69db428
Eloy Pérez <zer1t0ps@protonmail.com>
2021-03-13 13:20:33 +0100

    use #: to document tcache attributes

--- pwnlib/heap/bins/tcache/tcache.py
+++ pwnlib/heap/bins/tcache/tcache.py
@@ -3,32 +3,31 @@ from pwnlib.heap.bins.bin import Bins, Bin, BinEntry
 
 class Tcaches(Bins):
     """Sequence of tcache bins.
-
-    Attributes:
-        bins (:obj:`list` of :class:`Tcache`): The bins of the sequence.
-
     """
-    pass
+
+    @property
+    def bins(self):
+        """:obj:`list` of :class:`Tcache`: The bins of the sequence."""
+        return super(Tcaches, self).bins
 
 
 class Tcache(Bin):
     """Class to represent a tcache of the glibc
-
-    Attributes:
-        bin_entry (TcacheEntry): The entry of tcache_perthread_struct for the
-            bin.
     """
 
     def __init__(self, bin_entry, malloc_chunks):
         super(Tcache, self).__init__(bin_entry, malloc_chunks)
 
+    @property
+    def bin_entry(self):
+        """:class:`TcacheEntry`: The entry of tcache_perthread_struct for the
+            bin."""
+        return super(Tcache, self).bin_entry
+
 
 class TcacheEntry(BinEntry):
     """Class to contain the information of each entry in
     tcache_perthread_struct struct.
-
-    Attributes:
-        bk (int): 0 since it is not used.
     """
 
     def __init__(self, address, fd, chunks_size):
