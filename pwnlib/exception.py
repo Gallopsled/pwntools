a4ab6e3d9aaadca1043c2a9d74ea836c459302ca
Hunter Gregal <huntergregal@users.noreply.github.com>
2021-05-07 04:06:05 -0400

    Update pwnlib/exception.py
    tru
    
    Co-authored-by: Arusekk <arek_koz@o2.pl>

--- pwnlib/exception.py
+++ pwnlib/exception.py
@@ -20,7 +20,7 @@ class PwnlibException(Exception):
         if self.reason:
             s += '\nReason:\n'
             s += ''.join(traceback.format_exception(*self.reason))
-        elif sys.exc_info() not in [None, KeyboardInterrupt]:
+        elif sys.exc_info()[0] not in [None, KeyboardInterrupt]:
             s += '\n'
             s += ''.join(traceback.format_exc())
         return s
