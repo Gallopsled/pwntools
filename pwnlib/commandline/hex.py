47fddadac33c73dbf23156e8d3d2b351910aee6b
marcan2020 <marcan2020@users.noreply.github.com>
2022-10-17 16:02:35 -0400

    Add formats for hex

--- pwnlib/commandline/hex.py
+++ pwnlib/commandline/hex.py
@@ -16,14 +16,40 @@ parser = common.parser_commands.add_parser(
 parser.add_argument('data', nargs='*',
     help='Data to convert into hex')
 
+parser.add_argument(
+    '-p', '--prefix',
+    metavar = 'prefix',
+    type = str,
+    default = '',
+    help = 'Insert a prefix before each byte',
+)
+
+parser.add_argument(
+    '-s', '--separator',
+    metavar = 'separator',
+    type = str,
+    default = '',
+    help = 'Add a separator between each byte',
+)
+
+def format_hex(s, prefix, separator):
+    n = 2
+    parts = [s[i:i+n] for i in range(0, len(s), n)]
+    return separator.join([f"{prefix}{x}" for x in parts])
+
 def main(args):
     if not args.data:
-        print(enhex(getattr(sys.stdin, 'buffer', sys.stdin).read()))
+        encoded = enhex(getattr(sys.stdin, 'buffer', sys.stdin).read())
     else:
         data = ' '.join(args.data)
         if not hasattr(data, 'decode'):
             data = data.encode('utf-8', 'surrogateescape')
-        print(enhex(data))
+        encoded = enhex(data)
+
+    if args.prefix or args.separator:
+        encoded = format_hex(encoded, args.prefix, args.separator)
+
+    print(encoded)
 
 if __name__ == '__main__':
     common.main(__file__)
