39ef958bfeb7252abcdde304a8f2eb73eb7ec9e0
Arusekk <arek_koz@o2.pl>
2022-12-29 14:42:14 +0100

    Begin working on 4.11.0

--- setup.py
+++ setup.py
@@ -89,7 +89,7 @@ setup(
     name                 = 'pwntools',
     python_requires      = '>=2.7',
     packages             = find_packages(),
-    version              = '4.10.0beta0',
+    version              = '4.11.0dev',
     data_files           = [('pwntools-doc',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
