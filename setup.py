d590a0b77fc5908095e3fd351816be3c7c73de5c
Arusekk <arek_koz@o2.pl>
2023-05-21 16:16:11 +0200

    Begin working on 4.12.0

--- setup.py
+++ setup.py
@@ -59,7 +59,7 @@ if not os.path.exists(PythonH):
     sys.exit(-1)
 
 setup(
-    version              = '4.11.0beta0',
+    version              = '4.12.0dev',
     data_files           = [('pwntools-doc',
                              glob.glob('*.md') + glob.glob('*.txt')),
                             ],
