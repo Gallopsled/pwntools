edad4ab8daf583bed9a405213cedcc20651b89fe
Peace-Maker <peace-maker@wcfan.de>
2023-05-31 19:52:18 +0200

    Fix typo in ssh trying to access log.Progress.error
    That function doesn't exist and .failure() is used everywhere else.

--- pwnlib/tubes/ssh.py
+++ pwnlib/tubes/ssh.py
@@ -1474,7 +1474,7 @@ from ctypes import *; libc = CDLL('libc.so.6'); print(libc.getenv(%r))
             self._download_raw(remote, local, p)
 
             if not self._verify_local_fingerprint(fingerprint):
-                p.error('Could not download file %r' % remote)
+                raise PwnlibException('Could not download file %r' % remote)
 
         return local
 
