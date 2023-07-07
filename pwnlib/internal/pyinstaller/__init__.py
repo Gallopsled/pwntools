29de4c5bd06e03fed9218b5d1000d103e9997d2a
Peace-Maker <peace-maker@wcfan.de>
2023-07-07 22:57:34 +0200

    Add pyinstaller hook
    This allows to create single-file exploits without the need to have
    python installed. You still need to have external binutils for asm/disasm.
    
    Closes #2086

---
+++ pwnlib/internal/pyinstaller/__init__.py
@@ -0,0 +1,4 @@
+import os
+
+def get_hook_dirs():
+    return [os.path.dirname(__file__)]
---
+++ pwnlib/internal/pyinstaller/hook-pwnlib.py
@@ -0,0 +1,12 @@
+from PyInstaller.utils.hooks import collect_data_files, collect_submodules
+
+hiddenimports = [
+    "pwnlib.atexception",
+    "pwnlib.pep237",
+    "pwnlib.update",
+    "pwnlib.useragents",
+]
+hiddenimports += collect_submodules("pwnlib.constants")
+
+datas = collect_data_files("pwnlib.data")
+datas += collect_data_files("pwnlib.shellcraft", subdir="templates")
