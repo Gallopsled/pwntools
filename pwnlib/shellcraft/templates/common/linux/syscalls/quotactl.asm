1399c7a9698f27361c5175424557fd4e3c65948f
Arusekk <arek_koz@o2.pl>
2023-07-09 22:00:54 +0200

    Shellcraft stackargs (#2219)
    From: Peace-Maker <peace-maker@wcfan.de>
    
    * Shellcraft: Fix spilling syscall args to the stack
    
    There was a typo in the generated syscall template causing only the last
    stack argument to be generated.
    
    $ shellcraft -f asm mips.linux.sendto 3 0x123456 0x100 0 0xabcdefff 0x10
    The 0xabcdefff argument was missing.
    
    * Shellcraft: Generate 6 syscall arguments for unknown functions
    
    There are syscalls with 6 arguments like mmap and sendto which couldn't
    be called with all 6 arguments set.
    
    $ shellcraft -f asm mips.linux.mmap2 0 0x1000
    'PROT_READ | PROT_WRITE | PROT_EXEC' 'MAP_PRIVATE | MAP_ANONYMOUS' -1 0
    
    * Shellcraft: Fix typo in error message
    
    The `syscalls` variable is only present in the generate.py, not the
    generated template.
    
    * Regenerate syscall templates
    
    * MIPS shellcraft: Push last two syscall arguments to the stack
    
    The mips.linux.syscall template was only handling 4 syscall arguments
    and silently discarded the remaining ones. Push the arguments to the stack
    instead.
    
    $ shellcraft -f asm mips.linux.syscall SYS_sendto 3 0x123456 0x100 0 0xabcdefff 0x10
    
    Fixes #2153
    
    * Fix mips syscall doctest
    
    * Update CHANGELOG
    
    Co-authored-by: Peace-Maker <peace-maker@wcfan.de>

--- pwnlib/shellcraft/templates/common/linux/syscalls/quotactl.asm
+++ pwnlib/shellcraft/templates/common/linux/syscalls/quotactl.asm
@@ -51,7 +51,7 @@ Returns:
                 target = regs[index]
                 register_arguments[target] = arg
             elif arg is not None:
-                stack_arguments[index] = arg
+                stack_arguments[name] = arg
 
         # The argument is not a register.  It is a string value, and we
         # are expecting a string value
@@ -79,7 +79,7 @@ Returns:
                 target = regs[index]
                 register_arguments[target] = arg
             elif arg is not None:
-                stack_arguments[target] = arg
+                stack_arguments[name] = arg
 
     # Some syscalls have different names on various architectures.
     # Determine which syscall number to use for the current architecture.
@@ -87,7 +87,7 @@ Returns:
         if hasattr(pwnlib.constants, syscall):
             break
     else:
-        raise Exception("Could not locate any syscalls: %r" % syscalls)
+        raise Exception("Could not locate any syscalls: %r" % ['SYS_quotactl'])
 %>
     /* quotactl(${', '.join(syscall_repr)}) */
 %for name, arg in string_arguments.items():
