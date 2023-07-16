07bdf278a93a18eb427143fa57a43010fdea8741
Arusekk <arek_koz@o2.pl>
2023-07-16 23:05:02 +0200

    shellcraft/i386: optimize stackhunter

--- pwnlib/shellcraft/templates/i386/stackhunter.asm
+++ pwnlib/shellcraft/templates/i386/stackhunter.asm
@@ -1,7 +1,7 @@
 <% from pwnlib.shellcraft import common %>
-<%page args="cookie = 0x7afceb58"/>
+<%page args="cookie = 0xe4fffc75"/>
 <%docstring>
-    stackhunter(cookie = 0x7afceb58)
+    stackhunter(cookie = 0xe4fffc75)
 
     Returns an an egghunter, which searches from esp and upwards
     for a cookie. However to save bytes, it only looks at a single
@@ -9,7 +9,7 @@
     generate a suitable cookie prefix for you.
 
     The default cookie has been chosen, because it makes it possible
-    to shave a single byte, but other cookies can be used too.
+    to shave two bytes, but other cookies can be used too.
 
 Example:
 
@@ -23,15 +23,18 @@ Example:
     583defbeadde75f8ffe4
 </%docstring>
 <% stackhunter = common.label("stackhunter") %>
-%if (cookie & 0xffffff) == 0xfceb58:
 ${stackhunter}:
-    cmp eax, ${hex(cookie)}
+%if (cookie & 0xffffff) == 0xfceb58:
+    cmp eax, ${'%#x' % cookie}
     jne ${stackhunter}+1
     jmp esp
 %else:
-${stackhunter}:
     pop eax
-    cmp eax, ${hex(cookie)}
+    cmp eax, ${'%#x' % cookie}
+%if cookie == 0xe4fffc75:
+    jmp ${stackhunter}+2
+%else:
     jne ${stackhunter}
     jmp esp
+%endif
 %endif
