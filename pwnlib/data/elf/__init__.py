09226dbd79413a66375fbb58fcc1888672a518b4
ValekoZ <lucas.valentin88@gmail.com>
2023-12-06 13:30:45 +0100

    [ssh.libs] Import pwnlib.data.elf.ssh_libs in pwnlib.data.elf

--- pwnlib/data/elf/__init__.py
+++ pwnlib/data/elf/__init__.py
@@ -2,6 +2,7 @@ from __future__ import absolute_import
 from pwnlib.data.elf import fmtstr
 from pwnlib.data.elf import relro
 from pwnlib.data.elf import ret2dlresolve
+from pwnlib.data.elf import ssh_libs
 
 import os
 path = os.path.dirname(__file__)
