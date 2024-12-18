d80b0968b0e35c2dfa6767850683dd2b866f14c8
Peace-Maker <peacemakerctf@gmail.com>
2024-12-18 11:51:09 +0100

    Use threading.Timer for doctest timeout
    To interrupt the code running on the main thread, we send a signal using `_thread.interrupt_main()`. By default this causes a KeyboardInterrupt exception, which might be handled explicitly.
    
    To raise an explicit EndlessLoop exception inside the code that is taking too long, register a SIGABRT signal handler which raises the EndlessLoop exception. The exception from the signal handler is added to the call stack and handled by the code currently running. This allows to print a better stack trace on timeout.
    
    It is the same concept as the old implementation using `signal.alarm` but platform agnostic.
    
    https://anonbadger.wordpress.com/2018/12/15/python-signal-handlers-and-exceptions/

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -483,17 +483,24 @@ if 'doctest' in sys.argv:
         import binascii
         paramiko.client.hexlify = lambda x: binascii.hexlify(x).decode()
         paramiko.util.safe_string = lambda x: '' # function result never *actually used*
-    class EndlessLoop(Exception): pass
-    if sys.platform == 'win32':
-        def alrm_handler():
-            raise EndlessLoop()
-        import threading
-        timer = threading.Timer(600, alrm_handler)
+    class EndlessLoop(BaseException): pass
+    def sigabrt_handler(signum, frame):
+        raise EndlessLoop()
+    # thread.interrupt_main received the signum parameter in Python 3.10
+    if sys.version_info >= (3, 10):
+        signal.signal(signal.SIGABRT, sigabrt_handler)
+    def alrm_handler():
+        try:
+            import thread
+        except ImportError:
+            import _thread as thread
+        # pre Python 3.10 this raises a KeyboardInterrupt in the main thread.
+        # it might not show a traceback in that case, but it will stop the endless loop.
+        thread.interrupt_main(signal.SIGABRT)
+        timer = threading.Timer(interval=180, function=alrm_handler) # three minutes
         timer.daemon = True
         timer.start()
-    else:
-        def alrm_handler(sig, frame):
-            signal.alarm(180) # three minutes
-            raise EndlessLoop()
-        signal.signal(signal.SIGALRM, alrm_handler)
-        signal.alarm(600) # ten minutes
+    import threading
+    timer = threading.Timer(interval=600, function=alrm_handler) # ten minutes
+    timer.daemon = True
+    timer.start()
