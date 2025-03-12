7339952548cd771b23b07a7cd093a641dd0a7d4d
RocketDev <ma2014119@outlook.com>
2025-03-12 12:17:55 +0800

    filepointer: check type and len when setattr;
    filepointer: check type and len when setattr; format code and add type
    hint.

--- pwnlib/structs/filepointer.py
+++ pwnlib/structs/filepointer.py
@@ -28,88 +28,93 @@ from __future__ import division
 from pwnlib.context import context
 from pwnlib.log import getLogger
 from pwnlib.util.misc import python_2_bytes_compatible
-from pwnlib.util.packing import pack
+from pwnlib.util.packing import pack, _need_bytes
 
 log = getLogger(__name__)
 
-length=0
-size='size'
-name='name'
+length = 0
+size = 'size'
+name = 'name'
 
 variables={
-    0:{name:'flags',size:length},
-    1:{name:'_IO_read_ptr',size:length},
-    2:{name:'_IO_read_end',size:length},
-    3:{name:'_IO_read_base',size:length},
-    4:{name:'_IO_write_base',size:length},
-    5:{name:'_IO_write_ptr',size:length},
-    6:{name:'_IO_write_end',size:length},
-    7:{name:'_IO_buf_base',size:length},
-    8:{name:'_IO_buf_end',size:length},
-    9:{name:'_IO_save_base',size:length},
-    10:{name:'_IO_backup_base',size:length},
-    11:{name:'_IO_save_end',size:length},
-    12:{name:'markers',size:length},
-    13:{name:'chain',size:length},
-    14:{name:'fileno',size:4},
-    15:{name:'_flags2',size:4},
-    16:{name:'_old_offset',size:length},
-    17:{name:'_cur_column',size:2},
-    18:{name:'_vtable_offset',size:1},
-    19:{name:'_shortbuf',size:1},
-    20:{name:'unknown1',size:-4},
-    21:{name:'_lock',size:length},
-    22:{name:'_offset',size:8},
-    23:{name:'_codecvt',size:length},
-    24:{name:'_wide_data',size:length},
-    25:{name:'unknown2',size:length},
-    26:{name:'vtable',size:length}
+    0: {name: 'flags', size: length},
+    1: {name: '_IO_read_ptr', size: length},
+    2: {name: '_IO_read_end', size: length},
+    3: {name: '_IO_read_base', size: length},
+    4: {name: '_IO_write_base', size: length},
+    5: {name: '_IO_write_ptr', size: length},
+    6: {name: '_IO_write_end', size: length},
+    7: {name: '_IO_buf_base', size: length},
+    8: {name: '_IO_buf_end', size: length},
+    9: {name: '_IO_save_base', size: length},
+    10: {name: '_IO_backup_base', size: length},
+    11: {name: '_IO_save_end', size: length},
+    12: {name: 'markers', size: length},
+    13: {name: 'chain', size: length},
+    14: {name: 'fileno', size: 4},
+    15: {name: '_flags2', size: 4},
+    16: {name: '_old_offset', size: length},
+    17: {name: '_cur_column', size: 2},
+    18: {name: '_vtable_offset', size: 1},
+    19: {name: '_shortbuf', size: 1},
+    20: {name: 'unknown1', size: -4},
+    21: {name: '_lock', size: length},
+    22: {name: '_offset', size: 8},
+    23: {name: '_codecvt', size: length},
+    24: {name: '_wide_data', size: length},
+    25: {name: 'unknown2', size: length},
+    26: {name: 'vtable', size: length}
 }
 
 del name, size, length
 
 
-def update_var(l):
+def update_var(l: int) -> dict[str, int]:
     r"""
-    Since different members of the file structure have different sizes, we need to keep track of the sizes. The following function is used by the FileStructure class to initialise the lengths of the various fields.
+    Since different members of the file structure have different sizes,
+    we need to keep track of the sizes. The following function is used
+    by the FileStructure class to initialise the lengths of the various fields.
 
     Arguments:
         l(int)
             l=8 for 'amd64' architecture and l=4 for 'i386' architecture
 
     Return Value:
-        Returns a dictionary in which each field is mapped to its corresponding length according to the architecture set
+        Returns a dictionary in which each field is mapped to its corresponding
+        length according to the architecture set
 
     Examples:
 
         >>> update_var(8)
         {'flags': 8, '_IO_read_ptr': 8, '_IO_read_end': 8, '_IO_read_base': 8, '_IO_write_base': 8, '_IO_write_ptr': 8, '_IO_write_end': 8, '_IO_buf_base': 8, '_IO_buf_end': 8, '_IO_save_base': 8, '_IO_backup_base': 8, '_IO_save_end': 8, 'markers': 8, 'chain': 8, 'fileno': 4, '_flags2': 4, '_old_offset': 8, '_cur_column': 2, '_vtable_offset': 1, '_shortbuf': 1, 'unknown1': 4, '_lock': 8, '_offset': 8, '_codecvt': 8, '_wide_data': 8, 'unknown2': 48, 'vtable': 8}
     """
-    var={}
+    var = {}
     for i in variables:
-        var[variables[i]['name']]=variables[i]['size']
+        var[variables[i]['name']] = variables[i]['size']
     for i in var:
-        if var[i]<=0:
-            var[i]+=l
-    if l==4:
-        var['unknown2']=56
+        if var[i] <= 0:
+            var[i] += l
+    if l == 4:
+        var['unknown2'] = 56
     else:
-        var['unknown2']=48
+        var['unknown2'] = 48
     return var
 
 
-@python_2_bytes_compatible
 class FileStructure(object):
     r"""
-    Crafts a FILE structure, with default values for some fields, like _lock which should point to null ideally, set.
+    Crafts a FILE structure, with default values for some fields, like _lock
+    which should point to null ideally, set.
 
     Arguments:
         null(int)
-            A pointer to NULL value in memory. This pointer can lie in any segment (stack, heap, bss, libc etc)
+            A pointer to NULL value in memory. This pointer can lie in any
+            segment (stack, heap, bss, libc etc)
 
     Examples:
 
-        FILE structure with flags as 0xfbad1807 and _IO_buf_base and _IO_buf_end pointing to 0xcafebabe and 0xfacef00d
+        FILE structure with flags as 0xfbad1807 and _IO_buf_base and _IO_buf_end
+        pointing to 0xcafebabe and 0xfacef00d
 
         >>> context.clear(arch='amd64')
         >>> fileStr = FileStructure(null=0xdeadbeeef)
@@ -125,7 +130,9 @@ class FileStructure(object):
         >>> len(fileStr)
         224
 
-        The definition for __repr__ orders the structure members and displays then in a dictionary format. It's useful when viewing a structure objet in python/IPython shell
+        The definition for __repr__ orders the structure members and displays
+        then in a dictionary format. It's useful when viewing a structure
+        object in python/IPython shell.
 
         >>> q=FileStructure(0xdeadbeef)
         >>> q
@@ -161,47 +168,69 @@ class FileStructure(object):
     vars_=[]
     length={}
 
-    def __init__(self, null=0):
+    def __init__(self, null: int=0):
             self.vars_ = [variables[i]['name'] for i in sorted(variables.keys())]
-            self.setdefault(null)
             self.length = update_var(context.bytes)
+            self.setdefault(null)
             self._old_offset = (1 << context.bits) - 1
 
-    def __setattr__(self,item,value):
-        if item in FileStructure.__dict__ or item in self.vars_:
-            object.__setattr__(self,item,value)
+    def __setattr__(self, item: str, value: int | bytes | str):
+        if item in self._vars:
+            maxsize = self.length[item]
+            if isinstance(value, int):
+                if value < 0 or value >= (1 << maxsize):
+                    log.error(f"Out of bounds for {item}: expect item size {maxsize}, but get {value}")
+            elif isinstance(value, (str, bytes)):
+                value = _need_bytes(value)
+                if len(value) > maxsize:
+                    log.error(f"Value too large for {item}: expect item size {maxsize}, but get {len(value)}")
+            elif hasattr(value, '__bytes__') is False:
+                log.error(f"Unable to cast {item} to bytes")
+                # if value can cast to bytes, we don't check its size here
+                # as it may be mutable
+            object.__setattr__(self, item, value)
+        elif item in FileStructure.__dict__:
+            object.__setattr__(self, item, value)
         else:
-            log.error("Unknown variable %r" % item)
+            log.error(f"Unknown variable {item}")
 
-    def __repr__(self):
-        structure=[]
+    def __repr__(self) -> str:
+        structure = []
         for i in self.vars_:
             e = getattr(self, i)
             if isinstance(e, bytes):
-                structure.append(" %s: %s" % (i, e))
+                structure.append(f" {i}, {e}")
+            elif isinstance(e, int):
+                structure.append(f" {i}, {e:#x}")
             else:
-                structure.append(" %s: %#x" % (i, e))
+                structure.append(f" {i}, {bytes(e)}")
         return "{"+ "\n".join(structure)+"}"
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(bytes(self))
 
-    def __bytes__(self):
-        structure = b''
+    def __bytes__(self) -> bytes:
+        structure = bytearray()
         for val in self.vars_:
-            if isinstance(getattr(self, val), bytes):
-                structure += getattr(self, val).ljust(context.bytes, b'\x00')
-            else:
-                if self.length[val] > 0:
-                    structure += pack(getattr(self, val), self.length[val]*8)
-        return structure
+            if self.length[val] > 0:
+                value = getattr(self, val)
+                if isinstance(value, int):
+                    structure.append(pack(value, self.length[val] * 8))
+                elif isinstance(value, bytes):
+                    structure.append(value.ljust(self.length[val], b'\x00'))
+                else:
+                    cast = bytes(value)
+                    if len(cast) > self.length[val]:
+                        log.error(f"Unable to fit {value} as it's larger than {self.length[val]}")
+                    structure.append(cast.ljust(self.length[val], b'\x00'))
+        return bytes(structure)
 
-    def struntil(self,v):
+    def struntil(self, v) -> str:
         r"""
         Payload for stuff till 'v' where 'v' is a structure member. This payload includes 'v' as well.
 
         Arguments:
-            v(string)
+            v(str)
                 The name of the field uptil which the payload should be created.
 
         Example:
@@ -216,46 +245,53 @@ class FileStructure(object):
         """
         if v not in self.vars_:
             return b''
-        structure = b''
+        structure = bytearray()
         for val in self.vars_:
-            if isinstance(getattr(self, val), bytes):
-                structure += getattr(self, val).ljust(context.bytes, b'\x00')
-            else:
-                structure += pack(getattr(self, val), self.length[val]*8)
+            if self.length[val] > 0:
+                value = getattr(self, val)
+                if isinstance(value, int):
+                    structure.append(pack(value, self.length[val] * 8))
+                elif isinstance(value, bytes):
+                    structure.append(value.ljust(self.length[val], b'\x00'))
+                else:
+                    cast = bytes(value)
+                    if len(cast) > self.length[val]:
+                        log.error(f"Unable to fit {value} as it's larger than {self.length[val]}")
+                    structure.append(cast.ljust(self.length[val], b'\x00'))
             if val == v:
                 break
-        return structure[:-1]
+        return bytes(structure)[:-1]
 
-    def setdefault(self,null):
-            self.flags=0
-            self._IO_read_ptr=0
-            self._IO_read_end=0
-            self._IO_read_base=0
-            self._IO_write_base=0
-            self._IO_write_ptr=0
-            self._IO_write_end=0
-            self._IO_buf_base=0
-            self._IO_buf_end=0
-            self._IO_save_base=0
-            self._IO_backup_base=0
-            self._IO_save_end=0
-            self.markers=0
-            self.chain=0
-            self.fileno=0
-            self._flags2=0
-            self._old_offset=0
-            self._cur_column=0
-            self._vtable_offset=0
-            self._shortbuf=0
-            self.unknown1=0
-            self._lock=null
-            self._offset=0xffffffffffffffff
-            self._codecvt=0
-            self._wide_data=null
-            self.unknown2=0
-            self.vtable=0
+    def setdefault(self, null: int):
+            self.flags = 0
+            self._IO_read_ptr = 0
+            self._IO_read_end = 0
+            self._IO_read_base = 0
+            self._IO_write_base = 0
+            self._IO_write_ptr = 0
+            self._IO_write_end = 0
+            self._IO_buf_base = 0
+            self._IO_buf_end = 0
+            self._IO_save_base = 0
+            self._IO_backup_base = 0
+            self._IO_save_end = 0
+            self.markers = 0
+            self.chain = 0
+            self.fileno = 0
+            self._flags2 = 0
+            self._old_offset = 0
+            self._cur_column = 0
+            self._vtable_offset = 0
+            self._shortbuf = 0
+            self.unknown1 = 0
+            self._lock = null
+            self._offset = 0xffffffffffffffff
+            self._codecvt = 0
+            self._wide_data = null
+            self.unknown2 = 0
+            self.vtable = 0
 
-    def write(self,addr=0,size=0):
+    def write(self, addr: int=0, size: int=0) -> bytes:
         r"""
         Writing data out from arbitrary memory address.
 
@@ -275,15 +311,15 @@ class FileStructure(object):
             >>> payload
             b'\x00\x08\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xbe\xba\xfe\xca\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xbe\xba\xfe\xca\x00\x00\x00\x00"\xbb\xfe\xca\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x01\x00\x00'
         """
-        self.flags &=~8
-        self.flags |=0x800
+        self.flags &= ~8
+        self.flags |= 0x800
         self._IO_write_base = addr
-        self._IO_write_ptr = addr+size
+        self._IO_write_ptr = addr + size
         self._IO_read_end = addr
         self.fileno = 1
         return self.struntil('fileno')
 
-    def read(self,addr=0,size=0):
+    def read(self, addr: int=0, size: int=0) -> bytes:
         r"""
         Reading data into arbitrary memory location.
 
@@ -303,15 +339,15 @@ class FileStructure(object):
             >>> payload
             b'\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\xbe\xba\xfe\xca\x00\x00\x00\x00"\xbb\xfe\xca\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00\x00'
         """
-        self.flags &=~4
+        self.flags &= ~4
         self._IO_read_base = 0
         self._IO_read_ptr = 0
         self._IO_buf_base = addr
-        self._IO_buf_end = addr+size
+        self._IO_buf_end = addr + size
         self.fileno = 0
         return self.struntil('fileno')
 
-    def orange(self,io_list_all,vtable):
+    def orange(self, io_list_all: int, vtable: int) -> bytes:
         r"""
         Perform a House of Orange (https://github.com/shellphish/how2heap/blob/master/glibc_2.23/house_of_orange.c), provided you have libc leaks.
 
@@ -334,11 +370,11 @@ class FileStructure(object):
         if context.bits == 64:
             self.flags = b'/bin/sh\x00'
             self._IO_read_ptr = 0x61
-            self._IO_read_base = io_list_all-0x10
+            self._IO_read_base = io_list_all - 0x10
         elif context.bits == 32:
             self.flags = b'sh\x00'
             self._IO_read_ptr = 0x121
-            self._IO_read_base = io_list_all-0x8
+            self._IO_read_base = io_list_all - 0x8
         self._IO_write_base = 0
         self._IO_write_ptr = 1
         self.vtable = vtable
