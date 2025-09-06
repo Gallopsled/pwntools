2f1ab9c78ba3b8f9585064c4421accf89b6f95c4
Peace-Maker <peacemakerctf@gmail.com>
2025-09-06 14:20:43 +0200

    Fix lookup of binutils for `mipsel` architecture
    The endianness is encoded in the architecture name for mips targets which wasn't respected while looking for the correct binutils binary.
    
    Sync the CI and Dockerfile binutils packages while we're at it.
    
    Fixes #2591

--- pwnlib/asm.py
+++ pwnlib/asm.py
@@ -167,6 +167,8 @@ def which_binutils(util, check_version=False):
         '.../bin/arm-...-as'
         >>> which_binutils('as', arch='powerpc') #doctest: +ELLIPSIS
         '.../bin/powerpc...-as'
+        >>> which_binutils('as', arch='mips', endianness='little') #doctest: +ELLIPSIS
+        '.../bin/mipsel...-as'
         >>> which_binutils('as', arch='msp430') #doctest: +SKIP
         ...
         Traceback (most recent call last):
@@ -174,6 +176,12 @@ def which_binutils(util, check_version=False):
         Exception: Could not find 'as' installed for ContextType(arch = 'msp430')
     """
     arch = context.arch
+
+    # Handle endianness-specific naming for mips/mips64
+    arch = {
+        ('mips', 'little'): 'mipsel',
+        ('mips64', 'little'): 'mips64el',
+    }.get((arch, context.endianness), arch)
 
     # Fix up pwntools vs Debian triplet naming, and account
     # for 'thumb' being its own pwntools architecture.
@@ -183,6 +191,7 @@ def which_binutils(util, check_version=False):
         'i686':   ['x86_64', 'amd64'],
         'amd64':  ['x86_64', 'i386'],
         'mips64': ['mips'],
+        'mips64el': ['mipsel'],
         'powerpc64': ['powerpc'],
         'sparc64': ['sparc'],
         'riscv32': ['riscv32', 'riscv64', 'riscv'],
