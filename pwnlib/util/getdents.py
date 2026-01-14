18fef25ae1ba7886716402a8c3df6943178708ba
dbgbgtf <dudududuMaxVer@outlook.com>
2026-01-14 19:57:57 +0800

    fix test_case. use fromhex instead of unhex.

--- pwnlib/util/getdents.py
+++ pwnlib/util/getdents.py
@@ -95,8 +95,8 @@ def dirents(buf: bytes) -> list[linux_dirent]:
         A list of file names
 
     Example:
-        >>> with context.local(bits = 32):
-        ...     buf = unhex('223a2c0000000000786a631cc120fc1a2000746573742e6300e57464040000080d002c00000000004802ee451f347e3018002e000000000402002c0000000000ffffffffffffff7f18002e2e00000004')
+        >>> with context.local(bytes = 4):
+        ...     buf = bytes.fromhex('5e843600c120fc1a1400746573742e63000000085f8436001f347e3010002e00be36ba040d002c00ffffff7f10002e2e00000004')
         ...     print(dirents(buf))
         ...     
         [DT_REG  test.c, DT_DIR  ., DT_DIR  ..]
@@ -128,8 +128,8 @@ def dirents64(buf: bytes) -> list[linux_dirent]:
         A list of file names
 
     Example:
-        >>> with context.local(bits = 64):
-        ...     buf = unhex('223a2c0000000000786a631cc120fc1a200008746573742e63007464040000000d002c00000000004802ee451f347e301800042e0000000002002c0000000000ffffffffffffff7f1800042e2e000000')
+        >>> with context.local(bytes = 8):
+        ...     buf = bytes.fromhex('223a2c0000000000786a631cc120fc1a200008746573742e63007464040000000d002c00000000004802ee451f347e301800042e0000000002002c0000000000ffffffffffffff7f1800042e2e000000')
         ...     print(dirents64(buf))
         ...     
         [DT_REG  test.c, DT_DIR  ., DT_DIR  ..]
