d59150dc6c79a9f14da8f7e8efeb323af82ff277
RocketDev <ma2014119@outlook.com>
2026-02-20 22:17:19 +0800

    feat: implement basic Exit*

--- pwnlib/libc/glibc.py
+++ pwnlib/libc/glibc.py
@@ -1,8 +1,12 @@
 """
 Some glibc related convenient functions.
 """
+from __future__ import annotations
+from enum import IntEnum
+
 from pwnlib.context import context
 from pwnlib.util.fiddling import ror, rol
+from pwnlib.util.packing import flat, unpack, unpack_many, _need_bytes
 
 def ptr_mangle(guard: int, value: int) -> int:
     """
@@ -107,3 +111,148 @@ def reveal_ptr_same_page(ptr_value: int) -> int:
         ptr_value ^= key >> 12
         mask >>= 12
     return ptr_value
+
+
+class ExitFlavor(IntEnum):
+    FREE = 0
+    USED = 1
+    ON   = 2
+    AT   = 3
+    CXA  = 4
+
+class ExitFunc:
+    flavor: ExitFlavor
+    fn: int
+    guard: int
+    arg: int
+    dso: int
+
+    def __init__(self, flavor: ExitFlavor, func: int, guard: int,
+                 arg: int | None = None, dso: int | None = None):
+        self.flavor = flavor
+        self.fn = func
+        self.guard = guard
+        self.arg = arg
+        self.dso = dso
+
+        match flavor:
+            case ExitFlavor.ON:
+                if arg is None:
+                    raise TypeError(f'on_exit requires an arg pointer')
+            case ExitFlavor.FREE | ExitFlavor.USED | ExitFlavor.AT:
+                pass
+            case ExitFlavor.CXA:
+                if arg is None or dso is None:
+                    raise TypeError(f'cxa_exit requires both arg and dso_handle pointer')
+            case _:
+                raise TypeError(f'flavor must be an ExitFlavor, instead of {type(flavor)}')
+
+    def __repr__(self) -> str:
+        match self.flavor:
+            case ExitFlavor.ON:
+                extra = f', arg={self.arg:#x}'
+            case ExitFlavor.AT:
+                extra = ''
+            case ExitFlavor.CXA:
+                extra = f', arg={self.arg:#x}, dso_handle={self.dso:#x}'
+            case ExitFlavor.USED | ExitFlavor.FREE:
+                return f'ExitFunc({self.flavor.name})'
+
+        return f'ExitFunc({self.flavor.name}, fn={self.fn:#x} ^ {self.guard:#x}{extra})'
+
+    def __bytes__(self) -> bytes:
+        match self.flavor:
+            case ExitFlavor.ON:
+                return flat(self.flavor, ptr_mangle(self.guard, self.fn),
+                            self.arg)
+            case ExitFlavor.AT:
+                return flat(self.flavor, ptr_mangle(self.guard, self.fn))
+            case ExitFlavor.CXA:
+                return flat(self.flavor, ptr_mangle(self.guard, self.fn),
+                            self.arg, self.dso)
+            case ExitFlavor.FREE | ExitFlavor.USED:
+                return flat(self.flavor)
+
+    def __flat__(self) -> bytes:
+        return bytes(self)
+
+    @staticmethod
+    def from_bytes(data: bytes, guard: int) -> ExitFunc:
+        """
+        Examples:
+            >>> context.arch = 'amd64'
+            >>> guard = 0x2f21c4a298024bcd
+            >>> blob = bytes.fromhex('0300000000000000435ea835ae9aef23')
+            >>> glibc.ExitFunc.from_bytes(blob, guard)
+            ExitFunc(AT, fn=0x555555555119 ^ 0x2f21c4a298024bcd)
+            >>> blob = bytes.fromhex('0200000000000000435ea835ae9aef230000371337130000')
+            >>> glibc.ExitFunc.from_bytes(blob, guard)
+            ExitFunc(ON, fn=0x555555555119 ^ 0x2f21c4a298024bcd, arg=0x133713370000)
+            >>> blob = bytes.fromhex('0000000000000000')
+            >>> glibc.ExitFunc.from_bytes(blob, guard)
+            ExitFunc(FREE)
+            >>> blob = bytes.fromhex('0100000000000000')
+            >>> glibc.ExitFunc.from_bytes(blob, guard)
+            ExitFunc(USED)
+        """
+        data = _need_bytes(data)
+        if len(data) % 4 != 0 or len(data) == 0:
+            raise ValueError(f'data must be aligned to word boundry ({context.bytes} bytes)')
+        words = unpack_many(data)
+        try:
+            flavor = ExitFlavor(words[0])
+            match flavor:
+                case ExitFlavor.FREE | ExitFlavor.USED:
+                    return ExitFunc(flavor, 0, guard)
+                case ExitFlavor.ON:
+                    return ExitFunc(flavor, ptr_demangle(guard, words[1]), guard,
+                                    words[2])
+                case ExitFlavor.AT:
+                    return ExitFunc(flavor, ptr_demangle(guard, words[1]), guard)
+                case ExitFlavor.CXA:
+                    return ExitFunc(flavor, ptr_demangle(guard, words[1]), guard,
+                                    words[2], words[3])
+        except IndexError:
+            raise ValueError(f'Insufficient data when decoding ExitFunc') from None
+
+
+class ExitFuncList:
+    next: int
+    idx: int
+    fns: list[ExitFunc]
+
+    def __init__(self, nextp: int, funcs: list[ExitFunc]):
+        self.nextp = nextp
+        self.fns = funcs
+        self.idx = len(funcs)
+
+    def __repr__(self) -> str:
+        return f'ExitFuncList(next={self.nextp:#x}, idx={self.idx}, fns={self.fns})'
+
+    def __bytes__(self) -> bytes:
+        return flat(self.nextp, self.idx, self.fns)
+
+    def __flat__(self) -> bytes:
+        return bytes(self)
+
+    @staticmethod
+    def from_bytes(data: bytes, guard: int) -> ExitFuncList:
+        """
+        Examples:
+            >>> context.arch = 'amd64'
+            >>> guard = 0x2d42599562d398bb
+            >>> blob = bytes.fromhex('000000000000000001000000000000000400000000000000845ab66f5e2ad54c00000000000000000000000000000000')
+            >>> glibc.ExitFuncList.from_bytes(blob, guard)
+            ExitFuncList(next=0x0, idx=1, fns=[ExitFunc(CXA, fn=0x7ffff7fcaf60 ^ 0x2d42599562d398bb, arg=0x0, dso_handle=0x0)])
+        """
+        data = _need_bytes(data)
+        try:
+            nextp = unpack(data[0:context.bytes])
+            size  = unpack(data[context.bytes:context.bytes * 2])
+            off = context.bytes * 2
+            func_sz = 4 * context.bytes
+            fns = [ExitFunc.from_bytes(data[off + i * func_sz:off + (i + 1) * func_sz], guard)
+                   for i in range(size)]
+            return ExitFuncList(nextp, fns)
+        except IndexError:
+            raise ValueError(f'Insufficient data when decoding ExitFuncList') from None
