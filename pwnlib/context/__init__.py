e4e3228f162076ab148334e5b95597f7ba6300e8
RocketDev <ma2014119@outlook.com>
2026-04-13 19:04:32 +0800

    fix: correct escapes to suppress warning

--- pwnlib/context/__init__.py
+++ pwnlib/context/__init__.py
@@ -1585,7 +1585,7 @@ class ContextType(object):
         This is useful when you have multiple versions of WinDbg installed or the WinDbg binary is
         called something different.
 
-        Usually, it is installed to ``C:\Program Files (x86)\Windows Kits\10\Debuggers\x64\windbg.exe``.
+        Usually, it is installed to ``C:\\Program Files (x86)\\Windows Kits\\10\\Debuggers\\x64\\windbg.exe``.
         Adding the path to the Windows SDK to your PATH variable is recommended.
 
         If set to an empty string, pwntools will try to search for a reasonable WinDbg binary from 
@@ -1602,7 +1602,7 @@ class ContextType(object):
         This is useful when you have multiple versions of WinDbgX installed or the WinDbgX binary is
         called something different.
 
-        Usually, it is installed to ``%LocalAppData%\Microsoft\WindowsApps\WinDbgX.exe``.
+        Usually, it is installed to ``%LocalAppData%\\Microsoft\\WindowsApps\\WinDbgX.exe``.
 
         If set to an empty string, pwntools will try to search for a reasonable WinDbgX binary from 
         the path.
